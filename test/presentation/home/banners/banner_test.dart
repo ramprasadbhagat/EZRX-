ff149ac6bd6829e9cc0fae33e3111d9a63470a2d
Gokul R <gramachandran@zuelligpharma.com>
2022-09-13 21:43:26 +0530

    locator setup done for banner testing

--- test/presentation/home/banners/banner_test.dart
+++ test/presentation/home/banners/banner_test.dart
@@ -1,13 +1,10 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:dartz/dartz.dart';
 import 'package:ezrxmobile/application/auth/auth_bloc.dart';
-import 'package:ezrxmobile/application/auth/login/login_form_bloc.dart';
+import 'package:ezrxmobile/application/banner/banner_bloc.dart';
 import 'package:ezrxmobile/config.dart';
-import 'package:ezrxmobile/domain/auth/entities/loginv2.dart';
-import 'package:ezrxmobile/domain/auth/error/auth_failure.dart';
-import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
-import 'package:ezrxmobile/presentation/auth/login_page.dart';
-import 'package:ezrxmobile/presentation/splash/splash_page.dart';
+import 'package:ezrxmobile/infrastructure/core/countly/countly.dart';
+import 'package:ezrxmobile/infrastructure/core/http/http.dart';
+import 'package:ezrxmobile/presentation/home/banners/banner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,132 +13,57 @@ import 'package:mocktail/mocktail.dart';
 
 import '../../../utils/material_frame_wrapper.dart';
 
-class LoginFormBlocMock extends MockBloc<LoginFormEvent, LoginFormState>
-    implements LoginFormBloc {}
+class BannerBlocMock extends MockBloc<BannerEvent, BannerState>
+    implements BannerBloc {}
+
+class BannerMock extends Mock implements HomeBanner {
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return super.toString();
+  }
+}
 
 class AuthBlocMock extends MockBloc<AuthEvent, AuthState> implements AuthBloc {}
 
 void main() {
-  late LoginFormBloc loginBlocMock;
-  late AuthBloc authBlocMock;
+  late GetIt locator;
+  late BannerBlocMock bannerBlocMock;
 
   setUpAll(() {
-    GetIt.instance.registerSingleton<Config>(Config()..appFlavor = Flavor.uat);
+    locator = GetIt.instance;
+    locator.registerSingleton<Config>(Config()..appFlavor = Flavor.uat);
+    locator.registerLazySingleton(
+      () => CountlyService(),
+    );
+    locator.registerLazySingleton<HttpService>(
+      () => HttpService(
+        config: locator<Config>(),
+        interceptors: [],
+      ),
+    );
   });
 
-  group('Login Screen', () {
+  group('Home Banner', () {
     setUp(() {
-      loginBlocMock = LoginFormBlocMock();
-      authBlocMock = AuthBlocMock();
+      bannerBlocMock = BannerBlocMock();
 
-      when(() => loginBlocMock.state).thenReturn(LoginFormState.initial());
+      when(() => bannerBlocMock.state).thenReturn(BannerState.initial());
     });
-    testWidgets("Test don't have credential", (tester) async {
+
+    testWidgets('Banner test 1', (tester) async {
       await tester.pumpWidget(
         MaterialFrameWrapper(
-          child: BlocProvider<LoginFormBloc>(
-            create: (context) => loginBlocMock,
-            child: const LoginPage(),
+          child: BlocProvider<BannerBloc>(
+            create: (context) => bannerBlocMock,
+            child: HomeBanner(),
           ),
         ),
       );
 
       // Create the Finders.
-      final userNameTextField = find.byKey(const Key('loginUsernameField'));
-      final passwordTextField = find.byKey(const Key('loginPasswordField'));
-      final loginSubmitButton = find.byKey(const Key('loginSubmitButton'));
-      final ssoLoginButton = find.byKey(const Key('ssoLoginButton'));
-      final rememberPassWordCheckBox =
-          find.byKey(const Key('loginRememberPasswordCheckbox'));
+      final pageViewForHomeBanner = find.byKey(const Key('homeBanner'));
 
-      expect(userNameTextField, findsOneWidget);
-      expect(passwordTextField, findsOneWidget);
-      expect(loginSubmitButton, findsOneWidget);
-      expect(ssoLoginButton, findsOneWidget);
-      expect(rememberPassWordCheckBox, findsOneWidget);
-    });
-
-    testWidgets('Test login error', (tester) async {
-      final expectedStates = [
-        LoginFormState.initial().copyWith(
-          authFailureOrSuccessOption: optionOf(
-            const Left(AuthFailure.other('fake-message')),
-          ),
-          showErrorMessages: true,
-        ),
-      ];
-
-      whenListen(loginBlocMock, Stream.fromIterable(expectedStates));
-
-      await tester.pumpWidget(
-        MaterialFrameWrapper(
-          child: BlocProvider<LoginFormBloc>(
-            create: (context) => loginBlocMock,
-            child: const LoginPage(),
-          ),
-        ),
-      );
-
-      final errorMessage = find.byKey(const Key('snackBarMessage'));
-
-      expect(errorMessage, findsNothing);
-      await tester.pump();
-
-      expect(errorMessage, findsOneWidget);
-    });
-
-    testWidgets('Test login success', (tester) async {
-      final expectedStates = [
-        LoginFormState.initial().copyWith(
-          authFailureOrSuccessOption: optionOf(
-            Right(LoginV2(jwt: JWT('fake-success'))),
-          ),
-        ),
-      ];
-
-      when(() => authBlocMock.state)
-          .thenReturn(const AuthState.authenticated());
-
-      whenListen(loginBlocMock, Stream.fromIterable(expectedStates));
-
-      await tester.pumpWidget(
-        MaterialFrameWrapper(
-          child: MultiBlocProvider(
-            providers: [
-              BlocProvider<LoginFormBloc>(
-                create: (context) => loginBlocMock,
-              ),
-              BlocProvider<AuthBloc>(
-                create: (context) => authBlocMock,
-              ),
-            ],
-            child: const LoginPage(),
-          ),
-        ),
-      );
-
-      await tester.pump();
-
-      await tester.pumpWidget(MaterialFrameWrapper(
-        child: BlocProvider(
-          create: (context) => authBlocMock,
-          child: const SplashPage(),
-        ),
-      ));
-
-      // Create the Finders.
-      final userNameTextField = find.byKey(const Key('loginUsernameField'));
-      final passwordTextField = find.byKey(const Key('loginPasswordField'));
-      final loginSubmitButton = find.byKey(const Key('loginSubmitButton'));
-      final ssoLoginButton = find.byKey(const Key('ssoLoginButton'));
-      final rememberPassWordCheckBox =
-          find.byKey(const Key('loginRememberPasswordCheckbox'));
-
-      expect(userNameTextField, findsNothing);
-      expect(passwordTextField, findsNothing);
-      expect(loginSubmitButton, findsNothing);
-      expect(ssoLoginButton, findsNothing);
-      expect(rememberPassWordCheckBox, findsNothing);
+      expect(pageViewForHomeBanner, findsOneWidget);
     });
   });
 }
