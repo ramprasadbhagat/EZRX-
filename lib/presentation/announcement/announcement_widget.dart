0ffe366e55b6c346523457974e84f3b4d2f8cb62
Ujwal Vilas Chordiya <uvchordiya@zuelligpharma.com>
2022-10-17 10:24:36 +0000

    Merged PR 37625: Moved Announcements under safe area
    Moved Announcements under safe area

--- lib/presentation/announcement/announcement_widget.dart
+++ lib/presentation/announcement/announcement_widget.dart
@@ -10,80 +10,82 @@ class AnnouncementWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AnnouncementBloc, AnnouncementState>(
-      buildWhen: (previous, current) => previous != current,
-      builder: (_, state) {
-        if (!state.announcement.hasValidAnnouncement) {
-          return const SizedBox();
-        }
+    return SafeArea(
+      child: BlocBuilder<AnnouncementBloc, AnnouncementState>(
+        buildWhen: (previous, current) => previous != current,
+        builder: (_, state) {
+          if (!state.announcement.hasValidAnnouncement) {
+            return const SizedBox();
+          }
 
-        return Dismissible(
-          key: const Key('dismissibleAnnouncement'),
-          child: Card(
-            margin: const EdgeInsets.all(24.0),
-            color: ZPColors.announcementColor,
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 40,
-                  child: state.isLoading
-                      ? Container(
-                          key: const Key('announcementLoadingIndicator'),
-                          width: 30,
-                          height: 10,
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: const LoadingIndicator(
-                            indicatorType: Indicator.ballBeat,
-                            colors: [ZPColors.white],
+          return Dismissible(
+            key: const Key('dismissibleAnnouncement'),
+            child: Card(
+              margin: const EdgeInsets.symmetric(horizontal: 24.0),
+              color: ZPColors.announcementColor,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 40,
+                    child: state.isLoading
+                        ? Container(
+                            key: const Key('announcementLoadingIndicator'),
+                            width: 30,
+                            height: 10,
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: const LoadingIndicator(
+                              indicatorType: Indicator.ballBeat,
+                              colors: [ZPColors.white],
+                            ),
+                          )
+                        : IconButton(
+                            key: const Key('announcementReloadIcon'),
+                            onPressed: () {
+                              context.read<AnnouncementBloc>().add(
+                                    const AnnouncementEvent.getAnnouncement(),
+                                  );
+                            },
+                            padding: const EdgeInsets.only(right: 8.0),
+                            icon: const Icon(
+                              Icons.replay_outlined,
+                              color: ZPColors.white,
+                            ),
                           ),
-                        )
-                      : IconButton(
-                          key: const Key('announcementReloadIcon'),
-                          onPressed: () {
-                            context.read<AnnouncementBloc>().add(
-                                  const AnnouncementEvent.getAnnouncement(),
-                                );
-                          },
-                          padding: const EdgeInsets.only(right: 8.0),
-                          icon: const Icon(
-                            Icons.replay_outlined,
-                            color: ZPColors.white,
-                          ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        state.announcement.descriptionDisplay,
+                        key: const Key('announcementDescription'),
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          color: ZPColors.white,
                         ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(
-                      state.announcement.descriptionDisplay,
-                      key: const Key('announcementDescription'),
-                      textAlign: TextAlign.justify,
-                      style: const TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w500,
-                        color: ZPColors.white,
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  key: const Key('announcementCloseIcon'),
-                  onPressed: () {
-                    context
-                        .read<AnnouncementBloc>()
-                        .add(const AnnouncementEvent.close());
-                  },
-                  padding: const EdgeInsets.only(left: 8.0),
-                  icon: const Icon(
-                    Icons.close,
-                    color: ZPColors.white,
+                  IconButton(
+                    key: const Key('announcementCloseIcon'),
+                    onPressed: () {
+                      context
+                          .read<AnnouncementBloc>()
+                          .add(const AnnouncementEvent.close());
+                    },
+                    padding: const EdgeInsets.only(left: 8.0),
+                    icon: const Icon(
+                      Icons.close,
+                      color: ZPColors.white,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
