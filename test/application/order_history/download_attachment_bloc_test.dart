e3e1bc9f13f2acd36b41dec7b2cae26012f9dc84
Rawaha Muhammad Nazir <rmnazir@zuelligpharma.com>
2022-11-24 08:54:43 +0000

    Merged PR 40681: EZRX-1561 Order History Download Attachment Bloc 100% Coverage

---
+++ test/application/order_history/download_attachment_bloc_test.dart
@@ -0,0 +1,94 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/order/order_history_details/download_attachment/bloc/download_attachment_bloc.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/order/entities/order_history_details_po_document_buffer.dart';
+import 'package:ezrxmobile/infrastructure/order/repository/order_history_details_po_document_download_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockOrderHistoryDetailsPoDocumentRepository extends Mock
+    implements OrderHistoryDetailsPoDocumentRepository {}
+
+void main() {
+  group('OrderHistoryDetailsBloc should - ', () {
+    late DownloadAttachmentBloc downloadAttachmentBloc;
+    late OrderHistoryDetailsPoDocumentRepository downloadAttachmentRepository;
+    final emptyFile = Map<String, String>.from(
+      {},
+    );
+    final emptyResponseData = <OrderHistoryDetailsPoDocumentsBuffer>[];
+    const emptyFetchMode = FileFetchMode.none;
+    const apiServerTimeOut = ApiFailure.serverTimeout();
+    DownloadAttachmentBloc getDownloadAttachmentBloc() {
+      return DownloadAttachmentBloc(
+        downloadAttachmentRepository: downloadAttachmentRepository,
+      );
+    }
+
+    setUpAll(() {
+      downloadAttachmentRepository =
+          MockOrderHistoryDetailsPoDocumentRepository();
+      downloadAttachmentBloc = getDownloadAttachmentBloc();
+    });
+    test('have initial state as DownloadAttachmentState.initial()', () {
+      expect(
+        downloadAttachmentBloc.state,
+        DownloadAttachmentState.initial(),
+      );
+    });
+    blocTest<DownloadAttachmentBloc, DownloadAttachmentState>(
+      'emit correct data on initialized() event',
+      build: () => getDownloadAttachmentBloc(),
+      act: (bloc) => bloc.add(
+        const DownloadAttachmentEvent.initialized(),
+      ),
+      expect: () => [
+        DownloadAttachmentState.initial(),
+      ],
+    );
+    blocTest<DownloadAttachmentBloc, DownloadAttachmentState>(
+      'emit correct data on successful downloadFile() event',
+      setUp: () {
+        when(
+          () => downloadAttachmentRepository.downloadFiles(emptyFile),
+        ).thenAnswer((invocation) async => Right(emptyResponseData));
+      },
+      build: () => getDownloadAttachmentBloc(),
+      act: (bloc) => bloc.add(
+        DownloadAttachmentEvent.downloadFile(
+            files: emptyFile, fetchMode: emptyFetchMode),
+      ),
+      expect: () => [
+        DownloadAttachmentState.initial().copyWith(
+          fileFetchMode: emptyFetchMode,
+        ),
+        DownloadAttachmentState.initial().copyWith(
+          failureOrSuccessOption: optionOf(Right(emptyResponseData)),
+          fileData: emptyResponseData,
+        ),
+      ],
+    );
+    blocTest<DownloadAttachmentBloc, DownloadAttachmentState>(
+      'emit correct data on unsuccessful downloadFile() event',
+      setUp: () {
+        when(
+          () => downloadAttachmentRepository.downloadFiles(emptyFile),
+        ).thenAnswer((invocation) async => const Left(apiServerTimeOut));
+      },
+      build: () => getDownloadAttachmentBloc(),
+      act: (bloc) => bloc.add(
+        DownloadAttachmentEvent.downloadFile(
+            files: emptyFile, fetchMode: emptyFetchMode),
+      ),
+      expect: () => [
+        DownloadAttachmentState.initial().copyWith(
+          fileFetchMode: emptyFetchMode,
+        ),
+        DownloadAttachmentState.initial().copyWith(
+          failureOrSuccessOption: optionOf(const Left(apiServerTimeOut)),
+        ),
+      ],
+    );
+  });
+}
