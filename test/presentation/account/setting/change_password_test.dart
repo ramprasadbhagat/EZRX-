b61cf8aec4a140f17246a2cd181c6723f988eb89
Abdul Rehman <arehman@zuelligpharma.com>
2022-11-28 08:14:11 +0000

    Merged PR 40734: Change password widget tests
    Change password form  widget tests

--- test/presentation/account/setting/change_password_test.dart
+++ test/presentation/account/setting/change_password_test.dart
@@ -9,9 +9,9 @@ import 'package:ezrxmobile/domain/core/error/api_failures.dart';
 import 'package:ezrxmobile/infrastructure/auth/repository/change_password_repository.dart';
 import 'package:ezrxmobile/locator.dart';
 import 'package:ezrxmobile/presentation/account/change_password/change_password_page.dart';
-import 'package:ezrxmobile/presentation/account/settings_page.dart';
 import 'package:ezrxmobile/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -67,16 +67,6 @@ void main() {
       );
     }
 
-    testWidgets('Tap Change Password', (tester) async {
-      await getScopedWidget(const SettingsPage(), tester);
-      await tester.pump();
-      final changepasswordOnTap = find.byKey(const Key('changePasswordTile'));
-      expect(changepasswordOnTap, findsOneWidget);
-      await tester.tap(changepasswordOnTap);
-      await tester.pump();
-      expect(autoRouterMock.current.name, ChangePasswordPageRoute.name);
-    });
-
     testWidgets('Change Password Page', (tester) async {
       final expectedStates = [ResetPasswordState.initial()];
       when(() => mockUserBloc.state).thenReturn(UserState.initial());
@@ -99,73 +89,150 @@ void main() {
       expect(changePasswordButton, findsOneWidget);
     });
 
-    // TODO: Need Dipankar help
-    // testWidgets('Change Password Form', (tester) async {
-    //   final expectedStates = [ResetPasswordState.initial()];
-    //   when(() => mockUserBloc.state).thenReturn(UserState.initial());
-    //   when(() => resetPasswordBlocMock.state)
-    //       .thenReturn(ResetPasswordState.initial());
-    //   whenListen(resetPasswordBlocMock, Stream.fromIterable(expectedStates));
+    testWidgets('Change Password Form Tests', (WidgetTester tester) async {
+      final expectedStates = [ResetPasswordState.initial()];
+      when(() => mockUserBloc.state).thenReturn(UserState.initial());
+      when(() => resetPasswordBlocMock.state)
+          .thenReturn(ResetPasswordState.initial());
+      whenListen(resetPasswordBlocMock, Stream.fromIterable(expectedStates));
 
-    //   /// Finders for form fields ///
-    //   await getScopedWidget(const ChangePasswordPage(), tester);
-    //   final oldPasswordTextField =
-    //       find.byKey(const Key('oldPasswordTextField'));
-    //   final newPasswordTextField =
-    //       find.byKey(const Key('newPasswordTextField'));
-    //   final confirmPasswordTextField =
-    //       find.byKey(const Key('confirmPasswordTextField'));
-    //   expect(oldPasswordTextField, findsOneWidget);
-    //   expect(newPasswordTextField, findsOneWidget);
-    //   expect(confirmPasswordTextField, findsOneWidget);
-    //   await tester.pump();
+      /// Finders for form fields ///
+      await getScopedWidget(const ChangePasswordPage(), tester);
+      final oldPasswordTextField =
+          find.byKey(const Key('oldPasswordTextField'));
+      final newPasswordTextField =
+          find.byKey(const Key('newPasswordTextField'));
+      final confirmPasswordTextField =
+          find.byKey(const Key('confirmPasswordTextField'));
+      expect(oldPasswordTextField, findsOneWidget);
+      expect(newPasswordTextField, findsOneWidget);
+      expect(confirmPasswordTextField, findsOneWidget);
+      await tester.pump();
 
-    //   /// Password Fields Enter text validations ///
-    //   await tester.enterText(oldPasswordTextField, 'Auron@2022');
-    //   await tester.enterText(newPasswordTextField, 'Auron@2022!');
-    //   await tester.enterText(confirmPasswordTextField, 'Auron@2022!');
-    //   final oldPasswordText = find.text('Auron@2022');
-    //   final newPasswordText = find.text('Auron@2022');
-    //   final confirmPasswordText = find.text('Auron@2022');
-    //   expect(oldPasswordText, findsOneWidget);
-    //   expect(newPasswordText, findsOneWidget);
-    //   expect(confirmPasswordText, findsOneWidget);
-    //   await tester.pump();
+      /// Old Password Validation ///
+      final oldpasswordvalidationtextFinder = find.text('Old Password');
+      final oldvalidationMessageFinder = find.descendant(
+          of: oldPasswordTextField, matching: oldpasswordvalidationtextFinder);
+      expect(oldvalidationMessageFinder, findsOneWidget);
+      await tester.pump();
 
-    //   /// Old Password Validation ///
-    //   final oldpasswordvalidationtextFinder = find.text('Enter Old Password.');
-    //   final oldvalidationMessageFinder = find.descendant(
-    //       of: oldPasswordTextField, matching: oldpasswordvalidationtextFinder);
-    //   expect(oldvalidationMessageFinder, findsOneWidget);
-    //   await tester.pump();
-    //   final oldPasswordToggle = find.byKey(const Key('oldPasswordToggle'));
-    //   expect(oldPasswordToggle, findsOneWidget);
-    //   await tester.tap(oldPasswordToggle);
-    //   await tester.pump();
+      await tester.enterText(oldPasswordTextField, 'Auron@2022');
+      await tester.pump();
 
-    //   /// New Password Validations ///
-    //   final newpasswordvalidationtextFinder = find.text('Enter New Password.');
-    //   final newvalidationMessageFinder = find.descendant(
-    //       of: newPasswordTextField, matching: newpasswordvalidationtextFinder);
-    //   expect(newvalidationMessageFinder, findsOneWidget);
-    //   final newPasswordToggle = find.byKey(const Key('newPasswordToggle'));
-    //   expect(newPasswordToggle, findsOneWidget);
-    //   await tester.tap(newPasswordToggle);
-    //   await tester.pump();
+      final oldPasswordToggle = find.byKey(const Key('oldPasswordToggle'));
+      expect(oldPasswordToggle, findsOneWidget);
+      await tester.tap(oldPasswordToggle);
+      await tester.pump();
 
-    //   /// Confirm Password Validation ///
-    //   final confirmpasswordvalidationtextFinder =
-    //       find.text('Enter Confirm Password.');
-    //   final confirmvalidationMessageFinder = find.descendant(
-    //       of: confirmPasswordTextField,
-    //       matching: confirmpasswordvalidationtextFinder);
-    //   expect(confirmvalidationMessageFinder, findsOneWidget);
-    //   final confirmPasswordToggle =
-    //       find.byKey(const Key('confirmPasswordToggle'));
-    //   expect(confirmPasswordToggle, findsOneWidget);
-    //   await tester.tap(confirmPasswordToggle);
-    //   await tester.pump();
-    // });
+      final oldPassHintText = find.text('Auron@2022');
+      expect(oldPassHintText, findsOneWidget);
+
+      //await tester.sendKeyDownEvent(LogicalKeyboardKey.backspace);
+      //await tester.pump();
+
+      //expect(oldpasswordvalidationtextFinder, findsNWidgets(2));
+
+      // ..............New Password Validations..................
+      final newPasswordvalidationtextFinder = find.text('New Password');
+      final newPassvalidationMessageFinder = find.descendant(
+          of: newPasswordTextField, matching: newPasswordvalidationtextFinder);
+      expect(newPassvalidationMessageFinder, findsOneWidget);
+      await tester.pump();
+
+      await tester.enterText(newPasswordTextField, 'abc');
+
+      await tester.pumpAndSettle();
+      final upperCaseErrorMessage =
+          find.text('Contain at least 1 Upper case character (A to Z)'.tr());
+      await tester.pump();
+      expect(upperCaseErrorMessage, findsOneWidget);
+
+      await tester.sendKeyDownEvent(LogicalKeyboardKey.backspace);
+
+      final newPassErrorMessage = find.text('New Password.'.tr());
+      await tester.pump();
+      expect(newPassErrorMessage, findsNWidgets(2));
+
+      await tester.enterText(newPasswordTextField, 'ABC');
+      await tester.pump();
+      final lowerCaseErrorMessage =
+          find.text('Contain at least 1 Lower case character (a to z)'.tr());
+      expect(lowerCaseErrorMessage, findsOneWidget);
+
+      await tester.enterText(newPasswordTextField, 'Ab');
+      await tester.pump();
+      final numericErrorMessage =
+          find.text('Contain at least a numeric character (0 to 9)'.tr());
+      await tester.pump();
+      expect(numericErrorMessage, findsOneWidget);
+
+      await tester.enterText(newPasswordTextField, 'Ab2');
+      await tester.pump();
+      final specialCharErrorMessage = find.text(
+          'Contain at least one special character from the list (i.e. _ , # , ? , ! , @ , \$ , % , ^ , & , *, - )'
+              .tr());
+      await tester.pump();
+      expect(specialCharErrorMessage, findsOneWidget);
+
+      await tester.enterText(newPasswordTextField, 'Asdf!2');
+      await tester.pump();
+      final minLengthErrorMessage =
+          find.text('Minimum length of 10 characters'.tr());
+      await tester.pump();
+      expect(minLengthErrorMessage, findsOneWidget);
+
+
+      await tester.enterText(newPasswordTextField, 'Auron@2022!');
+      await tester.pump();
+
+      final newPasswordToggle = find.byKey(const Key('newPasswordToggle'));
+      expect(newPasswordToggle, findsOneWidget);
+      await tester.tap(newPasswordToggle);
+      await tester.pump();
+
+      final newPassHintText = find.text('Auron@2022!');
+      expect(newPassHintText, findsOneWidget);
+
+      // /// Confirm Password validations ///
+      final confirmPasswordvalidationtextFinder = find.text('Confirm Password');
+      final confirmPassvalidationMessageFinder = find.descendant(
+          of: confirmPasswordTextField,
+          matching: confirmPasswordvalidationtextFinder);
+      expect(confirmPassvalidationMessageFinder, findsOneWidget);
+      await tester.pump();
+
+      await tester.enterText(confirmPasswordTextField, 'Auron@2022!');
+      await tester.pump();
+
+      final confirmPasswordToggle =
+          find.byKey(const Key('confirmPasswordToggle'));
+      expect(confirmPasswordToggle, findsOneWidget);
+      await tester.tap(confirmPasswordToggle);
+      await tester.pump();
+
+      final confirmPassHintText = find.text('Auron@2022!');
+      expect(confirmPassHintText, findsNWidgets(2));
+      await tester.pump();
+
+      await tester.sendKeyDownEvent(LogicalKeyboardKey.backspace);
+      await tester.pump();
+      expect(confirmPasswordvalidationtextFinder, findsNWidgets(2));
+
+      await tester.enterText(oldPasswordTextField, 'Auron@2022');
+      await tester.enterText(newPasswordTextField, 'Auron@2022!');
+      await tester.enterText(confirmPasswordTextField, 'Auron@2023!');
+      final oldPasswordText = find.text('Auron@2022');
+      final newPasswordText = find.text('Auron@2022!');
+      final confirmPasswordText = find.text('Auron@2022!');
+      expect(oldPasswordText, findsOneWidget);
+      expect(newPasswordText, findsWidgets);
+      expect(confirmPasswordText, findsWidgets);
+
+      final mismatchErrorMessage = find.text('Password mismatch'.tr());
+      final validationMessageFinder = find.descendant(
+          of: confirmPasswordTextField, matching: mismatchErrorMessage);
+      expect(validationMessageFinder, findsNothing);
+    });
 
     testWidgets('Failure Change Password Button', (tester) async {
       final expectedfailureStates = [
@@ -189,7 +256,7 @@ void main() {
       await tester.tap(changePasswordButton);
     });
 
-    testWidgets('Sucess Change Password Button', (tester) async {
+    testWidgets('Success Change Password Button', (tester) async {
       final expectedSuccessStates = [
         ResetPasswordState.initial().copyWith(
             passwordResetFailureOrSuccessOption: optionOf(
