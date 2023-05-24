043f6a6f4f70e2ca070db0b2254626ee93691f27
Trinh Van Cong <tcong@zuelligpharma.com>
2023-05-24 13:56:04 +0000

    Merged PR 59336: Testcase/EZRX-5196: Add All Invoices test-cases for Payments section
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-5196
    
    ### TDD PR url
    - Not Required
    
    ### Root cause
    - Missing All Invoices test-cases for Payments section
    
    ### Solution
    - Add All Invoices test-cases for Payments section
    
    ### Test account
    - Username: ezrxtest05
    - Password: St@ysafe05
    - SalesOrg: 2601
    - Customer: 0030032223
    - ShipTo: 0070042688
    
    ### Output screenshot
    - Not Required
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases

---
+++ test/presentation/payments/all_invoices/all_invoices_filter_test.dart
@@ -0,0 +1,325 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:ezrxmobile/application/payments/all_invoices/filter/all_invoices_filter_bloc.dart';
+import 'package:ezrxmobile/config.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/value/value_transformers.dart';
+import 'package:ezrxmobile/domain/payments/entities/all_invoices_filter.dart';
+import 'package:ezrxmobile/domain/utils/string_utils.dart';
+import 'package:ezrxmobile/presentation/payments/all_invoices/filter_drawer.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/widget_utils.dart';
+
+class AllInvoicesFilterBlocMock
+    extends MockBloc<AllInvoicesFilterEvent, AllInvoicesFilterState>
+    implements AllInvoicesFilterBloc {}
+
+void main() {
+  late AllInvoicesFilterBloc allInvoicesFilterBlocMock;
+  late AppRouter autoRouterMock;
+  final locator = GetIt.instance;
+  final fakeToDate = DateTime.parse(
+    DateFormat('yyyy-MM-dd').format(
+      DateTime.now(),
+    ),
+  );
+
+  final fakeFromDate = DateTime.parse(
+    DateFormat('yyyy-MM-dd').format(
+      DateTime.now().subtract(
+        const Duration(days: 28),
+      ),
+    ),
+  );
+
+  setUpAll(() async {
+    locator.registerSingleton<Config>(Config()..appFlavor = Flavor.mock);
+    locator.registerLazySingleton(() => AppRouter());
+    autoRouterMock = locator<AppRouter>();
+  });
+
+  setUp(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    allInvoicesFilterBlocMock = AllInvoicesFilterBlocMock();
+    when(() => allInvoicesFilterBlocMock.state)
+        .thenReturn(AllInvoicesFilterState.initial());
+  });
+
+  Future getWidget(tester) async {
+    return tester.pumpWidget(
+      WidgetUtils.getScopedWidget(
+        autoRouterMock: autoRouterMock,
+        providers: [
+          BlocProvider<AllInvoicesFilterBloc>(
+            create: (context) => allInvoicesFilterBlocMock,
+          ),
+        ],
+        child: const AllInvoicesFilterDrawer(),
+      ),
+    );
+  }
+
+  group('All Invoices Filter Drawer Test', () {
+    testWidgets('=> Listen When condition', (tester) async {
+      final expectedState = [
+        AllInvoicesFilterState.initial().copyWith(
+          changed: true,
+        ),
+      ];
+      whenListen(allInvoicesFilterBlocMock, Stream.fromIterable(expectedState));
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('=> _FilterHeader Test', (tester) async {
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findFilterAllInvoicesText = find.text('Apply Filters'.tr());
+      expect(findFilterAllInvoicesText, findsOneWidget);
+
+      final findIconKey = find.byKey(const Key('filterCrossButton'));
+      expect(findIconKey, findsOneWidget);
+      await tester.tap(findIconKey);
+      await tester.pump();
+    });
+
+    testWidgets('=> _DueDateFilterState Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          dueDateFrom: DateTimeStringValue(
+            getDateStringByDateTime(fakeFromDate),
+          ),
+          dueDateTo: DateTimeStringValue(
+            getDateStringByDateTime(fakeToDate),
+          ),
+        ),
+      ));
+
+      final expectedState = [
+        AllInvoicesFilterState.initial().copyWith(
+          allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+            dueDateFrom: DateTimeStringValue(
+              getDateStringByDateTime(fakeFromDate),
+            ),
+            dueDateTo: DateTimeStringValue(
+              getDateStringByDateTime(fakeToDate),
+            ),
+          ),
+        ),
+      ];
+      whenListen(allInvoicesFilterBlocMock, Stream.fromIterable(expectedState));
+
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Due Date'.tr());
+      expect(findText, findsOneWidget);
+      final findTextField =
+          find.ancestor(of: findText, matching: find.byType(TextFormField));
+      await tester.tap(findTextField);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('SAVE'));
+      await tester.pumpAndSettle();
+
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          AllInvoicesFilterEvent.setDueDate(
+              DateTimeRange(start: fakeFromDate, end: fakeToDate)),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('_DocumentNumberByFilter test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        allInvoicesFilter: AllInvoicesFilter.empty(),
+      ));
+
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Document Number'.tr());
+      expect(findText, findsOneWidget);
+      await tester.enterText(
+          find.byKey(const Key('filterDocumentNumberField')), '1234567890');
+      await tester.pump();
+      expect(find.text('1234567899'), findsNothing);
+      expect(find.text('1234567890'), findsOneWidget);
+    });
+
+    testWidgets('=> _DocumentDateFilterState Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          documentDateFrom: DateTimeStringValue(
+            getDateStringByDateTime(fakeFromDate),
+          ),
+          documentDateTo: DateTimeStringValue(
+            getDateStringByDateTime(fakeToDate),
+          ),
+        ),
+      ));
+
+      final expectedState = [
+        AllInvoicesFilterState.initial().copyWith(
+          allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+            documentDateFrom: DateTimeStringValue(
+              getDateStringByDateTime(fakeFromDate),
+            ),
+            documentDateTo: DateTimeStringValue(
+              getDateStringByDateTime(fakeToDate),
+            ),
+          ),
+        ),
+      ];
+      whenListen(allInvoicesFilterBlocMock, Stream.fromIterable(expectedState));
+
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Document Date'.tr());
+      expect(findText, findsOneWidget);
+      final findTextField =
+          find.ancestor(of: findText, matching: find.byType(TextFormField));
+      await tester.tap(findTextField);
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('SAVE'));
+      await tester.pumpAndSettle();
+      
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          AllInvoicesFilterEvent.setDocumentDate(
+              DateTimeRange(start: fakeFromDate, end: fakeToDate)),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('=> _DebitValueToFilter Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        showErrorMessages: true,
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          debitValueTo: DoubleValue('12'),
+        ),
+      ));
+
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Debit Value To'.tr());
+      expect(findText, findsOneWidget);
+      final findTextField =
+          find.ancestor(of: findText, matching: find.byType(TextFormField));
+      await tester.enterText(findTextField, '123456');
+      await tester.pump();
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          AllInvoicesFilterEvent.debitValueToChanged(
+              StringUtils.formatter.format(double.parse('123456'))),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('=> _DebitValueFromFilter Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        showErrorMessages: true,
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          debitValueFrom: DoubleValue('12'),
+        ),
+      ));
+
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Debit Value From'.tr());
+      expect(findText, findsOneWidget);
+      final findTextField =
+          find.ancestor(of: findText, matching: find.byType(TextFormField));
+      await tester.enterText(findTextField, '12');
+      await tester.pump();
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          AllInvoicesFilterEvent.debitValueFromChanged(
+              StringUtils.formatter.format(double.parse('12'))),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('=> _ApplyButton Button Click Test', (tester) async {
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Apply'.tr());
+      expect(findText, findsOneWidget);
+      final findElevatedButton =
+          find.ancestor(of: findText, matching: find.byType(ElevatedButton));
+      await tester.tap(findElevatedButton);
+      await tester.pump();
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          const AllInvoicesFilterEvent.applyFilters(),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('=> _ClearButton Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        showErrorMessages: false,
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          debitValueFrom: DoubleValue('12'),
+        ),
+      ));
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Clear'.tr());
+      expect(findText, findsOneWidget);
+      final findOutlineButton = find.byKey(const Key('filterClearButton'));
+      await tester.tap(findOutlineButton);
+      await tester.pump();
+      verify(
+        () => allInvoicesFilterBlocMock.add(
+          const AllInvoicesFilterEvent.clearFilters(),
+        ),
+      ).called(1);
+    });
+
+    testWidgets(
+        '=> DebitValueError Test when debitValueTo greater than debitValueFrom',
+        (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        showErrorMessages: true,
+        allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+          debitValueFrom: DoubleValue('15'),
+          debitValueTo: DoubleValue('12'),
+        ),
+      ));
+      await getWidget(tester);
+      await tester.pumpAndSettle();
+
+      final findText = find.text('Apply'.tr());
+      expect(findText, findsOneWidget);
+      final findElevatedButton =
+          find.ancestor(of: findText, matching: find.byType(ElevatedButton));
+      await tester.tap(findElevatedButton);
+      await tester.pumpAndSettle();
+
+      final findInvalidText = find.text('Invalid Debit Value Range!'.tr());
+      expect(findInvalidText, findsOneWidget);
+      await tester.pump();
+    });
+  });
+}
---
+++ test/presentation/payments/all_invoices/all_invoices_test.dart
@@ -0,0 +1,418 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:ezrxmobile/application/account/customer_code/customer_code_bloc.dart';
+import 'package:ezrxmobile/application/account/sales_org/sales_org_bloc.dart';
+import 'package:ezrxmobile/application/account/ship_to_code/ship_to_code_bloc.dart';
+import 'package:ezrxmobile/application/account/user/user_bloc.dart';
+import 'package:ezrxmobile/application/announcement/announcement_bloc.dart';
+import 'package:ezrxmobile/application/auth/auth_bloc.dart';
+import 'package:ezrxmobile/application/payments/all_invoices/all_invoices_bloc.dart';
+import 'package:ezrxmobile/application/payments/all_invoices/filter/all_invoices_filter_bloc.dart';
+import 'package:ezrxmobile/config.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/account/entities/ship_to_address.dart';
+import 'package:ezrxmobile/domain/account/entities/ship_to_info.dart';
+import 'package:ezrxmobile/domain/account/entities/ship_to_name.dart';
+import 'package:ezrxmobile/domain/account/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/domain/payments/entities/all_invoices_filter.dart';
+import 'package:ezrxmobile/domain/payments/entities/credit_and_invoice_item.dart';
+import 'package:ezrxmobile/infrastructure/core/mixpanel/mixpanel_service.dart';
+import 'package:ezrxmobile/presentation/core/attention_row.dart';
+import 'package:ezrxmobile/presentation/core/filter_icon.dart';
+import 'package:ezrxmobile/presentation/payments/all_invoices/all_invoices.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/widget_utils.dart';
+import '../../order_history/order_history_details_widget_test.dart';
+
+class AllInvoicesBlocMock extends MockBloc<AllInvoicesEvent, AllInvoicesState>
+    implements AllInvoicesBloc {}
+
+class AllInvoicesFilterBlocMock
+    extends MockBloc<AllInvoicesFilterEvent, AllInvoicesFilterState>
+    implements AllInvoicesFilterBloc {}
+
+class CustomerCodeBlocMock
+    extends MockBloc<CustomerCodeEvent, CustomerCodeState>
+    implements CustomerCodeBloc {}
+
+class ShipToCodeBlocMock extends MockBloc<ShipToCodeEvent, ShipToCodeState>
+    implements ShipToCodeBloc {}
+
+class UserBlocMock extends MockBloc<UserEvent, UserState> implements UserBloc {}
+
+class SalesOrgBlocMock extends MockBloc<SalesOrgEvent, SalesOrgState>
+    implements SalesOrgBloc {}
+
+class AnnouncementBlocMock
+    extends MockBloc<AnnouncementEvent, AnnouncementState>
+    implements AnnouncementBloc {}
+
+class AuthBlocMock extends MockBloc<AuthEvent, AuthState> implements AuthBloc {}
+
+void main() {
+  late AllInvoicesBloc allInvoicesBlocMock;
+  late AllInvoicesFilterBloc allInvoicesFilterBlocMock;
+  late CustomerCodeBloc customerCodeBlocMock;
+  late ShipToCodeBloc shipToCodeBlocMock;
+  late UserBloc userBlocMock;
+  late SalesOrgBloc salesOrgBlocMock;
+  late AppRouter autoRouterMock;
+  final locator = GetIt.instance;
+  late AuthBloc authBlocMock;
+  late AnnouncementBloc announcementBlocMock;
+
+  setUpAll(() async {
+    locator.registerSingleton<Config>(Config()..appFlavor = Flavor.mock);
+    locator.registerLazySingleton(() => AppRouter());
+    locator.registerLazySingleton(() => MixpanelService());
+    autoRouterMock = locator<AppRouter>();
+    locator<MixpanelService>().init(mixpanel: MixpanelMock());
+  });
+
+  setUp(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    allInvoicesBlocMock = AllInvoicesBlocMock();
+    allInvoicesFilterBlocMock = AllInvoicesFilterBlocMock();
+    customerCodeBlocMock = CustomerCodeBlocMock();
+    shipToCodeBlocMock = ShipToCodeBlocMock();
+    userBlocMock = UserBlocMock();
+    salesOrgBlocMock = SalesOrgBlocMock();
+    authBlocMock = AuthBlocMock();
+    announcementBlocMock = AnnouncementBlocMock();
+
+    when(() => allInvoicesBlocMock.state)
+        .thenReturn(AllInvoicesState.initial());
+    when(() => allInvoicesFilterBlocMock.state)
+        .thenReturn(AllInvoicesFilterState.initial());
+    when(() => customerCodeBlocMock.state)
+        .thenReturn(CustomerCodeState.initial());
+    when(() => shipToCodeBlocMock.state).thenReturn(ShipToCodeState.initial());
+    when(() => userBlocMock.state).thenReturn(UserState.initial());
+    when(() => salesOrgBlocMock.state).thenReturn(SalesOrgState.initial());
+    when(() => authBlocMock.state).thenReturn(const AuthState.initial());
+    when(() => announcementBlocMock.state)
+        .thenReturn(AnnouncementState.initial());
+  });
+
+  Future getWidget(tester) async {
+    return tester.pumpWidget(
+      WidgetUtils.getScopedWidget(
+        autoRouterMock: autoRouterMock,
+        providers: [
+          BlocProvider<AllInvoicesBloc>(
+            create: (context) => allInvoicesBlocMock,
+          ),
+          BlocProvider<AllInvoicesFilterBloc>(
+            create: (context) => allInvoicesFilterBlocMock,
+          ),
+          BlocProvider<CustomerCodeBloc>(
+            create: (context) => customerCodeBlocMock,
+          ),
+          BlocProvider<ShipToCodeBloc>(
+            create: (context) => shipToCodeBlocMock,
+          ),
+          BlocProvider<UserBloc>(
+            create: (context) => userBlocMock,
+          ),
+          BlocProvider<SalesOrgBloc>(
+            create: (context) => salesOrgBlocMock,
+          ),
+          BlocProvider<AuthBloc>(create: (context) => authBlocMock),
+          BlocProvider<AnnouncementBloc>(
+              create: (context) => announcementBlocMock),
+        ],
+        child: const AllInvoicesPage(),
+      ),
+    );
+  }
+
+  group('All Invoices Screen', () {
+    testWidgets('=> AppBar Test', (tester) async {
+      when(() => allInvoicesBlocMock.state)
+          .thenReturn(AllInvoicesState.initial().copyWith(
+        isLoading: true,
+      ));
+      final expectedState = [
+        AllInvoicesFilterState.initial(),
+        AllInvoicesFilterState.initial().copyWith(
+          changed: true,
+        ),
+        AllInvoicesFilterState.initial().copyWith(
+          allInvoicesFilter: AllInvoicesFilter.empty(),
+        ),
+      ];
+      whenListen(allInvoicesFilterBlocMock, Stream.fromIterable(expectedState));
+
+      await getWidget(tester);
+
+      await tester.pump(const Duration(milliseconds: 100));
+      final filterButton = find.byType(FilterCountButton);
+      expect(filterButton, findsOneWidget);
+      await tester.tap(filterButton);
+      final allInvoicesText = find.text('All Invoices'.tr());
+      expect(allInvoicesText, findsOneWidget);
+
+      final statusText = find.textContaining('Status'.tr());
+      expect(statusText, findsOneWidget);
+      await tester.tap(statusText, warnIfMissed: false);
+      await tester.pump();
+    });
+
+    testWidgets('=> Body Test when loading', (tester) async {
+      when(() => customerCodeBlocMock.state)
+          .thenReturn(CustomerCodeState.initial().copyWith(
+        customerCodeInfo: CustomerCodeInfo.empty().copyWith(
+          customerCodeSoldTo: 'mock-customerCodeSoldTo',
+        ),
+      ));
+      when(() => shipToCodeBlocMock.state)
+          .thenReturn(ShipToCodeState.initial().copyWith(
+        shipToInfo: ShipToInfo.empty().copyWith(
+          shipToCustomerCode: 'mock-shipToCustomerCode',
+        ),
+      ));
+      when(() => allInvoicesBlocMock.state)
+          .thenReturn(AllInvoicesState.initial().copyWith(
+        isLoading: true,
+      ));
+
+      final expectedState = [
+        AllInvoicesFilterState.initial().copyWith(
+          changed: true,
+        ),
+        AllInvoicesFilterState.initial().copyWith(
+          changed: true,
+          allInvoicesFilter: AllInvoicesFilter.empty(),
+        ),
+      ];
+      whenListen(allInvoicesFilterBlocMock, Stream.fromIterable(expectedState));
+
+      await getWidget(tester);
+      await tester.pump(const Duration(milliseconds: 100));
+
+      final loaderImage = find.byKey(const Key('LoaderImage'));
+      expect(loaderImage, findsOneWidget);
+      await tester.pump();
+    });
+
+    testWidgets('=> Body Test onRefresh', (tester) async {
+      when(() => allInvoicesBlocMock.state)
+          .thenReturn(AllInvoicesState.initial());
+      when(() => shipToCodeBlocMock.state)
+          .thenReturn(ShipToCodeState.initial().copyWith(
+        shipToInfo: ShipToInfo(
+            defaultShipToAddress: true,
+            shipToCustomerCode: '',
+            shipToName: ShipToName.empty(),
+            shipToAddress: ShipToAddress.empty(),
+            status: Status(''),
+            building: '',
+            city1: '',
+            city2: '',
+            postalCode: '',
+            houseNumber1: '',
+            telephoneNumber: '',
+            region: '',
+            floor: '',
+            plant: '',
+            licenses: [],
+            country: ''),
+      ));
+
+      final expectedState = [
+        AllInvoicesState.initial().copyWith(
+          isLoading: true,
+        ),
+        AllInvoicesState.initial().copyWith(
+          isLoading: true,
+          failureOrSuccessOption:
+              optionOf(const Left(ApiFailure.other('mock-error'))),
+        ),
+        AllInvoicesState.initial().copyWith(
+          isLoading: true,
+          failureOrSuccessOption: none(),
+        ),
+        AllInvoicesState.initial().copyWith(
+          isLoading: false,
+          failureOrSuccessOption: optionOf(const Right('')),
+          invoices: [
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456789',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            )
+          ],
+          totalCount: 1,
+        ),
+      ];
+      whenListen(allInvoicesBlocMock, Stream.fromIterable(expectedState));
+
+      final handle = tester.ensureSemantics();
+      await getWidget(tester);
+      await tester.pump(const Duration(milliseconds: 100));
+
+      await tester.drag(
+        find.textContaining('123456789'),
+        const Offset(0.0, 1000.0),
+      );
+      await tester.pump(const Duration(seconds: 1));
+      expect(
+        tester.getSemantics(find.byType(RefreshProgressIndicator)),
+        matchesSemantics(
+          label: 'Refresh',
+        ),
+      );
+
+      await tester
+          .pump(const Duration(seconds: 1)); // finish the scroll animation
+      await tester.pump(
+          const Duration(seconds: 1)); // finish the indicator settle animation
+      await tester.pump(
+          const Duration(seconds: 1)); // finish the indicator hide animation
+
+      handle.dispose();
+      await tester.pump();
+    });
+
+    testWidgets('=> Body Test onLoadMore', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial());
+      when(() => allInvoicesBlocMock.state).thenReturn(
+        AllInvoicesState.initial().copyWith(
+          isLoading: false,
+          canLoadMore: true,
+          invoices: [
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+            CreditAndInvoiceItem.empty().copyWith(
+              accountingDocument: '123456780',
+              netDueDate: DateTimeStringValue('2023-12-25'),
+              documentDate: DateTimeStringValue('2023-12-25'),
+              amountInTransactionCurrency: 15.72,
+              invoiceProcessingStatus: 'Cleared',
+            ),
+          ],
+          totalCount: 6,
+        ),
+      );
+
+      await getWidget(tester);
+      await tester.drag(
+        find.textContaining('123456780').last,
+        const Offset(0.0, -1000.0),
+      );
+      await tester.pump(const Duration(seconds: 1));
+      await tester
+          .pump(const Duration(seconds: 1)); // finish the scroll animation
+      await tester.pump(
+          const Duration(seconds: 1)); // finish the indicator settle animation
+      await tester.pump(
+          const Duration(seconds: 1)); // finish the indicator hide animation
+      await tester.pump();
+    });
+    testWidgets('=> All Invoices AttentionRow visible', (tester) async {
+      when(() => allInvoicesBlocMock.state)
+          .thenReturn(AllInvoicesState.initial());
+
+      await getWidget(tester);
+      await tester.pump();
+
+      await tester.pump(const Duration(milliseconds: 100));
+      final attentionRow = find.byType(AttentionRow);
+      expect(attentionRow, findsOneWidget);
+    });
+
+    testWidgets('=> All Invoices Filter By Status Test', (tester) async {
+      when(() => allInvoicesFilterBlocMock.state)
+          .thenReturn(AllInvoicesFilterState.initial().copyWith(
+        statuses: ['Overdue', 'Open', 'Cleared', 'Outstanding'],
+      ));
+      when(() => allInvoicesBlocMock.state)
+          .thenReturn(AllInvoicesState.initial());
+
+      await getWidget(tester);
+      await tester.pump();
+
+      final statusFilterButton = find.byKey(const Key('statusFilterButton'));
+      expect(statusFilterButton, findsOneWidget);
+      await tester.tap(statusFilterButton);
+      await tester.pump(const Duration(seconds: 1));
+
+      final statusFilter =
+          find.byKey(const ValueKey('all_invoices_filter_by_status'));
+      expect(statusFilter, findsOneWidget);
+
+      final findRadioListText = find.text('Overdue');
+      await tester.tap(findRadioListText);
+      await tester.pump();
+
+      final closeButton = find.byKey(const Key('closeButton'));
+      expect(closeButton, findsOneWidget);
+      await tester.tap(closeButton);
+      await tester.pump();
+
+      final expectedState = [
+        AllInvoicesFilterState.initial().copyWith(
+          allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+            filterStatus: 'Cleared',
+          ),
+        ),
+        AllInvoicesFilterState.initial().copyWith(
+          allInvoicesFilter: AllInvoicesFilter.empty().copyWith(
+            filterStatus: 'Open',
+          ),
+        ),
+      ];
+      whenListen(
+        allInvoicesFilterBlocMock,
+        Stream.fromIterable(expectedState),
+      );
+      await tester.tap(statusFilterButton);
+      await tester.pump();
+    });
+  });
+}
