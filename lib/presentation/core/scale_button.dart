45cfcbbfab274d8fe55d3cb1465122faae16980a
Auron Choo <JHChoo@zuelligpharma.com>
2023-06-15 08:30:35 +0000

    Merged PR 63444: Feature/EZRX-6379 Add scale button
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-6379
    
    ### Root cause
    - We need a button can expand when on the top of page, collapse when no on the top of page
    
    ### Solution
    - Create a scale button to read scrollcontroller and support the usage
    
    ### Output screenshot
    - [Screen Recording 2023-06-12 at 10.36.46 PM.mov](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/62895/attachments/Screen%20Recording%202023-06-12%20at%2010.36.46%20PM.mov)

---
+++ lib/presentation/core/scale_button.dart
@@ -0,0 +1,97 @@
+//ignore_for_file: unused-code
+//ignore_for_file: unused-class
+//ignore_for_file: unused-files
+import 'package:ezrxmobile/presentation/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class ScaleButton extends StatefulWidget {
+  final GestureTapCallback onPress;
+  final IconData icon;
+  final String label;
+  final ScrollController scrollController;
+
+  const ScaleButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onPress,
+    required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  State<ScaleButton> createState() => _ScaleButtonState();
+}
+
+class _ScaleButtonState extends State<ScaleButton> {
+  bool isExpanded = true;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(_scrollListener);
+    widget.scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    setState(() => isExpanded = widget.scrollController.position.pixels == 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FloatingActionButton.extended(
+          extendedPadding: const EdgeInsets.only(left: 12, right: 12),
+          onPressed: widget.onPress,
+          label: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(
+                opacity: animation,
+                child: SizeTransition(
+                  sizeFactor: animation,
+                  axis: Axis.horizontal,
+                  child: child,
+                ),
+              );
+            },
+            child: isExpanded
+                ? Row(
+                    children: [
+                      Icon(widget.icon),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Text(widget.label),
+                      ),
+                    ],
+                  )
+                : Icon(widget.icon),
+          ),
+        ),
+        isExpanded
+            ? const SizedBox.shrink()
+            : FloatingActionButton.small(
+                backgroundColor: ZPColors.lightYellow,
+                onPressed: () {
+                  widget.scrollController.position.animateTo(
+                    widget.scrollController.position.minScrollExtent,
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: const Icon(
+                  Icons.expand_less_outlined,
+                  color: ZPColors.black,
+                ),
+              ),
+      ],
+    );
+  }
+}
