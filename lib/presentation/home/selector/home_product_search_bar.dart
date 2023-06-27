bd7bdfbbc925f816a93e8663d8811296e4ec039b
Rawaha Muhammad Nazir <rmnazir@zuelligpharma.com>
2023-06-27 13:07:26 +0000

    Merged PR 65543: Fix/EZRX-7018 -  When user clicks on search bar they are navigated to product search page
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-7018
    
    ### Root cause
    - We use `ProductSearchBar` in `HomeTab` however, the business requirement does not satisfy this usage.
    
    ### Solution
    - Introduce a new `TextFormField` (we used a this to make sure it looks exactly like a search bar) which when clicked, navigates user to `ProductSuggestionPage`.
    
    ### Test account
    - username: rootadmin
    - salesOrg: any
    - customer: any
    - shipto: any
    - material/setup: any

--- lib/presentation/home/selector/home_product_search_bar.dart
+++ lib/presentation/home/selector/home_product_search_bar.dart
@@ -1,44 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:ezrxmobile/application/account/customer_code/customer_code_bloc.dart';
-import 'package:ezrxmobile/application/account/sales_org/sales_org_bloc.dart';
-import 'package:ezrxmobile/application/order/product_search/product_search_bloc.dart';
-import 'package:ezrxmobile/domain/utils/error_utils.dart';
 import 'package:ezrxmobile/presentation/core/widget_keys.dart';
-import 'package:ezrxmobile/presentation/products/product_search_bar.dart';
 import 'package:ezrxmobile/presentation/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeProductSearchBar extends StatefulWidget {
+class HomeProductSearchBar extends StatelessWidget {
   const HomeProductSearchBar({Key? key}) : super(key: key);
-
-  @override
-  State<HomeProductSearchBar> createState() => HomeProductSearchBarState();
-}
-
-class HomeProductSearchBarState extends State<HomeProductSearchBar> {
-  final _searchController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _searchController.value = TextEditingValue(
-      text: context
-          .read<ProductSearchBloc>()
-          .state
-          .searchKey
-          .getOrDefaultValue(''),
-      selection: TextSelection.collapsed(
-        offset: _searchController.selection.base.offset,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,85 +35,26 @@ class HomeProductSearchBarState extends State<HomeProductSearchBar> {
           padding: const EdgeInsets.symmetric(
             horizontal: 16.0,
           ),
-          child: BlocConsumer<ProductSearchBloc, ProductSearchState>(
-            buildWhen: (previous, current) =>
-                previous.searchKey != current.searchKey ||
-                previous.isSearching != current.isSearching,
-            listenWhen: (previous, current) =>
-                previous.apiFailureOrSuccessOption !=
-                    current.apiFailureOrSuccessOption ||
-                previous.searchKey != current.searchKey,
-            listener: (context, state) {
-              state.apiFailureOrSuccessOption.fold(
-                () {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                },
-                (either) => either.fold(
-                  (failure) {
-                    ErrorUtils.handleApiFailure(context, failure);
-                  },
-                  (_) {},
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Search by product name or code'.tr(),
+              prefixIcon: const Icon(
+                Icons.search,
+                size: 20,
+              ),
+              suffixIcon: IconButton(
+                splashRadius: 1,
+                key: WidgetKeys.productScanCameraKey,
+                icon: const Icon(
+                  Icons.camera_alt_outlined,
                 ),
-              );
-              final searchText = state.searchKey.getOrDefaultValue('');
-              _searchController.value = TextEditingValue(
-                text: searchText,
-                selection: TextSelection.collapsed(
-                  offset: _searchController.selection.base.offset,
-                ),
-              );
-            },
-            builder: (context, state) {
-              return ProductSearchBar(
-                hintText: 'Search by product name or code'.tr(),
-                prefixIcon: const Icon(
-                  Icons.search,
-                  size: 20,
-                ),
-                enabled: true,
-                onClear: () => context.read<ProductSearchBloc>().add(
-                      const ProductSearchEvent.clearSearch(),
-                    ),
-                border: const OutlineInputBorder(),
-                customValidator: () => true,
-                onSearchChanged: (value) {
-                  context.read<ProductSearchBloc>().add(
-                        ProductSearchEvent.autoSearchProduct(
-                          salesOrganization: context
-                              .read<SalesOrgBloc>()
-                              .state
-                              .salesOrganisation,
-                          configs: context.read<SalesOrgBloc>().state.configs,
-                          customerCodeInfo: context
-                              .read<CustomerCodeBloc>()
-                              .state
-                              .customerCodeInfo,
-                          shipToInfo:
-                              context.read<CustomerCodeBloc>().state.shipToInfo,
-                          searchKey: value,
-                        ),
-                      );
-                },
-                onSearchSubmitted: (value) {
-                  context.read<ProductSearchBloc>().add(
-                        ProductSearchEvent.searchProduct(
-                          salesOrganization: context
-                              .read<SalesOrgBloc>()
-                              .state
-                              .salesOrganisation,
-                          configs: context.read<SalesOrgBloc>().state.configs,
-                          customerCodeInfo: context
-                              .read<CustomerCodeBloc>()
-                              .state
-                              .customerCodeInfo,
-                          shipToInfo:
-                              context.read<CustomerCodeBloc>().state.shipToInfo,
-                          searchKey: value,
-                        ),
-                      );
-                },
-                controller: _searchController,
-              );
+                onPressed: () => {},
+              ),
+              border: const OutlineInputBorder(),
+            ),
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+              context.router.pushNamed('product_suggestion_page');
             },
           ),
         ),
