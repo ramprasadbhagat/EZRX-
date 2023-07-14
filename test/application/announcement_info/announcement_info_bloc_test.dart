b94cc1d937f53c7b4baf0175b9b21b5c3576e364
Joseph Stalin <jstalin@zuelligpharma.com>
2023-07-14 14:37:40 +0000

    Merged PR 68626: Testcase/EZRX-7690: Test coverage for announcement info bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-7690
    
    ### TDD PR url
    - Not required
    
    ### Root cause
    - Test coverage for announcement info bloc
    
    ### Solution
    - Test coverage for announcement info bloc
    
    ###Output screenshot
    ![Screenshot 2023-07-13 at 2.29.51 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/68626/attachments/Screenshot%202023-07-13%20at%202.29.51%20PM.png)
    
    EZRX-7690 Test coverage for announcement info bloc

---
+++ test/application/announcement_info/announcement_info_bloc_test.dart
@@ -0,0 +1,87 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/announcement_info/announcement_info_bloc.dart';
+import 'package:ezrxmobile/domain/account/value/value_objects.dart';
+import 'package:ezrxmobile/domain/announcement_info/entities/announcement_info.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/infrastructure/announcement_info/datasource/announcement_info_local.dart';
+import 'package:ezrxmobile/infrastructure/announcement_info/repository/announcement_info_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class AnnouncementInfoRepositoryRepo extends Mock
+    implements AnnouncementInfoRepository {}
+
+const _pageSize = 24;
+
+void main() {
+  late AnnouncementInfoRepositoryRepo repository;
+  late AnnouncementInfo announcementInfoMock;
+  final announcementInfoState = AnnouncementInfoState.initial();
+  final salesOrg = SalesOrg('');
+
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    repository = AnnouncementInfoRepositoryRepo();
+
+    announcementInfoMock =
+        await AnnouncementInfoLocalDataSource().getAnnouncementInfo();
+  });
+
+  group('Announcement Info Bloc', () {
+    blocTest(
+      'Get announcement info fail',
+      build: () => AnnouncementInfoBloc(announcementInfoRepository: repository),
+      setUp: () {
+        when(
+          () => repository.getAnnouncement(
+            salesOrg: salesOrg,
+            pageSize: _pageSize,
+          ),
+        ).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('fake-error'),
+          ),
+        );
+      },
+      act: (AnnouncementInfoBloc bloc) => bloc
+        ..add(AnnouncementInfoEvent.getAnnouncement(
+          salesOrg: salesOrg,
+          pageSize: _pageSize,
+        )),
+      expect: () => [
+        announcementInfoState.copyWith(isLoading: true),
+        announcementInfoState,
+      ],
+    );
+
+    blocTest(
+      'Get announcement info Success',
+      build: () => AnnouncementInfoBloc(announcementInfoRepository: repository),
+      setUp: () {
+        when(
+          () => repository.getAnnouncement(
+            salesOrg: salesOrg,
+            pageSize: _pageSize,
+          ),
+        ).thenAnswer(
+          (invocation) async => Right(
+            announcementInfoMock,
+          ),
+        );
+      },
+      act: (AnnouncementInfoBloc bloc) => bloc
+        ..add(AnnouncementInfoEvent.getAnnouncement(
+          salesOrg: salesOrg,
+          pageSize: _pageSize,
+        )),
+      expect: () => [
+        announcementInfoState.copyWith(isLoading: true),
+        announcementInfoState.copyWith(
+          announcementInfo: announcementInfoMock,
+        ),
+      ],
+    );
+  });
+}
