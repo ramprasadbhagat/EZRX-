c6c480457979db0ed2c21425f5ee65832b1b2b10
Jyoti Sharma <JSharma@zuelligpharma.com>
2023-07-14 14:14:55 +0000

    Merged PR 68906: Fix/EZRX-7801-mobile-v-3-wrong-display-of-name-in-profile
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-7801
    
    ### TDD PR url
    - https://dev.azure.com/zuelligpharmadevops/eZRx%20Mobile/_git/eZRx%20Mobile/pullrequest/68994
    
    ### Root cause
    - Wrong display of Name in Profile
    
    ### Solution
    - Displaying userFullName instate of userRollName
    
    ### Test account
    - username: own ID
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Simulator Screenshot - iPhone 14 Pro Max - 2023-07-14 at 14.46.02.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/68906/attachments/Simulator%20Screenshot%20-%20iPhone%2014%20Pro%20Max%20-%202023-07-14%20at%2014.46.02.png =x200)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases
    
    EZRX-7801 fixed Wrong display of Name in Profile

--- lib/presentation/more/section/profile_tile_section.dart
+++ lib/presentation/more/section/profile_tile_section.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:ezrxmobile/application/account/user/user_bloc.dart';
 import 'package:ezrxmobile/domain/utils/error_utils.dart';
 import 'package:ezrxmobile/presentation/core/loading_shimmer/loading_shimmer.dart';
@@ -45,7 +46,7 @@ class ProfileTile extends StatelessWidget {
           ),
           title: state.isNotEmpty
               ? Text(
-                  ('Hello, ${state.userRoleName}'),
+                  '${'Hello'.tr()}, ${state.userFullName.displayFullName}',
                   style: Theme.of(context).textTheme.labelMedium,
                 )
               : LoadingShimmer.tile(line: 3),
---
+++ test/presentation/more/profile_tile_section_test.dart
@@ -0,0 +1,114 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization_loader/easy_localization_loader.dart';
+import 'package:ezrxmobile/application/account/user/user_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/full_name.dart';
+import 'package:ezrxmobile/domain/account/entities/user.dart';
+import 'package:ezrxmobile/presentation/core/widget_keys.dart';
+import 'package:ezrxmobile/presentation/more/section/profile_tile_section.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../utils/widget_utils.dart';
+
+class UserBlocMock extends MockBloc<UserEvent, UserState> implements UserBloc {}
+
+class AutoRouterMock extends Mock implements AppRouter {}
+
+final locator = GetIt.instance;
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  late UserBloc userBlocMock;
+  late AppRouter autoRouterMock;
+  setUpAll(() {
+    locator.registerLazySingleton(() => AppRouter());
+  });
+  group('Profile tile section Screen', () {
+    setUp(() {
+      userBlocMock = UserBlocMock();
+      autoRouterMock = locator<AppRouter>();
+      when(() => userBlocMock.state).thenReturn(UserState.initial());
+    });
+    Widget getScopedWidget() {
+      return EasyLocalization(
+        supportedLocales: const [
+          Locale('en'),
+        ],
+        path: 'assets/langs/langs.csv',
+        startLocale: const Locale('en'),
+        fallbackLocale: const Locale('en'),
+        saveLocale: true,
+        useOnlyLangCode: true,
+        assetLoader: CsvAssetLoader(),
+        child: WidgetUtils.getScopedWidget(
+          autoRouterMock: autoRouterMock,
+          providers: [
+            BlocProvider<UserBloc>(create: (context) => userBlocMock),
+          ],
+          child: const Material(child: ProfileTile()),
+        ),
+      );
+    }
+
+    testWidgets(
+      'Test Profile tile length more than or equal to 1',
+      (tester) async {
+        final expectedStates = [
+          UserState.initial().copyWith(
+            user: User.empty().copyWith(
+              id: 'testId',
+              fullName: const FullName(firstName: 'test', lastName: 'test'),
+            ),
+          ),
+        ];
+        whenListen(userBlocMock, Stream.fromIterable(expectedStates));
+        await tester.pumpWidget(getScopedWidget());
+        await tester.pump();
+        final findProfileTileKey = find.byKey(WidgetKeys.profileTile);
+        expect(findProfileTileKey, findsOneWidget);
+      },
+    );
+    testWidgets(
+      'Test Profile tile returns empty name',
+      (tester) async {
+        final expectedStates = [
+          UserState.initial().copyWith(
+            user: User.empty().copyWith(
+              id: 'testId',
+            ),
+          ),
+        ];
+        whenListen(userBlocMock, Stream.fromIterable(expectedStates));
+        await tester.pumpWidget(getScopedWidget());
+        final findProfileTileKey = find.byKey(WidgetKeys.profileTile);
+        expect(findProfileTileKey, findsOneWidget);
+      },
+    );
+    testWidgets(
+      'Test Profile Name',
+      (tester) async {
+        final expectedStates = [
+          UserState.initial().copyWith(
+            user: User.empty().copyWith(
+              id: 'testId',
+              fullName: const FullName(firstName: 'test', lastName: 'test'),
+            ),
+          ),
+        ];
+        whenListen(userBlocMock, Stream.fromIterable(expectedStates));
+        await tester.pumpWidget(getScopedWidget());
+        await tester.pump();
+        final findProfileName = find.text(
+          '${'Hello'.tr()}, ${const FullName(firstName: 'test', lastName: 'test').displayFullName}',
+        );
+        expect(findProfileName, findsOneWidget);
+      },
+    );
+  });
+}
