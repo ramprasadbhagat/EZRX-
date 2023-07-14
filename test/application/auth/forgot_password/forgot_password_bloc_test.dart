daf8d189ed6afb7f0476fd2b02e110326f5a3d62
Biswanath Singh <BiSingh@zuelligpharma.com>
2023-07-14 14:41:38 +0000

    Merged PR 68703: EZRX-7708 Test coverage for forgot password bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-7708
    
    ### Root cause
    - Test coverage for favourite bloc
    
    ### Solution
    - Test file - test/application/auth/forgot_password/forgot_password_bloc_test.dart
    
    ### Output screenshot
    - ![Screenshot from 2023-07-13 17-46-01.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/68703/attachments/Screenshot%20from%202023-07-13%2017-46-01.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases
    
    EZRX-7708 Test coverage for favourite bloc

---
+++ test/application/auth/forgot_password/forgot_password_bloc_test.dart
@@ -0,0 +1,145 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/auth/forgot_password/forgot_password_bloc.dart';
+import 'package:ezrxmobile/domain/auth/entities/forgot_password.dart';
+import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/infrastructure/auth/repository/forget_password_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ForgotPasswordRepositoryMock extends Mock
+    implements ForgotPasswordRepository {}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late ForgotPasswordRepository forgotPasswordRepositoryMock;
+  final userName = Username('fake-username');
+  const local = Locale('en');
+
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    forgotPasswordRepositoryMock = ForgotPasswordRepositoryMock();
+  });
+
+  group('forgot password Bloc', () {
+
+    blocTest(
+      'Initialized forget password bloc',
+      build: () => ForgotPasswordBloc(
+        forgotPasswordRepository: forgotPasswordRepositoryMock,
+      ),
+      act: (ForgotPasswordBloc bloc) => bloc.add(
+        const ForgotPasswordEvent.initialized(),
+      ),
+      expect: () => [
+        ForgotPasswordState.initial(),
+      ],
+    );
+    blocTest(
+      'User name change',
+      build: () => ForgotPasswordBloc(
+        forgotPasswordRepository: forgotPasswordRepositoryMock,
+      ),
+      act: (ForgotPasswordBloc bloc) => bloc.add(
+        ForgotPasswordEvent.usernameChanged(
+          userName.getOrDefaultValue(''),
+        ),
+      ),
+      expect: () => [
+        ForgotPasswordState.initial().copyWith(username: userName),
+      ],
+    );
+
+    blocTest(
+      'Try forget password with empty username',
+      build: () => ForgotPasswordBloc(
+        forgotPasswordRepository: forgotPasswordRepositoryMock,
+      ),
+      seed: () =>
+          ForgotPasswordState.initial().copyWith(username: Username('')),
+      act: (ForgotPasswordBloc bloc) => bloc.add(
+        const ForgotPasswordEvent.requestPasswordReset(local),
+      ),
+      expect: () => [
+        ForgotPasswordState.initial().copyWith(showErrorMessages: true),
+      ],
+    );
+
+    blocTest(
+      'Forget password success',
+      build: () => ForgotPasswordBloc(
+        forgotPasswordRepository: forgotPasswordRepositoryMock,
+      ),
+      setUp: () {
+        when(
+          () => forgotPasswordRepositoryMock.requestResetPassword(
+            locale: local,
+            username: userName,
+          ),
+        ).thenAnswer(
+          (invocation) async => Right(
+            ForgotPassword(requestResetPassword: false),
+          ),
+        );
+      },
+      seed: () => ForgotPasswordState.initial().copyWith(username: userName),
+      act: (ForgotPasswordBloc bloc) => bloc.add(
+        const ForgotPasswordEvent.requestPasswordReset(local),
+      ),
+      expect: () => [
+        ForgotPasswordState.initial().copyWith(
+          isSubmitting: true,
+          username: userName,
+        ),
+        ForgotPasswordState.initial().copyWith(
+          username: userName,
+          resetPasswordFailureOrSuccessOption: optionOf(
+            Right(
+              ForgotPassword(requestResetPassword: false),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Forget password fail',
+      build: () => ForgotPasswordBloc(
+        forgotPasswordRepository: forgotPasswordRepositoryMock,
+      ),
+      setUp: () {
+        when(
+          () => forgotPasswordRepositoryMock.requestResetPassword(
+            locale: local,
+            username: userName,
+          ),
+        ).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('fake-error'),
+          ),
+        );
+      },
+      seed: () => ForgotPasswordState.initial().copyWith(username: userName),
+      act: (ForgotPasswordBloc bloc) => bloc.add(
+        const ForgotPasswordEvent.requestPasswordReset(local),
+      ),
+      expect: () => [
+        ForgotPasswordState.initial().copyWith(
+          isSubmitting: true,
+          username: userName,
+        ),
+        ForgotPasswordState.initial().copyWith(
+          username: userName,
+          showErrorMessages: true,
+          resetPasswordFailureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('fake-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+  });
+}
