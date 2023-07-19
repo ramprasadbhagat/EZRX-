d835f0f728717ed09cb95d7f108610a61cc5634c
Rawaha Muhammad Nazir <rmnazir@zuelligpharma.com>
2023-07-19 12:44:17 +0000

    Merged PR 68832: Enhancement/EZRX-7759 - Add recent order bloc tests
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-7759
    
    ### Screenshot
    ![Full.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/68832/attachments/Full.png)

---
+++ test/application/order/recent_order/recent_order_bloc_test.dart
@@ -0,0 +1,375 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/order/recent_order/recent_order_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/account/entities/sales_organisation_configs.dart';
+import 'package:ezrxmobile/domain/account/entities/ship_to_info.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/core/product_images/entities/product_images.dart';
+import 'package:ezrxmobile/domain/order/entities/material_info.dart';
+import 'package:ezrxmobile/domain/order/entities/recent_order_item.dart';
+import 'package:ezrxmobile/domain/order/value/value_objects.dart';
+import 'package:ezrxmobile/infrastructure/order/repository/favourite_repository.dart';
+import 'package:ezrxmobile/infrastructure/order/repository/recent_order_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../payments/invoice_details/invoice_details_bloc_test.dart';
+
+class RecentOrderRepositoryMock extends Mock implements RecentOrderRepository {}
+
+class FavoriteRepositoryMock extends Mock implements FavouriteRepository {}
+
+void main() {
+  final recentOrderRepository = RecentOrderRepositoryMock();
+  final productImagesRepository = ProductImageRepositoryMock();
+  final favoriteRepository = FavoriteRepositoryMock();
+
+  final salesOrgConfigMock = SalesOrganisationConfigs.empty();
+  final customerCodeInfoMock = CustomerCodeInfo.empty();
+  final shipToInfoMock = ShipToInfo.empty();
+  final mockMaterialNumber = MaterialNumber('mockMaterialNumber');
+
+  final recentOrderItemsMock = [
+    RecentOrderItem.empty().copyWith(qty: 4),
+  ];
+
+  final updatedRecentOrderItemsMock = [
+    RecentOrderItem.empty().copyWith(
+      qty: 4,
+      isFavourite: false,
+    ),
+    RecentOrderItem.empty().copyWith(
+      qty: 5,
+      isFavourite: true,
+    ),
+  ];
+
+  final updatedRecentOrderItemsWithImagesMock = [
+    RecentOrderItem.empty().copyWith(
+      qty: 4,
+      isFavourite: false,
+      materialNumber: mockMaterialNumber,
+      productImages: ProductImages.empty().copyWith(name: 'mock1'),
+    ),
+    RecentOrderItem.empty().copyWith(
+      qty: 5,
+      isFavourite: true,
+      materialNumber: mockMaterialNumber,
+      productImages: ProductImages.empty().copyWith(name: 'mock2'),
+    ),
+  ];
+
+  final materialInfoMock = [
+    MaterialInfo.empty().copyWith(
+        materialNumber:
+            updatedRecentOrderItemsWithImagesMock[0].materialNumber),
+    MaterialInfo.empty().copyWith(
+        materialNumber:
+            updatedRecentOrderItemsWithImagesMock[1].materialNumber),
+  ];
+
+  group('Recent Order Bloc', () {
+    blocTest(
+      'Fetch recently ordered products success',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => recentOrderRepository.getRecentlyOrderedProducts(
+              salesOrgConfig: salesOrgConfigMock,
+              customerCodeInfo: customerCodeInfoMock,
+              shipToInfo: shipToInfoMock,
+            )).thenAnswer(
+          (invocation) async => Right(recentOrderItemsMock),
+        );
+
+        when(() => favoriteRepository.getFavouritesForList(
+              salesConfigs: salesOrgConfigMock,
+              list: recentOrderItemsMock,
+            )).thenAnswer(
+          (invocation) async => Right(updatedRecentOrderItemsMock),
+        );
+
+        when(() => productImagesRepository.getProductImages(
+              list: updatedRecentOrderItemsMock,
+            )).thenAnswer(
+          (invocation) async => Right(updatedRecentOrderItemsWithImagesMock),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.fetchRecentlyOrderedProducts(
+          configs: salesOrgConfigMock,
+          customerCodeInfo: customerCodeInfoMock,
+          shipToInfo: shipToInfoMock,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          isFetching: true,
+          recentlyOrderedProducts: <RecentOrderItem>[],
+        ),
+        RecentOrderState.initial().copyWith(
+          recentlyOrderedProducts: recentOrderItemsMock,
+          apiFailureOrSuccessOption: some(
+            Right(
+              recentOrderItemsMock,
+            ),
+          ),
+        ),
+        RecentOrderState.initial().copyWith(
+          recentlyOrderedProducts: updatedRecentOrderItemsMock,
+          apiFailureOrSuccessOption: some(
+            Right(
+              updatedRecentOrderItemsMock,
+            ),
+          ),
+        ),
+        RecentOrderState.initial().copyWith(
+          recentlyOrderedProducts: updatedRecentOrderItemsWithImagesMock,
+          apiFailureOrSuccessOption: some(
+            Right(
+              updatedRecentOrderItemsWithImagesMock,
+            ),
+          ),
+        ),
+      ],
+      verify: (RecentOrderBloc bloc) =>
+          bloc.state.toMaterialInfo == materialInfoMock,
+    );
+
+    blocTest(
+      'Fetch recently ordered products failure',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => recentOrderRepository.getRecentlyOrderedProducts(
+              salesOrgConfig: salesOrgConfigMock,
+              customerCodeInfo: customerCodeInfoMock,
+              shipToInfo: shipToInfoMock,
+            )).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('mock-error'),
+          ),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.fetchRecentlyOrderedProducts(
+          configs: salesOrgConfigMock,
+          customerCodeInfo: customerCodeInfoMock,
+          shipToInfo: shipToInfoMock,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          isFetching: true,
+          recentlyOrderedProducts: <RecentOrderItem>[],
+        ),
+        RecentOrderState.initial().copyWith(
+          apiFailureOrSuccessOption: some(
+            const Left(
+              ApiFailure.other('mock-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Fetch products favorite failure',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => favoriteRepository.getFavouritesForList(
+              salesConfigs: salesOrgConfigMock,
+              list: recentOrderItemsMock,
+            )).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('mock-error'),
+          ),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.fetchProductsFavouriteStatus(
+          configs: salesOrgConfigMock,
+          productList: recentOrderItemsMock,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          apiFailureOrSuccessOption: some(
+            const Left(
+              ApiFailure.other('mock-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Add favorite success',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => favoriteRepository.addToFavourites(
+              materialNumber: mockMaterialNumber,
+              list: <RecentOrderItem>[],
+            )).thenAnswer(
+          (invocation) async => Right(updatedRecentOrderItemsMock),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.addFavourite(
+          materialCode: mockMaterialNumber,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          recentlyOrderedProducts: updatedRecentOrderItemsMock,
+          apiFailureOrSuccessOption: some(
+            Right(updatedRecentOrderItemsMock),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Add favorite failure',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => favoriteRepository.addToFavourites(
+              materialNumber: mockMaterialNumber,
+              list: <RecentOrderItem>[],
+            )).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('mock-error'),
+          ),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.addFavourite(
+          materialCode: mockMaterialNumber,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          apiFailureOrSuccessOption: some(
+            const Left(
+              ApiFailure.other('mock-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Delete favorite success',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => favoriteRepository.removeFromFavourites(
+                  materialNumber: mockMaterialNumber,
+                  list: <RecentOrderItem>[],
+                ))
+            .thenAnswer(
+                (invocation) async => Right(updatedRecentOrderItemsMock));
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.deleteFavourite(
+          materialCode: mockMaterialNumber,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          recentlyOrderedProducts: updatedRecentOrderItemsMock,
+          apiFailureOrSuccessOption: some(
+            Right(
+              updatedRecentOrderItemsMock,
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Delete favorite failure',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => favoriteRepository.removeFromFavourites(
+              materialNumber: mockMaterialNumber,
+              list: <RecentOrderItem>[],
+            )).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('mock-error'),
+          ),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.deleteFavourite(
+          materialCode: mockMaterialNumber,
+        ),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          apiFailureOrSuccessOption: some(
+            const Left(
+              ApiFailure.other('mock-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Get Product Images failure',
+      build: () => RecentOrderBloc(
+        recentOrderRepository: recentOrderRepository,
+        productImagesRepository: productImagesRepository,
+        favouriteRepository: favoriteRepository,
+      ),
+      setUp: () {
+        when(() => productImagesRepository.getProductImages(
+              list: <RecentOrderItem>[],
+            )).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('mock-error'),
+          ),
+        );
+      },
+      act: (RecentOrderBloc bloc) => bloc.add(
+        RecentOrderEvent.getProductImages(),
+      ),
+      expect: () => [
+        RecentOrderState.initial().copyWith(
+          apiFailureOrSuccessOption: some(
+            const Left(
+              ApiFailure.other('mock-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+  });
+}
