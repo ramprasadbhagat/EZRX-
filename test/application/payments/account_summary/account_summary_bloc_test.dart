74c21bc9b0ae8ee144a5c98842b12e8827d6eda9
Abhishek Sen <ASen@zuelligpharma.com>
2023-07-20 13:45:17 +0000

    Merged PR 69889: EZRX-8010:Mobile v3: Test coverage for payments account summary bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8010
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Output screenshot
    ![Screenshot 2023-07-20 at 12.10.22 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/69889/attachments/Screenshot%202023-07-20%20at%2012.10.22%20PM.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases

---
+++ test/application/payments/account_summary/account_summary_bloc_test.dart
@@ -0,0 +1,177 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/payments/account_summary/account_summary_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/account/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/payments/entities/credit_limit.dart';
+import 'package:ezrxmobile/domain/payments/entities/outstanding_balance.dart';
+import 'package:ezrxmobile/infrastructure/payments/datasource/account_summary_local.dart';
+import 'package:ezrxmobile/infrastructure/payments/repository/account_summary_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class AccountSummaryRepositoryMock extends Mock
+    implements AccountSummaryRepository {}
+
+void main() {
+  late AccountSummaryRepository accountSummaryMockRepository;
+  late OutstandingBalance outstandingBalance;
+  late CreditLimit creditLimit;
+  final salesOrg = SalesOrg('2001');
+  final mockCustomerCodeInfo = CustomerCodeInfo.empty();
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    accountSummaryMockRepository = AccountSummaryRepositoryMock();
+    outstandingBalance =
+        await AccountSummaryLocalDataSource().fetchInvoiceSummary();
+    creditLimit = await AccountSummaryLocalDataSource().fetchCreditSummary();
+  });
+
+  group(
+    'Account Summary Bloc Test',
+    () {
+      blocTest<AccountSummaryBloc, AccountSummaryState>(
+        'Account Summary Initialize Event',
+        build: () => AccountSummaryBloc(
+          accountSummaryRepository: accountSummaryMockRepository,
+        ),
+        act: (AccountSummaryBloc bloc) => bloc.add(
+          const AccountSummaryEvent.initialize(),
+        ),
+        expect: () => [
+          AccountSummaryState.initial(),
+        ],
+      );
+
+      blocTest<AccountSummaryBloc, AccountSummaryState>(
+        'Account Summary "fetchInvoiceSummary" Event Success',
+        build: () => AccountSummaryBloc(
+          accountSummaryRepository: accountSummaryMockRepository,
+        ),
+        setUp: () {
+          when(
+            () => accountSummaryMockRepository.fetchInvoiceSummary(
+              custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+              salesOrg: salesOrg,
+            ),
+          ).thenAnswer((invocation) async => Right(outstandingBalance));
+        },
+        act: (AccountSummaryBloc bloc) => bloc.add(
+          AccountSummaryEvent.fetchInvoiceSummary(
+            custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+            salesOrg: salesOrg,
+          ),
+        ),
+        expect: () => [
+          AccountSummaryState.initial().copyWith(
+            isFetchingOutstandingBalance: true,
+            failureOrSuccessOption: none(),
+          ),
+          AccountSummaryState.initial().copyWith(
+            outstandingBalance: outstandingBalance,
+            isFetchingOutstandingBalance: false,
+          ),
+        ],
+      );
+
+      blocTest<AccountSummaryBloc, AccountSummaryState>(
+        'Account Summary "fetchInvoiceSummary" Event failure',
+        build: () => AccountSummaryBloc(
+          accountSummaryRepository: accountSummaryMockRepository,
+        ),
+        setUp: () {
+          when(
+            () => accountSummaryMockRepository.fetchInvoiceSummary(
+              custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+              salesOrg: salesOrg,
+            ),
+          ).thenAnswer(
+              (invocation) async => const Left(ApiFailure.other('Fake-Error')));
+        },
+        act: (AccountSummaryBloc bloc) => bloc.add(
+          AccountSummaryEvent.fetchInvoiceSummary(
+            custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+            salesOrg: salesOrg,
+          ),
+        ),
+        expect: () => [
+          AccountSummaryState.initial().copyWith(
+            isFetchingOutstandingBalance: true,
+            failureOrSuccessOption: none(),
+          ),
+          AccountSummaryState.initial().copyWith(
+            failureOrSuccessOption:
+                optionOf(const Left(ApiFailure.other('Fake-Error'))),
+            isFetchingOutstandingBalance: false,
+          ),
+        ],
+      );
+
+      blocTest<AccountSummaryBloc, AccountSummaryState>(
+        'Account Summary "fetchCreditSummary" Event Success',
+        build: () => AccountSummaryBloc(
+          accountSummaryRepository: accountSummaryMockRepository,
+        ),
+        setUp: () {
+          when(
+            () => accountSummaryMockRepository.fetchCreditSummary(
+              custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+              salesOrg: salesOrg,
+            ),
+          ).thenAnswer((invocation) async => Right(creditLimit));
+        },
+        act: (AccountSummaryBloc bloc) => bloc.add(
+          AccountSummaryEvent.fetchCreditSummary(
+            custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+            salesOrg: salesOrg,
+          ),
+        ),
+        expect: () => [
+          AccountSummaryState.initial().copyWith(
+            isFetchingCreditLimit: true,
+            failureOrSuccessOption: none(),
+          ),
+          AccountSummaryState.initial().copyWith(
+            creditLimit: creditLimit,
+            isFetchingCreditLimit: false,
+          ),
+        ],
+      );
+
+      blocTest<AccountSummaryBloc, AccountSummaryState>(
+        'Account Summary "fetchCreditSummary" Event failure',
+        build: () => AccountSummaryBloc(
+          accountSummaryRepository: accountSummaryMockRepository,
+        ),
+        setUp: () {
+          when(
+            () => accountSummaryMockRepository.fetchCreditSummary(
+              custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+              salesOrg: salesOrg,
+            ),
+          ).thenAnswer(
+              (invocation) async => const Left(ApiFailure.other('Fake-Error')));
+        },
+        act: (AccountSummaryBloc bloc) => bloc.add(
+          AccountSummaryEvent.fetchCreditSummary(
+            custCode: mockCustomerCodeInfo.customerCodeSoldTo,
+            salesOrg: salesOrg,
+          ),
+        ),
+        expect: () => [
+          AccountSummaryState.initial().copyWith(
+            isFetchingCreditLimit: true,
+            failureOrSuccessOption: none(),
+          ),
+          AccountSummaryState.initial().copyWith(
+            failureOrSuccessOption:
+                optionOf(const Left(ApiFailure.other('Fake-Error'))),
+            isFetchingCreditLimit: false,
+          ),
+        ],
+      );
+    },
+  );
+}
