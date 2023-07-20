b79c8060d96742c12c1daa45d2a57d89a8a39053
Mainak Shil <Mshil@zuelligpharma.com>
2023-07-20 13:46:38 +0000

    Merged PR 69922: EZRX-8011-mobile-v-3-test-coverage-for-credits-and-invoice-details-bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8011
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-07-20 at 1.52.00 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/69922/attachments/Screenshot%202023-07-20%20at%201.52.00%20PM.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases
    
    EZRX-8011-mobile-v-3-test-coverage-for-credits-and-invoice-details-bloc
    
    EZRX-8011-mobile-v-3-test-coverage-for-credits-and-invoice-details-bloc

--- test/application/payments/invoice_details/invoice_details_bloc_test.dart
+++ test/application/payments/invoice_details/invoice_details_bloc_test.dart
@@ -4,7 +4,9 @@ import 'package:ezrxmobile/application/payments/credit_and_invoice_details/credi
 import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
 import 'package:ezrxmobile/domain/account/entities/sales_organisation.dart';
 import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/order/value/value_objects.dart';
 import 'package:ezrxmobile/domain/payments/entities/credit_and_invoice_item.dart';
+import 'package:ezrxmobile/domain/payments/entities/customer_document_detail.dart';
 import 'package:ezrxmobile/infrastructure/account/repository/customer_code_repository.dart';
 import 'package:ezrxmobile/infrastructure/core/product_images/repository/product_images_repository.dart';
 import 'package:ezrxmobile/infrastructure/payments/repository/credit_and_invoice_details_repository.dart';
@@ -19,11 +21,11 @@ class CustomerCodeRepositoryMock extends Mock
     implements CustomerCodeRepository {}
 
 class ProductImageRepositoryMock extends Mock
-    implements ProductImagesRepository {}    
+    implements ProductImagesRepository {}
 
 void main() {
   late CreditAndInvoiceDetailsRepository creditAndInvoiceDetailsRepository;
-  //late List<CustomerDocumentDetail> fakeInvoiceItems;
+  late List<CustomerDocumentDetail> fakeInvoiceItems;
   late CreditAndInvoiceItem fakeInvoice;
   late ProductImagesRepository productImagesRepository;
   setUpAll(() async {
@@ -33,9 +35,10 @@ void main() {
   });
 
   setUp(() {
-    // fakeInvoiceItems = [
-    //   CustomerDocumentDetail.empty(),
-    // ];
+    fakeInvoiceItems = [
+      CustomerDocumentDetail.empty()
+          .copyWith(materialNumber: MaterialNumber('123456')),
+    ];
     fakeInvoice = CreditAndInvoiceItem.empty().copyWith(
       bpCustomerNumber: '0030032223',
       fiscalYear: '2023',
@@ -49,9 +52,9 @@ void main() {
     () {
       blocTest('Initialize',
           build: () => CreditAndInvoiceDetailsBloc(
-                creditAndInvoiceDetailsRepository: creditAndInvoiceDetailsRepository,
-                productImagesRepository: productImagesRepository
-              ),
+              creditAndInvoiceDetailsRepository:
+                  creditAndInvoiceDetailsRepository,
+              productImagesRepository: productImagesRepository),
           act: (CreditAndInvoiceDetailsBloc bloc) =>
               bloc.add(const CreditAndInvoiceDetailsEvent.initialized()),
           expect: () => [CreditAndInvoiceDetailsState.initial()]);
@@ -100,38 +103,111 @@ void main() {
           ),
         ],
       );
-      // blocTest(
-      //   'fetch -> Invoice Details fetch success',
-      //   build: () => CreditAndInvoiceDetailsBloc(
-      //     creditAndInvoiceDetailsRepository: creditAndInvoiceDetailsRepository,
-      //   ),
-      //   setUp: () {
-      //     when(
-      //       () => creditAndInvoiceDetailsRepository.getCreditAndInvoiceDetails(
-      //         salesOrganisation: SalesOrganisation.empty(),
-      //         customerCodeInfo: CustomerCodeInfo.empty(),
-      //         creditAndInvoiceItem: fakeInvoice,
-      //       ),
-      //     ).thenAnswer(
-      //       (invocation) async => Right(fakeInvoiceItems),
-      //     );
-      //   },
-      //   act: (CreditAndInvoiceDetailsBloc bloc) => bloc.add(
-      //     CreditAndInvoiceDetailsEvent.fetch(
-      //       salesOrganisation: SalesOrganisation.empty(),
-      //       customerCodeInfo: CustomerCodeInfo.empty(),
-      //       creditAndInvoiceItem: fakeInvoice,
-      //     ),
-      //   ),
-      //   expect: () => [
-      //     CreditAndInvoiceDetailsState.initial().copyWith(
-      //       isLoading: true,
-      //     ),
-      //     CreditAndInvoiceDetailsState.initial().copyWith(
-      //       details: fakeInvoiceItems,
-      //     ),
-      //   ],
-      // );
+      blocTest(
+        'fetch -> Invoice Details fetch success',
+        build: () => CreditAndInvoiceDetailsBloc(
+          creditAndInvoiceDetailsRepository: creditAndInvoiceDetailsRepository,
+          productImagesRepository: productImagesRepository,
+        ),
+        setUp: () {
+          when(
+            () => creditAndInvoiceDetailsRepository.getCreditAndInvoiceDetails(
+              salesOrganisation: SalesOrganisation.empty(),
+              customerCodeInfo: CustomerCodeInfo.empty(),
+              creditAndInvoiceItem: fakeInvoice,
+            ),
+          ).thenAnswer(
+            (invocation) async => const Right(<CustomerDocumentDetail>[]),
+          );
+        },
+        act: (CreditAndInvoiceDetailsBloc bloc) => bloc.add(
+          CreditAndInvoiceDetailsEvent.fetch(
+            salesOrganisation: SalesOrganisation.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            creditAndInvoiceItem: fakeInvoice,
+          ),
+        ),
+        expect: () => [
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            isLoading: true,
+          ),
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            isLoading: false,
+            details: <CustomerDocumentDetail>[],
+          ),
+        ],
+      );
+
+      blocTest(
+        'fetch -> Product Image fetch fail',
+        build: () => CreditAndInvoiceDetailsBloc(
+          creditAndInvoiceDetailsRepository: creditAndInvoiceDetailsRepository,
+          productImagesRepository: productImagesRepository,
+        ),
+        seed: () => CreditAndInvoiceDetailsState.initial().copyWith(
+          details: fakeInvoiceItems,
+        ),
+        setUp: () {
+          when(
+            () => productImagesRepository.getProductImages(
+                list: fakeInvoiceItems),
+          ).thenAnswer(
+            (invocation) async => const Left(
+              ApiFailure.other('fake-error'),
+            ),
+          );
+        },
+        act: (CreditAndInvoiceDetailsBloc bloc) => bloc.add(
+          const CreditAndInvoiceDetailsEvent.fetchProductImage(),
+        ),
+        expect: () => [
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            imageLoading: true,
+            details: fakeInvoiceItems,
+          ),
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            imageLoading: false,
+            failureOrSuccessOption: optionOf(
+              const Left(
+                ApiFailure.other('fake-error'),
+              ),
+            ),
+            details: fakeInvoiceItems,
+          ),
+        ],
+      );
+
+      blocTest(
+        'fetch -> Product Image fetch success',
+        build: () => CreditAndInvoiceDetailsBloc(
+          creditAndInvoiceDetailsRepository: creditAndInvoiceDetailsRepository,
+          productImagesRepository: productImagesRepository,
+        ),
+        seed: () => CreditAndInvoiceDetailsState.initial().copyWith(
+          details: fakeInvoiceItems,
+        ),
+        setUp: () {
+          when(
+            () => productImagesRepository.getProductImages(
+                list: fakeInvoiceItems),
+          ).thenAnswer(
+            (invocation) async => Right(fakeInvoiceItems),
+          );
+        },
+        act: (CreditAndInvoiceDetailsBloc bloc) => bloc.add(
+          const CreditAndInvoiceDetailsEvent.fetchProductImage(),
+        ),
+        expect: () => [
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            imageLoading: true,
+            details: fakeInvoiceItems,
+          ),
+          CreditAndInvoiceDetailsState.initial().copyWith(
+            imageLoading: false,
+            details: fakeInvoiceItems,
+          ),
+        ],
+      );
     },
   );
 }
