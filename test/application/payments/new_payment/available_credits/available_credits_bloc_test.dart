e3a15754bba4ff7af083da9b7817945984b87d8c
Biswaranjan Basak <bbasak@zuelligpharma.com>
2023-07-26 03:11:35 +0000

    Merged PR 70712: Testcase/EZRX-8416:Mobile v3: Test coverage for Available Credits Bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8416
    
    ### Output screenshot
    - ![image.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/70712/attachments/image.png)

---
+++ test/application/payments/new_payment/available_credits/available_credits_bloc_test.dart
@@ -0,0 +1,187 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/payments/new_payment/available_credits/available_credits_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/account/entities/sales_organisation.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/payments/entities/available_credit_filter.dart';
+import 'package:ezrxmobile/domain/payments/entities/customer_open_item.dart';
+import 'package:ezrxmobile/infrastructure/payments/datasource/new_payment_local.dart';
+import 'package:ezrxmobile/infrastructure/payments/repository/new_payment_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class NewPaymentRepositoryMock extends Mock implements NewPaymentRepository {}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  late NewPaymentRepository newPaymentRepositoryMock;
+  late List<CustomerOpenItem> openItems;
+
+  group(
+    'Available_Credit_Bloc Test => ',
+    () {
+      setUp(() async {
+        newPaymentRepositoryMock = NewPaymentRepositoryMock();
+        openItems = await NewPaymentLocalDataSource().getCustomerOpenItems();
+      });
+
+      blocTest<AvailableCreditsBloc, AvailableCreditsState>(
+        'For "initialized" Event',
+        build: () => AvailableCreditsBloc(
+          newPaymentRepository: newPaymentRepositoryMock,
+        ),
+        act: (bloc) => bloc.add(const AvailableCreditsEvent.initialized()),
+        expect: () => [AvailableCreditsState.initial()],
+      );
+
+      blocTest<AvailableCreditsBloc, AvailableCreditsState>(
+        'For "fetch" Event with failure',
+        build: () => AvailableCreditsBloc(
+          newPaymentRepository: newPaymentRepositoryMock,
+        ),
+        setUp: () {
+          when(
+            () => newPaymentRepositoryMock.getAvailableCreditNotes(
+              salesOrganisation: SalesOrganisation.empty(),
+              customerCodeInfo: CustomerCodeInfo.empty(),
+              pageSize: 24,
+              offset: 0,
+              appliedFilter: AvailableCreditFilter.empty(),
+            ),
+          ).thenAnswer(
+              (invocation) async => const Left(ApiFailure.other('Fake-Error')));
+        },
+        act: (bloc) => bloc.add(
+          AvailableCreditsEvent.fetch(
+            appliedFilter: AvailableCreditFilter.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            salesOrganisation: SalesOrganisation.empty(),
+          ),
+        ),
+        expect: () => [
+          AvailableCreditsState.initial().copyWith(
+            isLoading: true,
+          ),
+          AvailableCreditsState.initial().copyWith(
+            failureOrSuccessOption:
+                optionOf(const Left(ApiFailure.other('Fake-Error'))),
+          )
+        ],
+      );
+
+      blocTest<AvailableCreditsBloc, AvailableCreditsState>(
+        'For "fetch" Event with success',
+        build: () => AvailableCreditsBloc(
+          newPaymentRepository: newPaymentRepositoryMock,
+        ),
+        setUp: () {
+          when(
+            () => newPaymentRepositoryMock.getAvailableCreditNotes(
+              salesOrganisation: SalesOrganisation.empty(),
+              customerCodeInfo: CustomerCodeInfo.empty(),
+              pageSize: 24,
+              offset: 0,
+              appliedFilter: AvailableCreditFilter.empty(),
+            ),
+          ).thenAnswer((invocation) async => Right(openItems));
+        },
+        act: (bloc) => bloc.add(
+          AvailableCreditsEvent.fetch(
+            appliedFilter: AvailableCreditFilter.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            salesOrganisation: SalesOrganisation.empty(),
+          ),
+        ),
+        expect: () => [
+          AvailableCreditsState.initial().copyWith(
+            isLoading: true,
+          ),
+          AvailableCreditsState.initial().copyWith(
+            items: openItems,
+            canLoadMore: false,
+          )
+        ],
+      );
+
+      blocTest<AvailableCreditsBloc, AvailableCreditsState>(
+        'For "loadMore" Event with failure',
+        build: () => AvailableCreditsBloc(
+          newPaymentRepository: newPaymentRepositoryMock,
+        ),
+        seed: () => AvailableCreditsState.initial().copyWith(
+          appliedFilter: AvailableCreditFilter.empty(),
+          items: openItems,
+        ),
+        setUp: () {
+          when(
+            () => newPaymentRepositoryMock.getAvailableCreditNotes(
+              salesOrganisation: SalesOrganisation.empty(),
+              customerCodeInfo: CustomerCodeInfo.empty(),
+              pageSize: 24,
+              offset: openItems.length,
+              appliedFilter: AvailableCreditFilter.empty(),
+            ),
+          ).thenAnswer(
+              (invocation) async => const Left(ApiFailure.other('Fake-Error')));
+        },
+        act: (bloc) => bloc.add(
+          AvailableCreditsEvent.loadMore(
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            salesOrganisation: SalesOrganisation.empty(),
+          ),
+        ),
+        expect: () => [
+          AvailableCreditsState.initial().copyWith(
+            items: openItems,
+            isLoading: true,
+          ),
+          AvailableCreditsState.initial().copyWith(
+            items: openItems,
+            failureOrSuccessOption:
+                optionOf(const Left(ApiFailure.other('Fake-Error'))),
+          )
+        ],
+      );
+
+      blocTest<AvailableCreditsBloc, AvailableCreditsState>(
+        'For "loadMore" Event with success',
+        build: () => AvailableCreditsBloc(
+          newPaymentRepository: newPaymentRepositoryMock,
+        ),
+        seed: () => AvailableCreditsState.initial().copyWith(
+          appliedFilter: AvailableCreditFilter.empty(),
+          items: openItems,
+        ),
+        setUp: () {
+          when(
+            () => newPaymentRepositoryMock.getAvailableCreditNotes(
+              salesOrganisation: SalesOrganisation.empty(),
+              customerCodeInfo: CustomerCodeInfo.empty(),
+              pageSize: 24,
+              offset: openItems.length,
+              appliedFilter: AvailableCreditFilter.empty(),
+            ),
+          ).thenAnswer((invocation) async => Right(openItems));
+        },
+        act: (bloc) => bloc.add(
+          AvailableCreditsEvent.loadMore(
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            salesOrganisation: SalesOrganisation.empty(),
+          ),
+        ),
+        expect: () => [
+          AvailableCreditsState.initial().copyWith(
+            items: openItems,
+            isLoading: true,
+          ),
+          AvailableCreditsState.initial().copyWith(
+            canLoadMore: false,
+            items: List<CustomerOpenItem>.from(openItems)..addAll(openItems),
+          )
+        ],
+      );
+    },
+  );
+}
