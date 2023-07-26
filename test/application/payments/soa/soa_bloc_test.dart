c293e6f89723ee3a701d171728c1c9c4f1412d94
Rawaha Muhammad Nazir <rmnazir@zuelligpharma.com>
2023-07-26 15:06:30 +0000

    Merged PR 70964: TestCase/EZRX-8423 - Add Soa Bloc Unit tests
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8423
    
    ### Output screenshot
    ![Screenshot 2023-07-26 at 2.56.50 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/70964/attachments/Screenshot%202023-07-26%20at%202.56.50%20PM.png)

---
+++ test/application/payments/soa/soa_bloc_test.dart
@@ -0,0 +1,102 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/payments/soa/soa_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/payments/entities/soa.dart';
+import 'package:ezrxmobile/domain/payments/value/value_object.dart';
+import 'package:ezrxmobile/infrastructure/payments/repository/soa_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class SoaRepositoryMock extends Mock implements SoaRepository {}
+
+void main() {
+  late SoaRepository soaRepositoryMock;
+
+  final mockCustomerCodeInfo =
+      CustomerCodeInfo.empty().copyWith(customerCodeSoldTo: 'mockCustomerCode');
+  final mockSoaList = <Soa>[
+    Soa(
+      soaData: SoaData('mockData'),
+    ),
+  ];
+
+  setUpAll(() async {
+    soaRepositoryMock = SoaRepositoryMock();
+  });
+
+  group('Soa Bloc', () {
+    blocTest(
+      'Initialize',
+      build: () => SoaBloc(
+        repository: soaRepositoryMock,
+      ),
+      act: (SoaBloc bloc) => bloc.add(
+        const SoaEvent.initialized(),
+      ),
+      expect: () => [
+        SoaState.initial(),
+      ],
+    );
+
+    blocTest(
+      'Fetch Success',
+      build: () => SoaBloc(
+        repository: soaRepositoryMock,
+      ),
+      setUp: () {
+        when(() => soaRepositoryMock.fetchSoa(
+            customerCodeInfo: mockCustomerCodeInfo)).thenAnswer(
+          (invocation) async => Right(
+            mockSoaList,
+          ),
+        );
+      },
+      act: (SoaBloc bloc) => bloc.add(
+        SoaEvent.fetch(customerCodeInfo: mockCustomerCodeInfo),
+      ),
+      expect: () => [
+        SoaState.initial().copyWith(
+          failureOrSuccessOption: none(),
+          isFetching: false,
+          soaList: mockSoaList,
+        ),
+      ],
+      verify: (SoaBloc bloc) => [
+        expect(bloc.state.soaList.length, 1),
+      ],
+    );
+
+    blocTest(
+      'Fetch Failure',
+      build: () => SoaBloc(
+        repository: soaRepositoryMock,
+      ),
+      setUp: () {
+        when(() => soaRepositoryMock.fetchSoa(
+            customerCodeInfo: mockCustomerCodeInfo)).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('api-failure'),
+          ),
+        );
+      },
+      act: (SoaBloc bloc) => bloc.add(
+        SoaEvent.fetch(customerCodeInfo: mockCustomerCodeInfo),
+      ),
+      expect: () => [
+        SoaState.initial().copyWith(
+          failureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('api-failure'),
+            ),
+          ),
+          isFetching: false,
+        ),
+      ],
+      verify: (SoaBloc bloc) => [
+        expect(bloc.state.soaList.length, 0),
+      ],
+    );
+  });
+}
