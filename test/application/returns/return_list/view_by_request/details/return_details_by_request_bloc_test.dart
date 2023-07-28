35a9e4c1f5b183e63b614a28f5954e7f65d4ce75
Mainak Shil <Mshil@zuelligpharma.com>
2023-07-28 13:19:08 +0000

    Merged PR 71502: TEST COVERAGE/EZRX-8524-mobile-v-3-test-coverage-for-returns-view-by-requests-details-bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8524
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-07-28 at 6.11.21 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/71502/attachments/Screenshot%202023-07-28%20at%206.11.21%20PM.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases
    
    Cover test cases for Return Details By Request Bloc
    
    Cover test cases for Return Details By Request Bloc

---
+++ test/application/returns/return_list/view_by_request/details/return_details_by_request_bloc_test.dart
@@ -0,0 +1,110 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/returns/return_list/view_by_request/details/return_details_by_request_bloc.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/returns/entities/request_information.dart';
+import 'package:ezrxmobile/domain/returns/entities/return_request_information.dart';
+import 'package:ezrxmobile/domain/returns/entities/return_request_information_header.dart';
+import 'package:ezrxmobile/domain/returns/entities/return_requests_id.dart';
+import 'package:ezrxmobile/infrastructure/returns/repository/return_details_by_request_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ReturnDetailsByRequestRepositoryMock extends Mock
+    implements ReturnDetailsByRequestRepository {}
+
+void main() {
+  late ReturnDetailsByRequestRepository returnDetailsByRequestRepository;
+  late ReturnRequestsId fakeReturnRequestsId;
+  late RequestInformation fakeRequestInformation;
+
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    returnDetailsByRequestRepository = ReturnDetailsByRequestRepositoryMock();
+  });
+  setUp(() {
+    fakeReturnRequestsId = ReturnRequestsId(requestId: '12345');
+    fakeRequestInformation = RequestInformation.empty().copyWith(
+        requestInformationHeader: ReturnRequestInformationHeader.empty(),
+        returnRequestInformationList: <ReturnRequestInformation>[]);
+  });
+
+  group(
+    'Return Details By Request Bloc Initialize',
+    () {
+      blocTest('Initialize',
+          build: () => ReturnDetailsByRequestBloc(
+              returnDetailsByRequestRepository:
+                  returnDetailsByRequestRepository),
+          act: (ReturnDetailsByRequestBloc bloc) =>
+              bloc.add(const ReturnDetailsByRequestEvent.initialized()),
+          expect: () => [ReturnDetailsByRequestState.initial()]);
+    },
+  );
+
+  group('Return Details By Request Bloc Fetch', () {
+    blocTest(
+      'fetch -> Return Details By Request Bloc fail',
+      build: () => ReturnDetailsByRequestBloc(
+          returnDetailsByRequestRepository: returnDetailsByRequestRepository),
+      setUp: () {
+        when(
+          () =>
+              returnDetailsByRequestRepository.getReturnSummaryDetailsByRequest(
+                  returnRequestId: fakeReturnRequestsId),
+        ).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('fake-error'),
+          ),
+        );
+      },
+      act: (ReturnDetailsByRequestBloc bloc) => bloc.add(
+        ReturnDetailsByRequestEvent.fetch(
+            returnId: fakeReturnRequestsId.requestId),
+      ),
+      expect: () => [
+        ReturnDetailsByRequestState.initial().copyWith(
+          isLoading: true,
+        ),
+        ReturnDetailsByRequestState.initial().copyWith(
+          failureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('fake-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+    blocTest(
+      'fetch -> Return Details By Request Bloc Success',
+      build: () => ReturnDetailsByRequestBloc(
+          returnDetailsByRequestRepository: returnDetailsByRequestRepository),
+      setUp: () {
+        when(
+          () =>
+              returnDetailsByRequestRepository.getReturnSummaryDetailsByRequest(
+                  returnRequestId: fakeReturnRequestsId),
+        ).thenAnswer(
+          (invocation) async => Right(
+            fakeRequestInformation,
+          ),
+        );
+      },
+      act: (ReturnDetailsByRequestBloc bloc) => bloc.add(
+        ReturnDetailsByRequestEvent.fetch(
+            returnId: fakeReturnRequestsId.requestId),
+      ),
+      expect: () => [
+        ReturnDetailsByRequestState.initial().copyWith(
+          isLoading: true,
+        ),
+        ReturnDetailsByRequestState.initial().copyWith(
+            requestInformation:
+                fakeRequestInformation.returnRequestInformationList,
+            requestInformationHeader:
+                fakeRequestInformation.requestInformationHeader),
+      ],
+    );
+  });
+}
