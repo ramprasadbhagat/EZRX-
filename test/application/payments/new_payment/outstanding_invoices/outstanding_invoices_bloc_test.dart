aa90cdaa2979b8975212ba578a4f7c5bb2b0bb1e
Mainak Shil <Mshil@zuelligpharma.com>
2023-07-28 13:04:09 +0000

    Merged PR 71395: TEST COVERAGE/EZRX-8419-mobile-v-3-test-coverage-for-outstanding-invoices-bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-8419
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-07-28 at 1.19.49 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/71395/attachments/Screenshot%202023-07-28%20at%201.19.49%20PM.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases

---
+++ test/application/payments/new_payment/outstanding_invoices/outstanding_invoices_bloc_test.dart
@@ -0,0 +1,197 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/payments/new_payment/outstanding_invoices/outstanding_invoices_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
+import 'package:ezrxmobile/domain/account/entities/sales_organisation.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/payments/entities/customer_open_item.dart';
+import 'package:ezrxmobile/domain/payments/entities/outstanding_invoice_filter.dart';
+import 'package:ezrxmobile/infrastructure/payments/repository/new_payment_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class NewPaymentRepositoryMock extends Mock implements NewPaymentRepository {}
+
+void main() {
+  late NewPaymentRepository newPaymentRepository;
+  late OutstandingInvoiceFilter fakeOutstandingInvoiceFilter;
+  late List<CustomerOpenItem> fakeCustomerOpenItem;
+  late int pageSize;
+
+  setUpAll(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    newPaymentRepository = NewPaymentRepositoryMock();
+  });
+  setUp(() {
+    fakeOutstandingInvoiceFilter = OutstandingInvoiceFilter.empty().copyWith();
+    fakeCustomerOpenItem = <CustomerOpenItem>[];
+    pageSize = 24;
+  });
+
+  group(
+    'Outstanding Invoices Bloc Initialize',
+    () {
+      blocTest('Initialize',
+          build: () => OutstandingInvoicesBloc(
+              newPaymentRepository: newPaymentRepository),
+          act: (OutstandingInvoicesBloc bloc) =>
+              bloc.add(const OutstandingInvoicesEvent.initialized()),
+          expect: () => [OutstandingInvoicesState.initial()]);
+    },
+  );
+
+  group('Outstanding Invoices Bloc Fetch', () {
+    blocTest(
+      'fetch -> Outstanding Invoices fetch fail',
+      build: () =>
+          OutstandingInvoicesBloc(newPaymentRepository: newPaymentRepository),
+      setUp: () {
+        when(
+          () => newPaymentRepository.getOutstandingInvoices(
+            salesOrganisation: SalesOrganisation.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            pageSize: pageSize,
+            appliedFilter: fakeOutstandingInvoiceFilter,
+            offset: 0,
+          ),
+        ).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('fake-error'),
+          ),
+        );
+      },
+      act: (OutstandingInvoicesBloc bloc) => bloc.add(
+        OutstandingInvoicesEvent.fetch(
+          salesOrganisation: SalesOrganisation.empty(),
+          customerCodeInfo: CustomerCodeInfo.empty(),
+          appliedFilter: fakeOutstandingInvoiceFilter,
+        ),
+      ),
+      expect: () => [
+        OutstandingInvoicesState.initial().copyWith(
+          isLoading: true,
+        ),
+        OutstandingInvoicesState.initial().copyWith(
+          failureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('fake-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+    blocTest(
+      'fetch -> Outstanding Invoices fetch Success',
+      build: () =>
+          OutstandingInvoicesBloc(newPaymentRepository: newPaymentRepository),
+      setUp: () {
+        when(
+          () => newPaymentRepository.getOutstandingInvoices(
+            salesOrganisation: SalesOrganisation.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            pageSize: pageSize,
+            appliedFilter: fakeOutstandingInvoiceFilter,
+            offset: 0,
+          ),
+        ).thenAnswer(
+          (invocation) async => Right(
+            fakeCustomerOpenItem,
+          ),
+        );
+      },
+      act: (OutstandingInvoicesBloc bloc) => bloc.add(
+        OutstandingInvoicesEvent.fetch(
+          salesOrganisation: SalesOrganisation.empty(),
+          customerCodeInfo: CustomerCodeInfo.empty(),
+          appliedFilter: fakeOutstandingInvoiceFilter,
+        ),
+      ),
+      expect: () => [
+        OutstandingInvoicesState.initial().copyWith(
+          isLoading: true,
+        ),
+        OutstandingInvoicesState.initial().copyWith(
+          items: fakeCustomerOpenItem,
+          canLoadMore: fakeCustomerOpenItem.length >= pageSize,
+        ),
+      ],
+    );
+  });
+
+  group('Outstanding Invoices Bloc Load More', () {
+    blocTest(
+      'fetch -> Outstanding Invoices Load More fail',
+      build: () =>
+          OutstandingInvoicesBloc(newPaymentRepository: newPaymentRepository),
+      setUp: () {
+        when(
+          () => newPaymentRepository.getOutstandingInvoices(
+            salesOrganisation: SalesOrganisation.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            pageSize: pageSize,
+            appliedFilter: fakeOutstandingInvoiceFilter,
+            offset: 0,
+          ),
+        ).thenAnswer(
+          (invocation) async => const Left(
+            ApiFailure.other('fake-error'),
+          ),
+        );
+      },
+      act: (OutstandingInvoicesBloc bloc) => bloc.add(
+        OutstandingInvoicesEvent.loadMore(
+          salesOrganisation: SalesOrganisation.empty(),
+          customerCodeInfo: CustomerCodeInfo.empty(),
+        ),
+      ),
+      expect: () => [
+        OutstandingInvoicesState.initial().copyWith(
+          isLoading: true,
+        ),
+        OutstandingInvoicesState.initial().copyWith(
+          failureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('fake-error'),
+            ),
+          ),
+        ),
+      ],
+    );
+    blocTest(
+      'fetch -> Outstanding Invoices Load More Success',
+      build: () =>
+          OutstandingInvoicesBloc(newPaymentRepository: newPaymentRepository),
+      setUp: () {
+        when(
+          () => newPaymentRepository.getOutstandingInvoices(
+            salesOrganisation: SalesOrganisation.empty(),
+            customerCodeInfo: CustomerCodeInfo.empty(),
+            pageSize: pageSize,
+            appliedFilter: fakeOutstandingInvoiceFilter,
+            offset: 0,
+          ),
+        ).thenAnswer(
+          (invocation) async => Right(
+            fakeCustomerOpenItem,
+          ),
+        );
+      },
+      act: (OutstandingInvoicesBloc bloc) => bloc.add(
+        OutstandingInvoicesEvent.loadMore(
+          salesOrganisation: SalesOrganisation.empty(),
+          customerCodeInfo: CustomerCodeInfo.empty(),
+        ),
+      ),
+      expect: () => [
+        OutstandingInvoicesState.initial().copyWith(
+          isLoading: true,
+        ),
+        OutstandingInvoicesState.initial().copyWith(
+          items: fakeCustomerOpenItem,
+          canLoadMore: fakeCustomerOpenItem.length >= pageSize,
+        ),
+      ],
+    );
+  });
+}
