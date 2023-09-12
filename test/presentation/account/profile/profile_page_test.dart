bea5550ded1291cb3c780975a6eed33fba0bd381
Nguyen Quang Minh (Mike) <QMNguyen@zuelligpharma.com>
2023-09-12 01:52:03 +0000

    Merged PR 77290: EZRX-10690 Mobile v3 Widget Tests for Profile Page
    ### Jira ticket url
    https://zuelligpharma.atlassian.net/browse/EZRX-10690
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    - create the widget test to cover at least 80% of the file
    
    ### Test account
    - environment:
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-09-07 at 22.46.31 (2).png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/77287/attachments/Screenshot%202023-09-07%20at%2022.46.31%20%282%29.png)
    
    ### Reminder
    - if you code have significant change please run integration test `make run_sg_client_test` to check if you break any test cases
    
    EZRX-10690 Mobile v3 Widget Tests for Profile Page

---
+++ test/presentation/account/profile/profile_page_test.dart
@@ -0,0 +1,278 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ezrxmobile/application/account/customer_license_bloc/customer_license_bloc.dart';
+import 'package:ezrxmobile/application/account/eligibility/eligibility_bloc.dart';
+import 'package:ezrxmobile/application/account/language/language_bloc.dart';
+import 'package:ezrxmobile/application/account/sales_org/sales_org_bloc.dart';
+import 'package:ezrxmobile/application/account/user/user_bloc.dart';
+import 'package:ezrxmobile/domain/account/entities/access_right.dart';
+import 'package:ezrxmobile/domain/account/entities/customer_license.dart';
+import 'package:ezrxmobile/domain/account/entities/role.dart';
+import 'package:ezrxmobile/domain/account/entities/user.dart';
+import 'package:ezrxmobile/domain/account/value/value_objects.dart';
+import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/presentation/account/profile/profile_page.dart';
+import 'package:ezrxmobile/presentation/core/snack_bar/custom_snackbar.dart';
+import 'package:ezrxmobile/presentation/core/widget_keys.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/widget_utils.dart';
+
+class UserBlocMock extends MockBloc<UserEvent, UserState> implements UserBloc {}
+
+class CustomerLicenseBlocMock
+    extends MockBloc<CustomerLicenseEvent, CustomerLicenseState>
+    implements CustomerLicenseBloc {}
+
+class LanguageBlocMock extends MockBloc<LanguageEvent, LanguageState>
+    implements LanguageBloc {}
+
+class EligibilityBlocMock extends MockBloc<EligibilityEvent, EligibilityState>
+    implements EligibilityBloc {}
+
+class SalesOrgBlocMock extends MockBloc<SalesOrgEvent, SalesOrgState>
+    implements SalesOrgBloc {}
+
+void main() {
+  late GetIt locator;
+  late CustomerLicenseBloc customerLicenseBlocMock;
+  late UserBlocMock userBlockMock;
+  late AppRouter autoRouterMock;
+  late LanguageBloc languageBlocMock;
+  late EligibilityBloc eligibilityBlocMock;
+  late SalesOrgBloc salesOrgBlocMock;
+  late List<CustomerLicense> customerLicensesListMock;
+  late Stream<UserState> userStateMock;
+  late User userMock;
+  setUpAll(() async {
+    locator = GetIt.instance;
+    locator.registerLazySingleton(() => AppRouter());
+    locator.registerLazySingleton(() => customerLicenseBlocMock);
+    autoRouterMock = locator<AppRouter>();
+    customerLicenseBlocMock = CustomerLicenseBlocMock();
+    userBlockMock = UserBlocMock();
+    languageBlocMock = LanguageBlocMock();
+    eligibilityBlocMock = EligibilityBlocMock();
+    salesOrgBlocMock = SalesOrgBlocMock();
+    userMock = User.empty().copyWith(
+      username: Username('fakeUser'),
+      role: Role(
+        type: RoleType('client_user'),
+        description: '',
+        id: '',
+        name: '',
+      ),
+      disableCreateOrder: true,
+      accessRight: AccessRight.empty().copyWith(
+        orders: true,
+      ),
+    );
+    userStateMock = Stream.fromIterable([
+      UserState.initial().copyWith(user: userMock),
+    ]);
+    customerLicensesListMock = [
+      CustomerLicense(
+        licenseNumbers: StringValue('fake1'),
+        licenseType: StringValue('fake2'),
+        licenseDescription: StringValue('fake3'),
+        validFrom: DateTimeStringValue('fake4'),
+        validTo: DateTimeStringValue('fake4'),
+        status: StatusType('fake5'),
+      ),
+      CustomerLicense(
+        licenseNumbers: StringValue('fake1'),
+        licenseType: StringValue('fake2'),
+        licenseDescription: StringValue('fake3'),
+        validFrom: DateTimeStringValue('fake4'),
+        validTo: DateTimeStringValue('fake4'),
+        status: StatusType('fake5'),
+      )
+    ];
+  });
+  group('Test profile page', () {
+    setUp(() {
+      when(() => customerLicenseBlocMock.state)
+          .thenReturn(CustomerLicenseState.initial());
+      when(() => userBlockMock.state).thenReturn(
+        UserState.initial().copyWith(user: userMock),
+      );
+      when(() => languageBlocMock.state).thenReturn(LanguageState.initial());
+      when(() => eligibilityBlocMock.state)
+          .thenReturn(EligibilityState.initial());
+      when(() => salesOrgBlocMock.state).thenReturn(SalesOrgState.initial());
+    });
+    RouteDataScope getWidget() {
+      return WidgetUtils.getScopedWidget(
+        autoRouterMock: autoRouterMock,
+        providers: [
+          BlocProvider<CustomerLicenseBloc>(
+            create: (context) => customerLicenseBlocMock,
+          ),
+          BlocProvider<UserBloc>(
+            create: (context) => userBlockMock,
+          ),
+          BlocProvider<LanguageBloc>(
+            create: (context) => languageBlocMock,
+          ),
+          BlocProvider<EligibilityBloc>(
+            create: (context) => eligibilityBlocMock,
+          ),
+          BlocProvider<SalesOrgBloc>(
+            create: (context) => salesOrgBlocMock,
+          ),
+        ],
+        child: const ProfilePage(),
+      );
+    }
+
+    testWidgets('should show loading for the first time', (tester) async {
+      final customerLicenseState = [
+        CustomerLicenseState.initial().copyWith(isFetching: true)
+      ];
+
+      whenListen(
+        customerLicenseBlocMock,
+        Stream.fromIterable(customerLicenseState),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      await tester.pump(const Duration(seconds: 2));
+      expect(find.byKey(WidgetKeys.loaderImage), findsOneWidget);
+    });
+    testWidgets('should show toast error for the case api fail',
+        (tester) async {
+      final customerLicenseState = [
+        CustomerLicenseState.initial(),
+        CustomerLicenseState.initial().copyWith(
+          failureOrSuccessOption: optionOf(
+            const Left(
+              ApiFailure.other('Fake Error'),
+            ),
+          ),
+        )
+      ];
+
+      whenListen(
+        customerLicenseBlocMock,
+        Stream.fromIterable(customerLicenseState),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      await tester.pumpAndSettle();
+      expect(find.byType(CustomSnackBar), findsOneWidget);
+    });
+    testWidgets('should the user info', (tester) async {
+      final customerLicenseState = [
+        CustomerLicenseState.initial().copyWith(
+          isFetching: false,
+        ),
+      ];
+
+      whenListen(
+        customerLicenseBlocMock,
+        Stream.fromIterable(customerLicenseState),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      await tester.pumpAndSettle();
+      expect(find.text('fakeUser'), findsOneWidget);
+    });
+    testWidgets('should the license information list', (tester) async {
+      final customerLicenseState = [
+        CustomerLicenseState.initial().copyWith(
+          isFetching: false,
+          canLoadMore: true,
+          failureOrSuccessOption: optionOf(
+            Right(customerLicensesListMock),
+          ),
+          customerLicenses: customerLicensesListMock,
+        ),
+      ];
+
+      whenListen(
+        customerLicenseBlocMock,
+        Stream.fromIterable(customerLicenseState),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      expect(find.textContaining('License information'), findsOneWidget);
+    });
+    testWidgets('should add fetch data again when refresh', (tester) async {
+      final customerLicenseBloc = locator<CustomerLicenseBloc>();
+      final customerLicenseState = [
+        CustomerLicenseState.initial(),
+        CustomerLicenseState.initial().copyWith(
+          isFetching: false,
+          canLoadMore: true,
+          failureOrSuccessOption: optionOf(
+            Right(customerLicensesListMock),
+          ),
+          customerLicenses: customerLicensesListMock,
+        ),
+      ];
+
+      whenListen(
+        customerLicenseBlocMock,
+        Stream.fromIterable(customerLicenseState),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      await tester.fling(
+        find.textContaining('License information'),
+        const Offset(0.0, 300.0),
+        1000.0,
+      );
+      await tester.pumpAndSettle();
+      verify(
+        () => customerLicenseBloc.add(
+          CustomerLicenseEvent.fetch(
+            salesOrganisation: salesOrgBlocMock.state.salesOrganisation,
+            customerInfo: eligibilityBlocMock.state.customerCodeInfo,
+            user: eligibilityBlocMock.state.user,
+          ),
+        ),
+      ).called(1);
+    });
+    testWidgets('should add load more data  when loadmore', (tester) async {
+      final customerLicenseBloc = locator<CustomerLicenseBloc>();
+      when(() => customerLicenseBlocMock.state).thenReturn(
+        CustomerLicenseState.initial().copyWith(
+          isFetching: false,
+          canLoadMore: true,
+          failureOrSuccessOption: optionOf(
+            Right(customerLicensesListMock),
+          ),
+          customerLicenses: [
+            ...customerLicensesListMock,
+            ...customerLicensesListMock,
+            ...customerLicensesListMock
+          ],
+        ),
+      );
+      whenListen(userBlockMock, userStateMock);
+      await tester.pumpWidget(getWidget());
+      await tester.drag(
+        find.byType(CustomScrollView),
+        const Offset(0.0, -10000),
+      );
+      await tester.pumpAndSettle(const Duration(microseconds: 500));
+      verify(
+        () => customerLicenseBloc.add(
+          CustomerLicenseEvent.loadMore(
+            salesOrganisation: salesOrgBlocMock.state.salesOrganisation,
+            customerInfo: eligibilityBlocMock.state.customerCodeInfo,
+            user: eligibilityBlocMock.state.user,
+          ),
+        ),
+      ).called(1);
+    });
+  });
+}
