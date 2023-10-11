5cfb0fc5679bbc1d48727cda54507606980f6616
Mainak Shil <Mshil@zuelligpharma.com>
2023-10-11 07:23:54 +0000

    Merged PR 81384: TestCoverage/EZRX-12268-mobile-v-3-test-coverage-for-soa-filter-bloc
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-12268
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - environment:
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-10-09 at 10.33.28 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/81384/attachments/Screenshot%202023-10-09%20at%2010.33.28%E2%80%AFPM.png)
    
    EZRX-12268-mobile-v-3-test-coverage-for-soa-filter-bloc
    
    EZRX-12268-mobile-v-3-test-coverage-for-soa-filter-bloc

---
+++ test/application/payments/soa/soa_filter_bloc_test.dart
@@ -0,0 +1,91 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:ezrxmobile/application/payments/soa/soa_filter/soa_filter_bloc.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/value/value_transformers.dart';
+import 'package:ezrxmobile/domain/payments/entities/soa_filter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final fakeFromDate = DateTimeStringValue(
+    getDateStringByDateTime(
+      DateTime.now().subtract(const Duration(days: 365)),
+    ),
+  );
+  final fakeToDate =
+      DateTimeStringValue(getDateStringByDateTime(DateTime.now()));
+
+  final fakeSoaFilter =
+      SoaFilter.empty().copyWith(fromDate: fakeFromDate, toDate: fakeToDate);
+
+  group('Soa Filter Bloc', () {
+    blocTest(
+      'Initialize',
+      build: () => SoaFilterBloc(),
+      act: (SoaFilterBloc bloc) => bloc.add(
+        const SoaFilterEvent.initialized(),
+      ),
+      expect: () => [
+        SoaFilterState.initial(),
+      ],
+    );
+
+    blocTest(
+      'Change Form Date Event',
+      build: () => SoaFilterBloc(),
+      act: (SoaFilterBloc bloc) => bloc.add(
+        SoaFilterEvent.changeFormDate(fakeFromDate),
+      ),
+      expect: () => [
+        SoaFilterState.initial().copyWith(
+          filter: SoaFilter.empty().copyWith(fromDate: fakeFromDate),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Change To Date Event',
+      build: () => SoaFilterBloc(),
+      act: (SoaFilterBloc bloc) => bloc.add(
+        SoaFilterEvent.changeToDate(fakeToDate),
+      ),
+      expect: () => [
+        SoaFilterState.initial().copyWith(
+          filter: SoaFilter.empty().copyWith(toDate: fakeToDate),
+        ),
+      ],
+    );
+
+    blocTest(
+      'Change Filter Event when filter is different',
+      build: () => SoaFilterBloc(),
+      seed: () => SoaFilterState.initial().copyWith(
+        filter: fakeSoaFilter.copyWith(
+          fromDate: DateTimeStringValue(
+            getDateStringByDateTime(
+              DateTime.now().subtract(const Duration(days: 65)),
+            ),
+          ),
+        ),
+      ),
+      act: (SoaFilterBloc bloc) => bloc.add(
+        SoaFilterEvent.setFilter(fakeSoaFilter),
+      ),
+      expect: () => [
+        SoaFilterState.initial().copyWith(
+          filter: fakeSoaFilter,
+        ),
+      ],
+    );
+    blocTest(
+      'Change Filter Event when filter is Same',
+      build: () => SoaFilterBloc(),
+      seed: () => SoaFilterState.initial().copyWith(
+        filter: fakeSoaFilter,
+      ),
+      act: (SoaFilterBloc bloc) => bloc.add(
+        SoaFilterEvent.setFilter(fakeSoaFilter),
+      ),
+      expect: () => [],
+    );
+  });
+}
