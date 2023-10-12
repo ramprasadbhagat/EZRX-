5b400205671cba4aa94f4e96acbfd1fefb9d6dc7
Vuong Quoc Viet (Victor) <VVuongQuoc@zuelligpharma.com>
2023-10-12 06:34:30 +0000

    Merged PR 81651: Testcase/EZRX-12470: Create widget test for filter page
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-12470
    
    ### TDD PR url
    - We need the widget test coverage for Product Filters page and sub widgets
    
    ### Root cause
    - Creating the widget test coverage for Product Filters page and sub widgets
    
    ### Output screenshot
    ![Screenshot 2023-10-12 at 09.53.15.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/81651/attachments/Screenshot%202023-10-12%20at%2009.53.15.png)
    ![Screenshot 2023-10-12 at 09.53.34.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/81651/attachments/Screenshot%202023-10-12%20at%2009.53.34.png)

---
+++ test/presentation/products/product_filter/product_filter_page_test.dart
@@ -0,0 +1,240 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:ezrxmobile/application/order/material_filter/material_filter_bloc.dart';
+import 'package:ezrxmobile/application/order/material_list/material_list_bloc.dart';
+import 'package:ezrxmobile/config.dart';
+import 'package:ezrxmobile/domain/order/entities/material_filter.dart';
+import 'package:ezrxmobile/presentation/core/widget_keys.dart';
+import 'package:ezrxmobile/presentation/products/filter_by_page/filter_by_page.dart';
+import 'package:ezrxmobile/presentation/products/product_filter/product_filter_page.dart';
+import 'package:ezrxmobile/presentation/products/widgets/enum_material_filter.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../utils/widget_utils.dart';
+
+class MockMaterialFilterBloc
+    extends MockBloc<MaterialFilterEvent, MaterialFilterState>
+    implements MaterialFilterBloc {}
+
+class MockMaterialListBloc
+    extends MockBloc<MaterialListEvent, MaterialListState>
+    implements MaterialListBloc {}
+
+final locator = GetIt.instance;
+
+void main() {
+  late AppRouter autoRouterMock;
+  late MaterialFilterBloc mockMaterialFilterBloc;
+  late MaterialListBloc mockMaterialListBloc;
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    locator.registerSingleton<Config>(Config()..appFlavor = Flavor.mock);
+    locator.registerLazySingleton(() => AppRouter());
+    locator.registerLazySingleton(() => mockMaterialFilterBloc);
+    locator.registerLazySingleton(() => mockMaterialListBloc);
+  });
+  setUp(() async {
+    mockMaterialFilterBloc = MockMaterialFilterBloc();
+    mockMaterialListBloc = MockMaterialListBloc();
+    autoRouterMock = locator<AppRouter>();
+    when(() => mockMaterialFilterBloc.state)
+        .thenReturn(MaterialFilterState.initial());
+    when(() => mockMaterialListBloc.state)
+        .thenReturn(MaterialListState.initial());
+  });
+  Widget getScopedWidget() {
+    return WidgetUtils.getScopedWidget(
+      autoRouterMock: autoRouterMock,
+      usingLocalization: true,
+      providers: [
+        BlocProvider<MaterialFilterBloc>(
+          create: (context) => mockMaterialFilterBloc,
+        ),
+        BlocProvider<MaterialListBloc>(
+          create: (context) => mockMaterialListBloc,
+        ),
+      ],
+      child: const Scaffold(
+        body: ProductFilterPage(),
+      ),
+    );
+  }
+
+  group(
+    'Product Filter Page',
+    () {
+      testWidgets(
+        'Test App bar display',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+          expect(find.text('Filter & Sort'.tr()), findsOneWidget);
+          final backButton =
+              find.widgetWithIcon(IconButton, Icons.close_outlined);
+          expect(backButton, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'Test Product Filter By Type Options',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+          expect(find.text('Show product'.tr()), findsOneWidget);
+          final favoriteTileFinder =
+              find.byKey(WidgetKeys.showProductCheckbox('Favourites'));
+          expect(
+            favoriteTileFinder,
+            findsOneWidget,
+          );
+          await tester.tap(favoriteTileFinder);
+          verify(
+            () => mockMaterialFilterBloc.add(
+              const MaterialFilterEvent.updateSelectedMaterialFilter(
+                MaterialFilterType.isFavourite,
+                true,
+              ),
+            ),
+          ).called(1);
+          final itemOffersTileFinder =
+              find.byKey(WidgetKeys.showProductCheckbox('Items with offers'));
+          expect(
+            itemOffersTileFinder,
+            findsOneWidget,
+          );
+          await tester.tap(itemOffersTileFinder);
+          verify(
+            () => mockMaterialFilterBloc.add(
+              const MaterialFilterEvent.updateSelectedMaterialFilter(
+                MaterialFilterType.productOffers,
+                true,
+              ),
+            ),
+          ).called(1);
+          final bundleOffersTileFinder =
+              find.byKey(WidgetKeys.showProductCheckbox('Bundle offers'));
+          expect(
+            bundleOffersTileFinder,
+            findsOneWidget,
+          );
+          await tester.tap(bundleOffersTileFinder);
+          verify(
+            () => mockMaterialFilterBloc.add(
+              const MaterialFilterEvent.updateSelectedMaterialFilter(
+                MaterialFilterType.bundleOffers,
+                true,
+              ),
+            ),
+          ).called(1);
+        },
+      );
+
+      testWidgets(
+        'Test Product Sort Options',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+          expect(find.text('Sort by'.tr()), findsOneWidget);
+          expect(
+            find.byKey(WidgetKeys.sortByRadio('A-Z')),
+            findsOneWidget,
+          );
+          final zARadioTile = find.byKey(WidgetKeys.sortByRadio('Z-A'));
+          expect(
+            zARadioTile,
+            findsOneWidget,
+          );
+          await tester.tap(zARadioTile);
+          verify(
+            () => mockMaterialFilterBloc.add(
+              const MaterialFilterEvent.updateSelectedMaterialFilter(
+                MaterialFilterType.sortBy,
+                Sort.za,
+              ),
+            ),
+          ).called(1);
+        },
+      );
+
+      testWidgets(
+        'Test Product Filter By Category Options',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+          expect(find.text('Filter by'.tr()), findsOneWidget);
+
+          final showCountryButton =
+              find.widgetWithText(InkWell, 'Country of origin'.tr());
+          expect(showCountryButton, findsOneWidget);
+          final showManufaturerButton =
+              find.widgetWithText(InkWell, 'Manufacturer'.tr());
+          expect(showManufaturerButton, findsOneWidget);
+          await tester.tap(showManufaturerButton);
+          await tester.pumpAndSettle();
+          expect(find.byType(FilterByPage), findsOneWidget);
+          final manufacturerSelectedFinder =
+              find.byKey(WidgetKeys.manufactureListSelectedLength);
+          expect(manufacturerSelectedFinder, findsNothing);
+        },
+      );
+
+      testWidgets(
+        'Test Product Filter By Category Options When Filter Selected',
+        (tester) async {
+          final expectedMaterialFilterStates = Stream.fromIterable(
+            [
+              MaterialFilterState.initial().copyWith(),
+              MaterialFilterState.initial().copyWith(
+                materialFilter: MaterialFilter.empty().copyWith(
+                  manufactureListSelected: ['fake-manufacturer'],
+                  countryListSelected: [MaterialFilterCountry.empty()],
+                ),
+              ),
+            ],
+          );
+          whenListen(mockMaterialFilterBloc, expectedMaterialFilterStates);
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pumpAndSettle();
+          final manufacturerSelectedFinder =
+              find.byKey(WidgetKeys.manufactureListSelectedLength);
+          expect(manufacturerSelectedFinder, findsOneWidget);
+          final countryListSelectedFinder =
+              find.byKey(WidgetKeys.countryListSelectedLength);
+          expect(countryListSelectedFinder, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'Test Product Filter Reset Button',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+
+          final resetButtonFinder = find.byKey(WidgetKeys.filterResetButton);
+          await tester.tap(resetButtonFinder);
+          verify(
+            () => mockMaterialFilterBloc.add(
+              const MaterialFilterEvent.resetFilter(),
+            ),
+          ).called(1);
+        },
+      );
+
+      testWidgets(
+        'Test Product Filter Apply Button',
+        (tester) async {
+          await tester.pumpWidget(getScopedWidget());
+          await tester.pump();
+
+          final applyButtonFinder = find.byKey(WidgetKeys.filterApplyButton);
+          await tester.tap(applyButtonFinder);
+        },
+      );
+    },
+  );
+}
