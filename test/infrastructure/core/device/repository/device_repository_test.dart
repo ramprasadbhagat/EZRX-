262f91f63154eaab30ddf9c97177b4afecf76e0e
Vuong Quoc Viet (Victor) <VVuongQuoc@zuelligpharma.com>
2023-10-13 09:47:14 +0000

    Merged PR 81846: Testcase/EZRX-12572: Create test case for device repository
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-12572
    
    ### Root cause
    - We need the test coverage for Device Repository
    
    ### Solution
    - Creating the test coverage for Device Repository
    
    ### Output screenshot
    
    ![Screenshot 2023-10-13 at 08.28.43.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/81846/attachments/Screenshot%202023-10-13%20at%2008.28.43.png)

---
+++ test/infrastructure/core/device/repository/device_repository_test.dart
@@ -0,0 +1,192 @@
+import 'package:ezrxmobile/domain/core/error/exception.dart';
+import 'package:ezrxmobile/infrastructure/core/common/device_info.dart';
+import 'package:ezrxmobile/infrastructure/core/common/permission_service.dart';
+import 'package:ezrxmobile/infrastructure/core/device/repository/device_repository.dart';
+import 'package:ezrxmobile/infrastructure/core/local_storage/device_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class MockDeviceStorage extends Mock implements DeviceStorage {}
+
+class MockPermissionService extends Mock implements PermissionService {}
+
+class MockDeviceInfo extends Mock implements DeviceInfo {}
+
+void main() {
+  late DeviceStorage mockDeviceStorage;
+  late PermissionService mockPermissionService;
+  late DeviceInfo mockDeviceInfo;
+  late DeviceRepository repository;
+  final mockException = MockException(message: 'exception');
+  const mockMarket = 'mock-market';
+
+  setUp(
+    () async {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+      mockDeviceStorage = MockDeviceStorage();
+      mockPermissionService = MockPermissionService();
+      mockDeviceInfo = MockDeviceInfo();
+      repository = DeviceRepository(
+        deviceStorage: mockDeviceStorage,
+        permissionService: mockPermissionService,
+        deviceInfo: mockDeviceInfo,
+      );
+    },
+  );
+  group(
+    'Device Repository',
+    () {
+      test('Test setDeviceData success', () async {
+        when(
+          () => mockDeviceStorage.setAppFirstLaunch(
+            isAppFirstLaunch: true,
+          ),
+        ).thenAnswer((_) => Future.value());
+        final result = await repository.setDeviceData(isAppFirstLaunch: true);
+        expect(result.isRight(), true);
+      });
+
+      test('Test setDeviceData failure', () async {
+        when(
+          () => mockDeviceStorage.setAppFirstLaunch(
+            isAppFirstLaunch: true,
+          ),
+        ).thenThrow(mockException);
+        final result = await repository.setDeviceData(isAppFirstLaunch: true);
+        expect(result.isLeft(), true);
+      });
+
+      test('Test initDeviceStorage success', () async {
+        when(
+          () => mockDeviceStorage.init(),
+        ).thenAnswer((_) => Future.value());
+        final result = await repository.initDeviceStorage();
+        expect(result.isRight(), true);
+      });
+
+      test('Test initDeviceStorage failure', () async {
+        when(
+          () => mockDeviceStorage.init(),
+        ).thenThrow(mockException);
+        final result = await repository.initDeviceStorage();
+        expect(result.isLeft(), true);
+      });
+
+      test('Test getDeviceData success', () async {
+        when(
+          () => mockDeviceStorage.isAppFirstLaunch(),
+        ).thenAnswer((_) => true);
+        final result = await repository.getDeviceData();
+        expect(result.isRight(), true);
+      });
+
+      test('Test getDeviceData failure', () async {
+        when(
+          () => mockDeviceStorage.isAppFirstLaunch(),
+        ).thenThrow(mockException);
+        final result = await repository.getDeviceData();
+        expect(result.isLeft(), true);
+      });
+
+      test('Test setCurrentMarket success', () async {
+        when(
+          () => mockDeviceStorage.putCurrentMarket(currentMarket: mockMarket),
+        ).thenAnswer((_) => Future.value());
+        final result =
+            await repository.setCurrentMarket(currentMarket: mockMarket);
+        expect(result.isRight(), true);
+      });
+
+      test('Test setCurrentMarket failure', () async {
+        when(
+          () => mockDeviceStorage.putCurrentMarket(currentMarket: mockMarket),
+        ).thenThrow(mockException);
+        final result =
+            await repository.setCurrentMarket(currentMarket: mockMarket);
+        expect(result.isLeft(), true);
+      });
+
+      test('Test getCurrentMarket success', () async {
+        when(
+          () => mockDeviceStorage.currentMarket(),
+        ).thenAnswer((_) => mockMarket);
+        final result = await repository.getCurrentMarket();
+        expect(result.isRight(), true);
+      });
+
+      test('Test getCurrentMarket failure', () async {
+        when(
+          () => mockDeviceStorage.currentMarket(),
+        ).thenThrow(mockException);
+        final result = await repository.getCurrentMarket();
+        expect(result.isLeft(), true);
+      });
+
+      test('Test getSavePermission Success With Device Is iOS', () async {
+        debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+        final result = await repository.getSavePermission();
+        expect(result.isRight(), true);
+      });
+      test('Test getSavePermission Success With Device Is Android With SDK 33',
+          () async {
+        when(
+          () => mockDeviceInfo.checkIfDeviceIsAndroidWithSDK33(),
+        ).thenAnswer((_) async => true);
+        final result = await repository.getSavePermission();
+        expect(result.isRight(), true);
+      });
+
+      test('Test getSavePermission Success With Android Permission Granted',
+          () async {
+        when(
+          () => mockDeviceInfo.checkIfDeviceIsAndroidWithSDK33(),
+        ).thenAnswer((_) async => false);
+        when(
+          () => mockPermissionService.requestStoragePermission(),
+        ).thenAnswer((_) async => PermissionStatus.granted);
+        final result = await repository.getSavePermission();
+        expect(result.isRight(), true);
+      });
+
+      test('Test getSavePermission Success With Android Permission Limited',
+          () async {
+        when(
+          () => mockDeviceInfo.checkIfDeviceIsAndroidWithSDK33(),
+        ).thenAnswer((_) async => false);
+        when(
+          () => mockPermissionService.requestStoragePermission(),
+        ).thenAnswer((_) async => PermissionStatus.limited);
+        final result = await repository.getSavePermission();
+        expect(result.isRight(), true);
+      });
+
+      test('Test getSavePermission Success With Android Do Not Have Permission',
+          () async {
+        when(
+          () => mockDeviceInfo.checkIfDeviceIsAndroidWithSDK33(),
+        ).thenAnswer((_) async => false);
+        when(
+          () => mockPermissionService.requestStoragePermission(),
+        ).thenAnswer((_) async => PermissionStatus.denied);
+        final result = await repository.getSavePermission();
+        expect(result.isLeft(), true);
+      });
+
+      test(
+        'Test getSavePermission Failure',
+        () async {
+          when(
+            () => mockDeviceInfo.checkIfDeviceIsAndroidWithSDK33(),
+          ).thenAnswer((_) async => false);
+          when(
+            () => mockPermissionService.requestStoragePermission(),
+          ).thenThrow(mockException);
+          final result = await repository.getSavePermission();
+          expect(result.isLeft(), true);
+        },
+      );
+    },
+  );
+}
