b130a21e281f8935a5b297d42271705578e2ca98
Biswanath Singh <BiSingh@zuelligpharma.com>
2023-10-16 10:51:59 +0000

    Merged PR 82091: Testcase/EZRX-12678: Test Coverage for Account Summary Repository
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-12678
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - environment:
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -
    ![Screenshot from 2023-10-16 12-43-29.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/82091/attachments/Screenshot%20from%202023-10-16%2012-43-29.png)
    
    EZRX-12678 Test Coverage for Account Summary Repository

---
+++ test/infrastructure/payments/repository/account_summary_repository_test.dart
@@ -0,0 +1,204 @@
+import 'package:ezrxmobile/config.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ezrxmobile/domain/core/error/exception.dart';
+import 'package:ezrxmobile/domain/payments/entities/credit_limit.dart';
+import 'package:ezrxmobile/domain/payments/entities/outstanding_balance.dart';
+import 'package:ezrxmobile/infrastructure/payments/datasource/account_summary_local.dart';
+import 'package:ezrxmobile/infrastructure/payments/datasource/account_summary_remote.dart';
+import 'package:ezrxmobile/infrastructure/payments/repository/account_summary_repository.dart';
+
+import '../../../common_mock_data/customer_code_mock.dart';
+import '../../../common_mock_data/sales_organsiation_mock.dart';
+
+class ConfigMock extends Mock implements Config {}
+
+class AccountSummaryLocalDataSourceMock extends Mock
+    implements AccountSummaryLocalDataSource {}
+
+class AccountSummaryRemoteDataSourceMock extends Mock
+    implements AccountSummaryRemoteDataSource {}
+
+void main() {
+  late Config configMock;
+  late CreditLimit creditLimit;
+  late OutstandingBalance outstandingBalance;
+  late AccountSummaryRepository accountSummaryRepository;
+  late AccountSummaryLocalDataSource accountSummaryLocalDataSource;
+  late AccountSummaryRemoteDataSource accountSummaryRemoteDataSource;
+
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    configMock = ConfigMock();
+    accountSummaryLocalDataSource = AccountSummaryLocalDataSourceMock();
+    accountSummaryRemoteDataSource = AccountSummaryRemoteDataSourceMock();
+    accountSummaryRepository = AccountSummaryRepository(
+      config: configMock,
+      localDataSource: accountSummaryLocalDataSource,
+      remoteDataSource: accountSummaryRemoteDataSource,
+    );
+    outstandingBalance =
+        await AccountSummaryLocalDataSource().fetchInvoiceSummary();
+    creditLimit = await AccountSummaryLocalDataSource().fetchCreditSummary();
+  });
+
+  group('Account summary Repository fetchInvoiceSummary Test', () {
+    test('fetchInvoiceSummary locally success', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.mock);
+
+      when(
+        () => accountSummaryLocalDataSource.fetchInvoiceSummary(),
+      ).thenAnswer(
+        (invocation) async => outstandingBalance,
+      );
+
+      final result = await accountSummaryRepository.fetchInvoiceSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isRight(), true);
+      expect(
+        result.getOrElse(() => OutstandingBalance.empty()),
+        outstandingBalance,
+      );
+    });
+
+    test('fetchInvoiceSummary locally fail', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.mock);
+
+      when(
+        () => accountSummaryLocalDataSource.fetchInvoiceSummary(),
+      ).thenThrow(
+        (invocation) async => MockException(),
+      );
+
+      final result = await accountSummaryRepository.fetchInvoiceSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isLeft(), true);
+    });
+
+    test('fetchInvoiceSummary remote success', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.uat);
+
+      when(
+        () => accountSummaryRemoteDataSource.fetchInvoiceSummary(
+          customerCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+          salesOrg: fakeSalesOrg.getValue(),
+        ),
+      ).thenAnswer(
+        (invocation) async => outstandingBalance,
+      );
+
+      final result = await accountSummaryRepository.fetchInvoiceSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isRight(), true);
+      expect(
+        result.getOrElse(() => OutstandingBalance.empty()),
+        outstandingBalance,
+      );
+    });
+
+    test('fetchInvoiceSummary remote fail', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.uat);
+
+      when(
+        () => accountSummaryRemoteDataSource.fetchInvoiceSummary(
+          customerCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+          salesOrg: fakeSalesOrg.getValue(),
+        ),
+      ).thenThrow(
+        (invocation) async => MockException(),
+      );
+
+      final result = await accountSummaryRepository.fetchInvoiceSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isLeft(), true);
+    });
+  });
+
+  group('Account summary Repository fetchCreditSummary Test', () {
+    test('fetchCreditSummary locally success', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.mock);
+
+      when(
+        () => accountSummaryLocalDataSource.fetchCreditSummary(),
+      ).thenAnswer(
+        (invocation) async => creditLimit,
+      );
+
+      final result = await accountSummaryRepository.fetchCreditSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isRight(), true);
+      expect(
+        result.getOrElse(() => CreditLimit.empty()),
+        creditLimit,
+      );
+    });
+
+    test('fetchCreditSummary locally fail', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.mock);
+
+      when(
+        () => accountSummaryLocalDataSource.fetchCreditSummary(),
+      ).thenThrow(
+        (invocation) async => MockException(),
+      );
+
+      final result = await accountSummaryRepository.fetchCreditSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isLeft(), true);
+    });
+
+    test('fetchCreditSummary remote success', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.uat);
+
+      when(
+        () => accountSummaryRemoteDataSource.fetchCreditLimit(
+          customerCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+          salesOrg: fakeSalesOrg.getValue(),
+        ),
+      ).thenAnswer(
+        (invocation) async => creditLimit,
+      );
+
+      final result = await accountSummaryRepository.fetchCreditSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isRight(), true);
+      expect(
+        result.getOrElse(() => CreditLimit.empty()),
+        creditLimit,
+      );
+    });
+
+    test('fetchCreditSummary remote fail', () async {
+      when(() => configMock.appFlavor).thenReturn(Flavor.uat);
+
+      when(
+        () => accountSummaryRemoteDataSource.fetchCreditLimit(
+          customerCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+          salesOrg: fakeSalesOrg.getValue(),
+        ),
+      ).thenThrow(
+        (invocation) async => MockException(),
+      );
+
+      final result = await accountSummaryRepository.fetchCreditSummary(
+        custCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+        salesOrg: fakeSalesOrg,
+      );
+      expect(result.isLeft(), true);
+    });
+  });
+}
