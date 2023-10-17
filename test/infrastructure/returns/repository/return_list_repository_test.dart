514a3f4d64b0f1e4f38286d11c45f6388558bef5
Vuong Quoc Viet (Victor) <VVuongQuoc@zuelligpharma.com>
2023-10-17 10:46:03 +0000

    Merged PR 82160: Testcase/EZRX-12684: Test coverage for return list repository
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-12684
    
    ### Root cause
    - We need the test coverage for Return List Repository
    
    ### Solution
    - Creating test coverage for Return List Repository
    
    ### Output screenshot
    ![Screenshot 2023-10-17 at 09.15.19.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/82160/attachments/Screenshot%202023-10-17%20at%2009.15.19.png)

--- test/infrastructure/returns/repository/return_list_repository_test.dart
+++ test/infrastructure/returns/repository/return_list_repository_test.dart
@@ -1,17 +1,18 @@
 import 'package:ezrxmobile/config.dart';
-import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
-import 'package:ezrxmobile/domain/account/entities/ship_to_info.dart';
-import 'package:ezrxmobile/domain/account/entities/user.dart';
-import 'package:ezrxmobile/domain/account/value/value_objects.dart';
-import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
 import 'package:ezrxmobile/domain/core/value/value_objects.dart';
 import 'package:ezrxmobile/domain/returns/entities/return_filter.dart';
 import 'package:ezrxmobile/domain/returns/entities/return_item.dart';
+import 'package:ezrxmobile/domain/returns/entities/return_list_request.dart';
 import 'package:ezrxmobile/infrastructure/returns/datasource/return_list_local.dart';
 import 'package:ezrxmobile/infrastructure/returns/datasource/return_list_remote.dart';
+import 'package:ezrxmobile/infrastructure/returns/dtos/return_list_request_dto.dart';
 import 'package:ezrxmobile/infrastructure/returns/repository/return_list_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
+import '../../../common_mock_data/customer_code_mock.dart';
+import '../../../common_mock_data/sales_organsiation_mock.dart';
+import '../../../common_mock_data/user_mock.dart';
 
 class MockConfig extends Mock implements Config {}
 
@@ -29,6 +30,19 @@ void main() {
   late Map<String, dynamic> inputParams;
 
   final returnListByRequest = [ReturnItem.empty()];
+  final errorMock = Exception('fake-error');
+  final appliedFilterMock = ReturnFilter.empty();
+  final searchKeyMock = SearchKey('fake-search-key');
+  final returnListRequest = ReturnListRequest.empty().copyWith(
+    customerCode: fakeCustomerCodeInfo.customerCodeSoldTo,
+    salesOrg: fakeSalesOrg,
+    shipToInfo: fakeCustomerCodeInfo.shipToInfos.first.shipToCustomerCode,
+    userName: fakeClient.username,
+    after: 0,
+    first: 10,
+    filter: appliedFilterMock,
+    searchKey: searchKeyMock,
+  );
 
   setUpAll(() {
     mockConfig = MockConfig();
@@ -40,51 +54,217 @@ void main() {
       localDataSource: returnListLocalDataSource,
       remoteDataSource: returnListRemoteDataSource,
     );
-    inputParams = {
-      'soldTo': '0030082707',
-      'shipTo': '0070149863',
-      'username': 'test-name',
-      'first': 24,
-      'salesOrg': '2001',
-      'after': 0,
-    };
+    inputParams = ReturnListRequestDto.fromDomain(returnListRequest).toMap();
   });
 
   group(
     'Return list repository test',
     () {
-      test('Fetch Return Item By Request', () async {
-        when(() => mockConfig.appFlavor).thenReturn(Flavor.uat);
-        when(
-          () => returnListRemoteDataSource.fetchReturnByRequest(
-            requestParams: inputParams,
-          ),
-        ).thenAnswer((invocation) async => returnListByRequest);
+      test(
+        'Fetch Return List By Item Success Locally',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.mock);
+          when(
+            () => returnListLocalDataSource.fetchReturnByItems(),
+          ).thenAnswer((invocation) async => returnListByRequest);
+          final result = await returnListRepository.fetchReturnListByItem(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isRight(), true);
+          expect(
+            result.getOrElse(
+              () => <ReturnItem>[],
+            ),
+            returnListByRequest,
+          );
+        },
+      );
 
-        final result = await returnListRepository.fetchReturnListByRequest(
-          appliedFilter: ReturnFilter.empty().copyWith(
-            returnDateFrom: DateTimeStringValue('-'),
-            returnDateTo: DateTimeStringValue('-'),
-          ),
-          customerCode: CustomerCodeInfo.empty().copyWith(
-            customerCodeSoldTo: '0030082707',
-          ),
-          offset: 0,
-          pageSize: 24,
-          salesOrg: SalesOrg('2001'),
-          searchKey: SearchKey(''),
-          shipToInfo: ShipToInfo.empty().copyWith(
-            shipToCustomerCode: '0070149863',
-          ),
-          user: User.empty().copyWith(
-            username: Username('test-name'),
-          ),
-        );
-        expect(
-          result.isRight(),
-          true,
-        );
-      });
+      test(
+        'Fetch Return List By Item Failure Locally',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.mock);
+          when(
+            () => returnListLocalDataSource.fetchReturnByItems(),
+          ).thenThrow(errorMock);
+          final result = await returnListRepository.fetchReturnListByItem(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isLeft(), true);
+        },
+      );
+
+      test(
+        'Fetch Return List By Item Success Remote',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.uat);
+          when(
+            () => returnListRemoteDataSource.fetchReturnByItems(
+              requestParams: inputParams,
+            ),
+          ).thenAnswer(
+            (invocation) async => returnListByRequest,
+          );
+          final result = await returnListRepository.fetchReturnListByItem(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isRight(), true);
+          expect(
+            result.getOrElse(
+              () => <ReturnItem>[],
+            ),
+            returnListByRequest,
+          );
+        },
+      );
+
+      test(
+        'Fetch Return List By Item Failure Remote',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.uat);
+          when(
+            () => returnListRemoteDataSource.fetchReturnByItems(
+              requestParams: inputParams,
+            ),
+          ).thenThrow(errorMock);
+          final result = await returnListRepository.fetchReturnListByItem(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isLeft(), true);
+        },
+      );
+
+      test(
+        'Fetch Return Item By Request Success Remote',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.uat);
+          when(
+            () => returnListRemoteDataSource.fetchReturnByRequest(
+              requestParams: inputParams,
+            ),
+          ).thenAnswer((invocation) async => returnListByRequest);
+
+          final result = await returnListRepository.fetchReturnListByRequest(
+            appliedFilter: appliedFilterMock,
+            customerCode: fakeCustomerCodeInfo,
+            offset: 0,
+            pageSize: 10,
+            salesOrg: fakeSalesOrg,
+            searchKey: searchKeyMock,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+          );
+          expect(
+            result.isRight(),
+            true,
+          );
+          expect(
+            result.getOrElse(
+              () => <ReturnItem>[],
+            ),
+            returnListByRequest,
+          );
+        },
+      );
+
+      test(
+        'Fetch Return List By Request Success Locally',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.mock);
+          when(
+            () => returnListLocalDataSource.fetchReturnByRequests(),
+          ).thenAnswer((invocation) async => returnListByRequest);
+          final result = await returnListRepository.fetchReturnListByRequest(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isRight(), true);
+          expect(
+            result.getOrElse(
+              () => <ReturnItem>[],
+            ),
+            returnListByRequest,
+          );
+        },
+      );
+
+      test(
+        'Fetch Return List By Request Failure Locally',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.mock);
+          when(
+            () => returnListLocalDataSource.fetchReturnByRequests(),
+          ).thenThrow(errorMock);
+          final result = await returnListRepository.fetchReturnListByRequest(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isLeft(), true);
+        },
+      );
+
+      test(
+        'Fetch Return List By Request Failure Remote',
+        () async {
+          when(() => mockConfig.appFlavor).thenReturn(Flavor.uat);
+          when(
+            () => returnListRemoteDataSource.fetchReturnByRequest(
+              requestParams: inputParams,
+            ),
+          ).thenThrow(errorMock);
+          final result = await returnListRepository.fetchReturnListByRequest(
+            salesOrg: fakeSalesOrg,
+            customerCode: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClient,
+            pageSize: 10,
+            offset: 0,
+            appliedFilter: appliedFilterMock,
+            searchKey: searchKeyMock,
+          );
+          expect(result.isLeft(), true);
+        },
+      );
     },
   );
 }
