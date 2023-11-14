4c97bdce3b86ffd109c06e69287da80094931f6a
Vuong Quoc Viet (Victor) <VVuongQuoc@zuelligpharma.com>
2023-11-14 15:16:49 +0000

    Merged PR 85408: Testcase/EZRX-13703: Widget tests for Recent order section
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-13703
    
    ### Root cause
    - We need widget tests for Recent order section
    
    ### Solution
    - create widget tests for Recent order section
    
    ### Output screenshot
    ![Screenshot 2023-11-13 at 17.09.43.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/85408/attachments/Screenshot%202023-11-13%20at%2017.09.43.png)

---
+++ test/presentation/orders/recent_order/recent_order_section_test.dart
@@ -0,0 +1,225 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:ezrxmobile/application/account/eligibility/eligibility_bloc.dart';
+import 'package:ezrxmobile/application/order/view_by_item/view_by_item_bloc.dart';
+import 'package:ezrxmobile/application/order/view_by_item_details/view_by_item_details_bloc.dart';
+import 'package:ezrxmobile/application/product_image/product_image_bloc.dart';
+import 'package:ezrxmobile/domain/order/entities/order_history.dart';
+import 'package:ezrxmobile/domain/order/entities/order_history_item.dart';
+import 'package:ezrxmobile/locator.dart';
+import 'package:ezrxmobile/presentation/core/widget_keys.dart';
+import 'package:ezrxmobile/presentation/orders/recent_order/recent_order_section.dart';
+import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../common_mock_data/customer_code_mock.dart';
+import '../../../common_mock_data/sales_organsiation_mock.dart';
+import '../../../common_mock_data/user_mock.dart';
+import '../../../utils/widget_utils.dart';
+
+class ViewByItemsBlocMock extends MockBloc<ViewByItemsEvent, ViewByItemsState>
+    implements ViewByItemsBloc {}
+
+class EligibilityBlocMock extends MockBloc<EligibilityEvent, EligibilityState>
+    implements EligibilityBloc {}
+
+class ViewByItemDetailsBlocMock
+    extends MockBloc<ViewByItemDetailsEvent, ViewByItemDetailsState>
+    implements ViewByItemDetailsBloc {}
+
+class ProductImageBlocMock
+    extends MockBloc<ProductImageEvent, ProductImageState>
+    implements ProductImageBloc {}
+
+class MockAppRouter extends Mock implements AppRouter {}
+
+void main() {
+  late ViewByItemsBloc viewByItemsBlocMock;
+  late EligibilityBloc eligibilityBlocMock;
+  late ProductImageBloc productImageBlocMock;
+  late ViewByItemDetailsBloc viewByItemDetailsBlocMock;
+  late AppRouter autoRouterMock;
+
+  setUpAll(() {
+    locator.registerLazySingleton(() => MockAppRouter());
+    eligibilityBlocMock = EligibilityBlocMock();
+    viewByItemsBlocMock = ViewByItemsBlocMock();
+    productImageBlocMock = ProductImageBlocMock();
+    viewByItemDetailsBlocMock = ViewByItemDetailsBlocMock();
+    locator.registerLazySingleton(() => viewByItemsBlocMock);
+    autoRouterMock = locator<MockAppRouter>();
+  });
+
+  setUp(() {
+    when(() => eligibilityBlocMock.state)
+        .thenReturn(EligibilityState.initial());
+    when(() => viewByItemsBlocMock.state)
+        .thenReturn(ViewByItemsState.initial());
+    when(() => productImageBlocMock.state)
+        .thenReturn(ProductImageState.initial());
+    when(() => viewByItemDetailsBlocMock.state)
+        .thenReturn(ViewByItemDetailsState.initial());
+  });
+
+  Widget getScopedWidget() {
+    return WidgetUtils.getScopedWidget(
+      autoRouterMock: autoRouterMock,
+      usingLocalization: true,
+      providers: [
+        BlocProvider<ViewByItemsBloc>(
+          create: (context) => viewByItemsBlocMock,
+        ),
+        BlocProvider<EligibilityBloc>(
+          create: (context) => eligibilityBlocMock,
+        ),
+        BlocProvider<ProductImageBloc>(
+          create: (context) => productImageBlocMock,
+        ),
+        BlocProvider<ViewByItemDetailsBloc>(
+          create: (context) => viewByItemDetailsBlocMock,
+        ),
+      ],
+      child: const Material(
+        child: RecentOrdersSection(),
+      ),
+    );
+  }
+
+  group('RecentOrderSection test - ', () {
+    testWidgets('Calling initialize when fetching eligibility done',
+        (tester) async {
+      whenListen(
+        eligibilityBlocMock,
+        Stream.fromIterable(
+          [
+            EligibilityState.initial().copyWith(
+              isLoading: true,
+            ),
+            EligibilityState.initial().copyWith(
+              salesOrgConfigs: fakeSalesOrganisationConfigs,
+              customerCodeInfo: fakeCustomerCodeInfo,
+              shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+              user: fakeClientUser,
+              salesOrganisation: fakeSalesOrganisation,
+            ),
+          ],
+        ),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pumpAndSettle();
+      verify(
+        () => viewByItemsBlocMock.add(
+          ViewByItemsEvent.initialized(
+            salesOrgConfigs: fakeSalesOrganisationConfigs,
+            customerCodeInfo: fakeCustomerCodeInfo,
+            shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+            user: fakeClientUser,
+            salesOrganisation: fakeSalesOrganisation,
+          ),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('View By Order Fetching', (tester) async {
+      when(
+        () => viewByItemsBlocMock.state,
+      ).thenAnswer(
+        (invocation) => ViewByItemsState.initial().copyWith(
+          isFetching: true,
+          orderHistory: OrderHistory.empty().copyWith(
+            orderHistoryItems: [
+              OrderHistoryItem.empty(),
+            ],
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      expect(
+        find.byKey(WidgetKeys.recentOrderSectionLoaderImage),
+        findsOneWidget,
+      );
+      final recentOrderButtonFinder =
+          find.byKey(WidgetKeys.sectionTileIcon('Recently ordered'.tr()));
+      expect(recentOrderButtonFinder, findsOneWidget);
+      await tester.tap(recentOrderButtonFinder);
+      verifyNever(() => autoRouterMock.navigate(const OrdersTabRoute()));
+    });
+
+    testWidgets('View By Order Show Items', (tester) async {
+      whenListen(
+        viewByItemsBlocMock,
+        Stream.fromIterable([
+          ViewByItemsState.initial().copyWith(
+            isFetching: true,
+          ),
+          ViewByItemsState.initial().copyWith(
+            orderHistory: OrderHistory.empty().copyWith(
+              orderHistoryItems: [
+                OrderHistoryItem.empty(),
+              ],
+            ),
+          ),
+        ]),
+      );
+      when(() => autoRouterMock.navigate(const OrdersTabRoute()))
+          .thenAnswer((invocation) => Future.value());
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pumpAndSettle();
+
+      final recentOrderButtonFinder =
+          find.byKey(WidgetKeys.sectionTileIcon('Recently ordered'.tr()));
+      expect(recentOrderButtonFinder, findsOneWidget);
+      await tester.tap(recentOrderButtonFinder);
+      verify(() => autoRouterMock.navigate(const OrdersTabRoute())).called(1);
+    });
+
+    testWidgets('Tap on Item', (tester) async {
+      final fakeOrderHistory = OrderHistory.empty().copyWith(
+        orderHistoryItems: [
+          OrderHistoryItem.empty(),
+        ],
+      );
+      when(
+        () => viewByItemsBlocMock.state,
+      ).thenAnswer(
+        (invocation) => ViewByItemsState.initial().copyWith(
+          orderHistory: fakeOrderHistory,
+        ),
+      );
+
+      when(() => eligibilityBlocMock.state).thenAnswer(
+        (invocation) => EligibilityState.initial().copyWith(
+          salesOrgConfigs: fakeSalesOrganisationConfigs,
+        ),
+      );
+
+      when(() => autoRouterMock.push(const ViewByItemDetailsPageRoute()))
+          .thenAnswer((invocation) => Future.value());
+
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(WidgetKeys.listRecentlyOrdered).first);
+      verify(
+        () => viewByItemDetailsBlocMock.add(
+          ViewByItemDetailsEvent.setItemOrderDetails(
+            orderHistory: fakeOrderHistory,
+            orderHistoryItem: fakeOrderHistory.orderHistoryItems.first,
+            disableDeliveryDateForZyllemStatus:
+                fakeSalesOrganisationConfigs.disableDeliveryDate,
+          ),
+        ),
+      ).called(1);
+      verify(
+        () => autoRouterMock.push(
+          const ViewByItemDetailsPageRoute(),
+        ),
+      ).called(1);
+    });
+  });
+}
