94d32f36dddcfebe54f543b73e79324f86ee33d4
Dipankar Das <ddas@zuelligpharma.com>
2023-11-14 14:33:39 +0000

    Merged PR 85557: TestCase/EZRX-13699 : Widget tests for View by item section
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-13699
    
    ### Output screenshot
    - ![Screenshot 2023-11-14 at 1.49.56 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/85557/attachments/Screenshot%202023-11-14%20at%201.49.56%20PM.png)
    ![Screenshot 2023-11-14 at 1.50.23 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/85557/attachments/Screenshot%202023-11-14%20at%201.50.23%20PM.png)
    
    **Description** - Test Case updated (only not able to cover super keyword, not possible I guess)

--- test/presentation/orders/order_tab/view_by_item_section_test.dart
+++ test/presentation/orders/order_tab/view_by_item_section_test.dart
@@ -5,20 +5,19 @@ import 'package:ezrxmobile/application/account/sales_org/sales_org_bloc.dart';
 import 'package:ezrxmobile/application/account/user/user_bloc.dart';
 import 'package:ezrxmobile/application/order/view_by_item/view_by_item_bloc.dart';
 import 'package:ezrxmobile/application/order/view_by_item/view_by_item_filter/view_by_item_filter_bloc.dart';
+import 'package:ezrxmobile/application/order/view_by_item_details/view_by_item_details_bloc.dart';
 import 'package:ezrxmobile/application/order/view_by_order_details/view_by_order_details_bloc.dart';
 import 'package:ezrxmobile/application/product_image/product_image_bloc.dart';
-import 'package:ezrxmobile/domain/account/entities/customer_code_info.dart';
-import 'package:ezrxmobile/domain/account/entities/role.dart';
-import 'package:ezrxmobile/domain/account/entities/sales_organisation.dart';
-import 'package:ezrxmobile/domain/account/entities/ship_to_info.dart';
-import 'package:ezrxmobile/domain/account/entities/user.dart';
-import 'package:ezrxmobile/domain/account/value/value_objects.dart';
-import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/domain/order/entities/invoice_data.dart';
 import 'package:ezrxmobile/domain/order/entities/order_history.dart';
 import 'package:ezrxmobile/domain/order/entities/order_history_item.dart';
+import 'package:ezrxmobile/domain/order/entities/view_by_item_filter.dart';
 import 'package:ezrxmobile/infrastructure/core/http/http.dart';
+import 'package:ezrxmobile/infrastructure/core/mixpanel/mixpanel_service.dart';
 import 'package:ezrxmobile/infrastructure/order/datasource/view_by_item_local.dart';
 import 'package:ezrxmobile/locator.dart';
+import 'package:ezrxmobile/presentation/core/loading_shimmer/loading_shimmer.dart';
 import 'package:ezrxmobile/presentation/core/widget_keys.dart';
 import 'package:ezrxmobile/presentation/orders/order_tab/section/view_by_item/view_by_item_section.dart';
 import 'package:ezrxmobile/presentation/routes/router.gr.dart';
@@ -59,46 +58,58 @@ class ProductImageMockBloc
     extends MockBloc<ProductImageEvent, ProductImageState>
     implements ProductImageBloc {}
 
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  WidgetsFlutterBinding.ensureInitialized();
-  final mockViewByItemsBloc = ViewByItemsBlocMock();
-  final mockViewByOrderDetailsBloc = ViewByOrderDetailsBlockMock();
-  final mockSalesOrgBloc = SalesOrgMockBloc();
-  final userBlocMock = UserMockBloc();
-  final mockViewByItemFilterBloc = ViewByItemFilterMockBloc();
-  final mockProductImageBloc = ProductImageMockBloc();
+class MockMixpanelService extends Mock implements MixpanelService {}
 
+class ViewByItemDetailsMockBloc
+    extends MockBloc<ViewByItemDetailsEvent, ViewByItemDetailsState>
+    implements ViewByItemDetailsBloc {}
+
+void main() {
   late CustomerCodeBloc customerCodeBlocMock;
   late AppRouter autoRouterMock;
   late OrderHistory orderHistory;
-
   late EligibilityBlocMock eligibilityBlocMock;
-  final fakeUser = User.empty().copyWith(
-    username: Username('fake-user'),
-    role: Role.empty().copyWith(
-      type: RoleType('client'),
+  late ViewByItemsBloc mockViewByItemsBloc;
+  late ViewByOrderDetailsBloc mockViewByOrderDetailsBloc;
+  late SalesOrgBloc mockSalesOrgBloc;
+  late UserMockBloc userBlocMock;
+  late ViewByItemFilterMockBloc mockViewByItemFilterBloc;
+  late ProductImageMockBloc mockProductImageBloc;
+  late ViewByItemDetailsMockBloc mockViewByItemDetailsBloc;
+
+  final fakeOrderHistoryItems = [
+    OrderHistoryItem.empty().copyWith(
+      manufactureName: 'fake_manufactureName_1',
+      createdDate: DateTimeStringValue('2023041107'),
     ),
-    enableOrderType: true,
-  );
+    OrderHistoryItem.empty().copyWith(
+      manufactureName: 'fake_manufactureName_2',
+      createdDate: DateTimeStringValue('2023041307'),
+    ),
+  ];
 
   setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    WidgetsFlutterBinding.ensureInitialized();
     locator.registerLazySingleton(() => AppRouter());
-    registerFallbackValue(CustomerCodeInfo.empty());
-    registerFallbackValue(SalesOrganisation.empty());
-    registerFallbackValue(ShipToInfo.empty());
+    locator.registerLazySingleton<MixpanelService>(() => MockMixpanelService());
     orderHistory = await ViewByItemLocalDataSource().getViewByItems();
-
     autoRouterMock = locator<AppRouter>();
+    customerCodeBlocMock = CustomerCodeBlocMock();
+    eligibilityBlocMock = EligibilityBlocMock();
+    mockViewByItemsBloc = ViewByItemsBlocMock();
+    mockViewByOrderDetailsBloc = ViewByOrderDetailsBlockMock();
+    mockSalesOrgBloc = SalesOrgMockBloc();
+    userBlocMock = UserMockBloc();
+    mockViewByItemFilterBloc = ViewByItemFilterMockBloc();
+    mockProductImageBloc = ProductImageMockBloc();
+    mockViewByItemDetailsBloc = ViewByItemDetailsMockBloc();
   });
+
   group('View by Item In Order section', () {
     setUp(() {
-      customerCodeBlocMock = CustomerCodeBlocMock();
-      eligibilityBlocMock = EligibilityBlocMock();
       when(() => userBlocMock.state).thenReturn(
-        UserState.initial().copyWith(
-          user: fakeUser,
-        ),
+        UserState.initial(),
       );
       when(() => mockViewByItemsBloc.state)
           .thenReturn(ViewByItemsState.initial());
@@ -108,27 +119,14 @@ void main() {
           .thenReturn(ViewByOrderDetailsState.initial());
       when(() => mockSalesOrgBloc.state).thenReturn(SalesOrgState.initial());
       when(() => eligibilityBlocMock.state).thenReturn(
-        EligibilityState.initial().copyWith(
-          user: User.empty().copyWith(
-            role: Role(
-              description: '',
-              name: '',
-              id: '',
-              type: RoleType('client'),
-            ),
-          ),
-          salesOrganisation:
-              SalesOrganisation.empty().copyWith(salesOrg: SalesOrg('SG')),
-          customerCodeInfo: CustomerCodeInfo.empty().copyWith(
-            customerAttr7: CustomerAttr7('ZEV'),
-            customerGrp4: CustomerGrp4('VR'),
-          ),
-        ),
+        EligibilityState.initial(),
       );
       when(() => mockViewByItemFilterBloc.state)
           .thenReturn(ViewByItemFilterState.initial());
       when(() => mockProductImageBloc.state)
           .thenReturn(ProductImageState.initial());
+      when(() => mockViewByItemDetailsBloc.state)
+          .thenReturn(ViewByItemDetailsState.initial());
     });
     Widget getScopedWidget() {
       return WidgetUtils.getScopedWidget(
@@ -155,6 +153,9 @@ void main() {
           BlocProvider<ProductImageBloc>(
             create: (context) => mockProductImageBloc,
           ),
+          BlocProvider<ViewByItemDetailsBloc>(
+            create: (context) => mockViewByItemDetailsBloc,
+          ),
         ],
         child: const Material(
           child: ViewByItemsPage(),
@@ -176,22 +177,160 @@ void main() {
       );
       expect(loaderImage, findsOneWidget);
     });
+
     testWidgets('Displaying manufactureName  test ', (tester) async {
       when(() => mockViewByItemsBloc.state).thenReturn(
         ViewByItemsState.initial().copyWith(
           isFetching: false,
           orderHistory: orderHistory.copyWith(
-            orderHistoryItems: [
-              OrderHistoryItem.empty()
-                  .copyWith(manufactureName: 'fake_manufactureName')
-            ],
+            orderHistoryItems: fakeOrderHistoryItems,
           ),
         ),
       );
       await tester.pumpWidget(getScopedWidget());
       await tester.pump();
-      final manufactureName = find.text('fake_manufactureName');
-      expect(manufactureName, findsOneWidget);
+      expect(find.text('fake_manufactureName_1'), findsOneWidget);
+      expect(find.text('fake_manufactureName_2'), findsOneWidget);
+    });
+
+    testWidgets('Items - On refresh Feature', (tester) async {
+      when(() => mockViewByItemsBloc.state).thenReturn(
+        ViewByItemsState.initial().copyWith(
+          isFetching: false,
+          orderHistory: orderHistory.copyWith(
+            orderHistoryItems: fakeOrderHistoryItems,
+          ),
+        ),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      await tester.drag(
+        find.textContaining('fake_manufactureName_1'),
+        const Offset(0.0, 1000.0),
+      );
+      await tester.pump(const Duration(seconds: 1));
+      expect(
+        tester.getSemantics(find.byType(RefreshProgressIndicator)),
+        matchesSemantics(
+          label: 'Refresh',
+        ),
+      );
+
+      await tester.pump(const Duration(seconds: 1));
+      verify(
+        () => mockViewByItemFilterBloc
+            .add(const ViewByItemFilterEvent.initialize()),
+      ).called(1);
+      verify(
+        () => mockViewByItemsBloc.add(
+          ViewByItemsEvent.fetch(
+            viewByItemFilter: ViewByItemFilter.empty(),
+            searchKey: SearchKey(''),
+          ),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('Items - On loadMore Feature', (tester) async {
+      when(() => mockViewByItemsBloc.state).thenReturn(
+        ViewByItemsState.initial().copyWith(
+          orderHistory: orderHistory,
+          canLoadMore: true,
+        ),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      await tester.fling(
+        find.byKey(WidgetKeys.scrollList),
+        const Offset(0.0, -5000.0),
+        800.0,
+      );
+      verify(
+        () => mockViewByItemsBloc.add(
+          const ViewByItemsEvent.loadMore(),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('Tap on view by item Tile ', (tester) async {
+      final orderHistoryList = orderHistory.copyWith(
+        orderHistoryItems: fakeOrderHistoryItems,
+      );
+      when(() => mockViewByItemsBloc.state).thenReturn(
+        ViewByItemsState.initial().copyWith(
+          isFetching: false,
+          orderHistory: orderHistoryList,
+        ),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      await tester.tap(
+        find.text(orderHistoryList.orderHistoryItems.first.manufactureName),
+      );
+      verify(
+        () => mockViewByItemDetailsBloc.add(
+          ViewByItemDetailsEvent.setItemOrderDetails(
+            orderHistory: orderHistoryList,
+            orderHistoryItem: orderHistoryList.orderHistoryItems.first,
+            disableDeliveryDateForZyllemStatus: false,
+          ),
+        ),
+      ).called(1);
+      expect(
+        autoRouterMock.current.name,
+        'ViewByItemDetailsPageRoute',
+      );
+    });
+
+    testWidgets('Invoice Number Loading', (tester) async {
+      final orderHistoryList = orderHistory.copyWith(
+        orderHistoryItems: [
+          fakeOrderHistoryItems.first.copyWith(
+            invoiceData: InvoiceData.empty()
+                .copyWith(invoiceNumber: StringValue('fake-Invoice')),
+          ),
+        ],
+      );
+      when(() => mockViewByItemsBloc.state).thenReturn(
+        ViewByItemsState.initial().copyWith(
+          isFetchingInvoices: false,
+          orderHistory: orderHistoryList,
+        ),
+      );
+      final expectedState = [
+        ViewByItemsState.initial().copyWith(
+          isFetchingInvoices: true,
+          orderHistory: orderHistoryList,
+        ),
+      ];
+      whenListen(
+        mockViewByItemsBloc,
+        Stream.fromIterable(expectedState),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      expect(find.byType(LoadingShimmer), findsOneWidget);
+    });
+
+    testWidgets('Invoice Number Loaded', (tester) async {
+      final fakeInvoice = StringValue('fake-Invoice');
+      final orderHistoryList = orderHistory.copyWith(
+        orderHistoryItems: [
+          fakeOrderHistoryItems.first.copyWith(
+            invoiceData:
+                InvoiceData.empty().copyWith(invoiceNumber: fakeInvoice),
+          ),
+        ],
+      );
+      when(() => mockViewByItemsBloc.state).thenReturn(
+        ViewByItemsState.initial().copyWith(
+          isFetchingInvoices: true,
+          orderHistory: orderHistoryList,
+        ),
+      );
+      await tester.pumpWidget(getScopedWidget());
+      await tester.pump();
+      expect(find.text('Invoice #${fakeInvoice.getOrCrash()}'), findsOneWidget);
     });
   });
 }
