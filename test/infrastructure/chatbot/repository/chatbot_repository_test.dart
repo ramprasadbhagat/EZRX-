02d92bfc0e0c1d99c9fb68f285f8a05a443bba33
Dipankar Das <ddas@zuelligpharma.com>
2023-11-24 15:06:50 +0000

    Merged PR 86759: TestCase/EZRX-14549: Test cases for Chatbot repository
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-14549
    
    ### Output screenshot
    - ![Screenshot 2023-11-23 at 3.03.32 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/86759/attachments/Screenshot%202023-11-23%20at%203.03.32%20PM.png)
    
    **Description** - It's 95% done, only one line we are not able to cover that is poor network connection, we are not able to setup, If possible anyone can suggest me for this

---
+++ test/infrastructure/chatbot/repository/chatbot_repository_test.dart
@@ -0,0 +1,246 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:ezrxmobile/config.dart';
+import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/infrastructure/auth/dtos/jwt_dto.dart';
+import 'package:ezrxmobile/infrastructure/chatbot/repository/chatbot_repository.dart';
+import 'package:ezrxmobile/infrastructure/core/chatbot/chatbot_service.dart';
+import 'package:ezrxmobile/infrastructure/core/local_storage/device_storage.dart';
+import 'package:ezrxmobile/infrastructure/core/local_storage/token_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../common_mock_data/customer_code_mock.dart';
+import '../../../common_mock_data/sales_organsiation_mock.dart';
+import '../../../common_mock_data/user_mock.dart';
+
+class ConfigMock extends Mock implements Config {}
+
+class ChatBotServiceMock extends Mock implements ChatBotService {}
+
+class TokenStorageMock extends Mock implements TokenStorage {}
+
+class DeviceStorageMock extends Mock implements DeviceStorage {}
+
+class InternetAddressMock extends Mock implements InternetAddress {}
+
+void main() {
+  late Config configMock;
+  late ChatBotService chatBotService;
+  late TokenStorage tokenStorageMock;
+  late DeviceStorage deviceStorageMock;
+  late JWTDto jwtDto;
+  late EmailAddress fakeEmail;
+  late ChatBotRepository chatBotRepository;
+  late Exception errorMock;
+
+
+  setUpAll(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      configMock = Config()..appFlavor = Flavor.uat;
+      chatBotService = ChatBotServiceMock();
+      tokenStorageMock = TokenStorageMock();
+      deviceStorageMock = DeviceStorageMock();
+      jwtDto = JWTDto(
+        access: 'access_token',
+        refresh: 'refresh_token',
+      );
+      fakeEmail = EmailAddress('fakeemail@zuelligpharma.com');
+      errorMock = Exception('fake-error');
+      chatBotRepository = ChatBotRepository(
+        chatBotService: chatBotService,
+        tokenStorage: tokenStorageMock,
+        config: configMock..appFlavor = Flavor.uat,
+        deviceStorage: deviceStorageMock,
+      );
+      when(
+        () => deviceStorageMock.currentMarket(),
+      ).thenReturn('my');
+      when(() => tokenStorageMock.get())
+          .thenAnswer((invocation) async => jwtDto);
+    },
+  );
+
+  group('Chat Bot Repository', () {
+    test('load chat bot remote success', () async {
+      when(
+        () => chatBotService.startChatbot(),
+      ).thenAnswer(
+        (invocation) async => true,
+      );
+
+      final result = await chatBotRepository.startChatbot();
+      expect(result, const Right(true));
+    });
+
+    test('load chat bot remote failure', () async {
+      when(
+        () => chatBotService.startChatbot(),
+      ).thenThrow(
+        errorMock,
+      );
+      final result = await chatBotRepository.startChatbot();
+      expect(result, Left(ApiFailure.other(errorMock.toString())));
+    });
+
+    test('pass payload to chat bot remote success', () async {
+      final toDate = DateTime.now();
+      final dateFormat = DateFormat('yyyyMMdd');
+      final toDateStringValue = dateFormat.format(toDate);
+      final fromDateStringValue = dateFormat
+          .format(DateTime(toDate.year, toDate.month - 2, toDate.day));
+      final payload = <String, Object>{
+        'soldTo': fakeCustomerCodeInfo.customerCodeSoldTo,
+        'shipTo': [fakeCustomerCodeInfo.shipToInfos.first.shipToCustomerCode],
+        'language': fakeClientUser.preferredLanguage.languageCode,
+        'token': 'access_token',
+        'username': fakeClientUser.username.getOrCrash(),
+        'customerCode': fakeCustomerCodeInfo.customerCodeSoldTo,
+        'market': fakeMYSalesOrganisation.salesOrg.country,
+        'userRole': fakeClientUser.role.name,
+        'salesorg': fakeMYSalesOrganisation.salesOrg.getOrCrash(),
+        'locale': fakeMYSalesOrganisation.salesOrg.locale.toLanguageTag(),
+        'fromDate': fromDateStringValue,
+        'toDate': toDateStringValue,
+        'currency': fakeSalesOrganisationConfigs
+            .copyWith(salesOrg: fakeMYSalesOrg)
+            .currency
+            .code,
+        'name': fakeClientUser.fullName.displayFullName,
+        'email': fakeClientUser
+            .copyWith(email: fakeEmail)
+            .email
+            .getOrCrash(),
+        'baseUrl': configMock.baseUrl(
+          currentMarket: AppMarket(
+            deviceStorageMock.currentMarket(),
+          ),
+        ),
+      };
+      when(
+        () => chatBotService.passPayloadToBot(payload: payload),
+      ).thenAnswer(
+        (invocation) async => true,
+      );
+
+      final result = await chatBotRepository.passPayloadToChatbot(
+        customerCodeInfo: fakeCustomerCodeInfo,
+        salesOrganisation: fakeMYSalesOrganisation,
+        salesOrganisationConfigs:
+            fakeSalesOrganisationConfigs.copyWith(salesOrg: fakeMYSalesOrg),
+        shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+        user: fakeClientUser.copyWith(
+          email: fakeEmail,
+        ),
+      );
+
+      expect(result, equals(const Right(true)));
+    });
+
+    test('pass payload to chat bot remote failure', () async {
+      final toDate = DateTime.now();
+      final dateFormat = DateFormat('yyyyMMdd');
+      final toDateStringValue = dateFormat.format(toDate);
+      final fromDateStringValue = dateFormat
+          .format(DateTime(toDate.year, toDate.month - 2, toDate.day));
+      final payload = <String, Object>{
+        'soldTo': fakeCustomerCodeInfo.customerCodeSoldTo,
+        'shipTo': [fakeCustomerCodeInfo.shipToInfos.first.shipToCustomerCode],
+        'language': fakeClientUser.preferredLanguage.languageCode,
+        'token': 'access_token',
+        'username': fakeClientUser.username.getOrCrash(),
+        'customerCode': fakeCustomerCodeInfo.customerCodeSoldTo,
+        'market': fakeMYSalesOrganisation.salesOrg.country,
+        'userRole': fakeClientUser.role.name,
+        'salesorg': fakeMYSalesOrganisation.salesOrg.getOrCrash(),
+        'locale': fakeMYSalesOrganisation.salesOrg.locale.toLanguageTag(),
+        'fromDate': fromDateStringValue,
+        'toDate': toDateStringValue,
+        'currency': fakeSalesOrganisationConfigs
+            .copyWith(salesOrg: fakeMYSalesOrg)
+            .currency
+            .code,
+        'name': fakeClientUser.fullName.displayFullName,
+        'email': fakeClientUser.copyWith(email: fakeEmail).email.getOrCrash(),
+        'baseUrl': configMock.baseUrl(
+          currentMarket: AppMarket(
+            deviceStorageMock.currentMarket(),
+          ),
+        ),
+      };
+      when(
+        () => chatBotService.passPayloadToBot(payload: payload),
+      ).thenThrow(
+        errorMock,
+      );
+
+      final result = await chatBotRepository.passPayloadToChatbot(
+        customerCodeInfo: fakeCustomerCodeInfo,
+        salesOrganisation: fakeMYSalesOrganisation,
+        salesOrganisationConfigs:
+            fakeSalesOrganisationConfigs.copyWith(salesOrg: fakeMYSalesOrg),
+        shipToInfo: fakeCustomerCodeInfo.shipToInfos.first,
+        user: fakeClientUser.copyWith(
+          email: fakeEmail,
+        ),
+      );
+
+      expect(result, Left(ApiFailure.other(errorMock.toString())));
+    });
+
+    test('pass payload to reset chat bot remote success', () async {
+      final market = AppMarket(
+        deviceStorageMock.currentMarket(),
+      );
+      final marketDomain = market.marketDomain.toUpperCase();
+      final payload = <String, Object>{
+        'token': '',
+        'username': '',
+        'baseUrl': configMock.baseUrl(
+          currentMarket: market,
+        ),
+        'market': marketDomain,
+        'locale': market.locale,
+        'currency': market.currency,
+        'language': marketDomain,
+      };
+      when(
+        () => chatBotService.passPayloadToBot(payload: payload),
+      ).thenAnswer(
+        (invocation) async => true,
+      );
+      final result = await chatBotRepository.resetChatbot();
+      expect(result, const Right(true));
+    });
+
+    test('pass payload to reset chat bot remote failure', () async {
+      final market = AppMarket(
+        deviceStorageMock.currentMarket(),
+      );
+      final marketDomain = market.marketDomain.toUpperCase();
+      final payload = <String, Object>{
+        'token': '',
+        'username': '',
+        'baseUrl': configMock.baseUrl(
+          currentMarket: market,
+        ),
+        'market': marketDomain,
+        'locale': market.locale,
+        'currency': market.currency,
+        'language': marketDomain,
+      };
+      when(
+        () => chatBotService.passPayloadToBot(payload: payload),
+      ).thenThrow(
+        errorMock,
+      );
+      final result = await chatBotRepository.resetChatbot();
+      expect(result, Left(ApiFailure.other(errorMock.toString())));
+    });
+  });
+}
