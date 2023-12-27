3c80cdb69ecefa38b9ba3f1568c7278d75dc67e1
Mainak Shil <Mshil@zuelligpharma.com>
2023-12-27 12:24:08 +0000

    Merged PR 90032: Testcases/EZRX-15772-mobile-v-3-widget-test-for-password-confirmation-page
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-15772
    
    ### TDD PR url
    -
    
    ### Root cause
    -
    
    ### Solution
    -
    
    ### Test account
    - environment:
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -![Screenshot 2023-12-19 at 9.25.34 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/90032/attachments/Screenshot%202023-12-19%20at%209.25.34%E2%80%AFPM.png)
    
    EZRX-15772-mobile-v-3-widget-test-for-password-confirmation-page
    
    EZRX-15772-mobile-v-3-widget-test-for-password-confirmation-page

--- test/presentation/auth/forgot_password/forgot_password_confirmation_page_test.dart
+++ test/presentation/auth/forgot_password/forgot_password_confirmation_page_test.dart
@@ -1,13 +1,19 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:ezrxmobile/application/auth/forgot_password/forgot_password_bloc.dart';
 import 'package:ezrxmobile/config.dart';
 import 'package:ezrxmobile/domain/auth/entities/forgot_password.dart';
 import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
 import 'package:ezrxmobile/locator.dart';
 import 'package:ezrxmobile/presentation/auth/forgot_password/widgets/back_to_login_button.dart';
 import 'package:ezrxmobile/presentation/auth/forgot_password/forgot_password_confirmation_page.dart';
 import 'package:ezrxmobile/presentation/core/logo.dart';
+import 'package:ezrxmobile/presentation/core/snack_bar/custom_snackbar.dart';
+import 'package:ezrxmobile/presentation/core/widget_keys.dart';
 import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -86,5 +92,99 @@ void main() {
         findsOneWidget,
       );
     });
+
+    testWidgets(' -> Check failure snackbar', (WidgetTester tester) async {
+      final expectedState = [
+        ForgotPasswordState.initial().copyWith(
+          resetPasswordFailureOrSuccessOption:
+              optionOf(const Left(ApiFailure.other('fake_error'))),
+        ),
+      ];
+      whenListen(forgotPasswordBlocMock, Stream.fromIterable(expectedState));
+      await tester.pumpWidget(getWidget());
+      await tester.pumpAndSettle();
+      expect(
+        find.byType(
+          CustomSnackBar,
+        ),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets(' -> test Forget Password Confirmation Page body loading',
+        (WidgetTester tester) async {
+      when(() => forgotPasswordBlocMock.state).thenReturn(
+        ForgotPasswordState.initial().copyWith(
+          isSubmitting: true,
+        ),
+      );
+      await tester.pumpWidget(getWidget());
+      await tester.pump();
+      expect(
+        find.byKey(WidgetKeys.loaderImage),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets(' -> test Forget Password Confirmation Page body',
+        (WidgetTester tester) async {
+      when(() => forgotPasswordBlocMock.state).thenReturn(
+        ForgotPasswordState.initial().copyWith(
+          username: Username('fake.username'),
+          resetPasswordResponse: ForgotPassword.empty().copyWith(
+            email: 'fake.username@email.com',
+            success: true,
+          ),
+        ),
+      );
+      await tester.pumpWidget(getWidget());
+      await tester.pumpAndSettle();
+      expect(
+        find.byType(Logo),
+        findsOneWidget,
+      );
+      expect(
+        find.textContaining(
+          'fake.username@email.com',
+          findRichText: true,
+        ),
+        findsOneWidget,
+      );
+      final resendEmailFinder = find.textContaining(
+        'Resend Email',
+        findRichText: true,
+      );
+      expect(
+        resendEmailFinder,
+        findsOneWidget,
+      );
+      final finder = find.byWidgetPredicate(
+        (widget) => widget is RichText && tapTextSpan(widget, 'Resend Email'),
+      );
+      await tester.pumpAndSettle();
+      expect(finder, findsOneWidget);
+      verify(
+        () => forgotPasswordBlocMock
+            .add(ForgotPasswordEvent.requestPasswordReset(Language.english())),
+      ).called(1);
+    });
   });
+}
+
+bool findTextAndTap(InlineSpan visitor, String text) {
+  if (visitor is TextSpan && visitor.text == text) {
+    (visitor.recognizer as TapGestureRecognizer).onTap!();
+
+    return false;
+  }
+
+  return true;
+}
+
+bool tapTextSpan(RichText richText, String text) {
+  final isTapped = !richText.text.visitChildren(
+    (visitor) => findTextAndTap(visitor, text),
+  );
+
+  return isTapped;
 }
