0804b3a6c0773d2f2ed209e9d483379917938c1e
Joseph Stalin <jstalin@zuelligpharma.com>
2024-01-16 06:51:03 +0000

    Merged PR 92624: TestCase/EZRX-16502 Unit tests for deep linking bloc
    ### Jira ticket url
    -  https://zuelligpharma.atlassian.net/browse/EZRX-16502
    
    ### Output screenshot
    - ![Screenshot 2024-01-15 at 5.43.19 PM.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/92624/attachments/Screenshot%202024-01-15%20at%205.43.19%E2%80%AFPM.png)
    
    EZRX-16502 Unit tests for deep linking bloc

--- test/application/deep_linking/deep_linking_bloc_test.dart
+++ test/application/deep_linking/deep_linking_bloc_test.dart
@@ -1,24 +1,25 @@
 import 'dart:async';
-
-import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
-import 'package:ezrxmobile/application/deep_linking/deep_linking_bloc.dart';
-import 'package:ezrxmobile/domain/core/error/api_failures.dart';
-import 'package:ezrxmobile/domain/deep_linking/repository/i_deep_linking_repository.dart';
-import 'package:ezrxmobile/domain/order/value/value_objects.dart';
-import 'package:ezrxmobile/infrastructure/core/chatbot/chatbot_service.dart';
-import 'package:ezrxmobile/infrastructure/core/deep_linking/deep_linking_service.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ezrxmobile/domain/core/error/api_failures.dart';
+import 'package:ezrxmobile/domain/core/value/value_objects.dart';
+import 'package:ezrxmobile/domain/order/value/value_objects.dart';
+import 'package:ezrxmobile/domain/auth/value/value_objects.dart';
+import 'package:ezrxmobile/domain/returns/entities/return_requests_id.dart';
+import 'package:ezrxmobile/application/deep_linking/deep_linking_bloc.dart';
+import 'package:ezrxmobile/infrastructure/core/chatbot/chatbot_service.dart';
+import 'package:ezrxmobile/domain/payments/entities/payment_summary_details.dart';
+import 'package:ezrxmobile/infrastructure/core/deep_linking/deep_linking_service.dart';
+import 'package:ezrxmobile/domain/deep_linking/repository/i_deep_linking_repository.dart';
 
+import '../../common_mock_data/mock_other.dart';
 import '../../common_mock_data/customer_code_mock.dart';
 
 class DeepLinkingRepositoryMock extends Mock
     implements IDeepLinkingRepository {}
 
-class UniversalLinkServiceMock extends Mock implements DeepLinkingService {}
-
-class ChatBotServiceMock extends Mock implements ChatBotService {}
 
 void main() {
   late IDeepLinkingRepository repository;
@@ -28,9 +29,25 @@ void main() {
   final fakeSubscription = fakeStream.listen((_) {});
   final fakeCustomerCode = fakeCustomerCodeInfo;
   final fakeShipToCode = fakeShipToInfo;
+  const fakeError = ApiFailure.other('fake-error');
+  final materialNumber = MaterialNumber('000000000011002079');
   const orderDetailLink = '/my-account/orders/order-detail';
+  const contactUsLink = '/contact-us';
+  const productDetailsLink = '/product-details';
+  const bundleDetailsLink = '/bundle-details';
+  const productListingLink = '/product-listing';
+  const returnSummaryLink = '/my-account/return-summary-details';
+  const paymentSummaryInvoiceDetailsLink =
+      '/payments/payment-summary/invoice-details';
+const accountSummaryInvoiceDetailsLink =
+      '/payments/account-summary/invoice-details';
+  const paymentLink = '/my-account/payments';
+  const faqLink = '/faq';
 
-  setUp(() {
+
+
+
+  setUpAll(() {
     repository = DeepLinkingRepositoryMock();
     service = UniversalLinkServiceMock();
     chatBotService = ChatBotServiceMock();
@@ -169,4 +186,457 @@ void main() {
       const DeepLinkingState.error(ApiFailure.orderDetailRoute()),
     ],
   );
+
+  group('Redirect contact-us', () {
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect contact-us success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.getCurrentMarket(),
+        ).thenReturn(Right(AppMarket.malaysia()));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: contactUsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectContactUs(AppMarket.malaysia()),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect contact-us failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.getCurrentMarket(),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: contactUsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+  });
+
+  group('Product Redirection ', () {
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect product details success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractMaterialNumber(
+            link: Uri(path: productDetailsLink),
+          ),
+        ).thenReturn(Right(materialNumber));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: productDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectProductDetail(materialNumber),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect product details failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractMaterialNumber(
+            link: Uri(path: productDetailsLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: productDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect bundle details success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractMaterialNumber(
+            link: Uri(path: bundleDetailsLink),
+          ),
+        ).thenReturn(Right(materialNumber));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: bundleDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectBundleDetail(materialNumber),
+      ],
+    );
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect bundle details failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractMaterialNumber(
+            link: Uri(path: bundleDetailsLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: bundleDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect product listing success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractProductSearchKey(
+            link: Uri(path: productListingLink),
+          ),
+        ).thenReturn(Right(SearchKey(materialNumber.getValue())));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: productListingLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectProductSuggestion(
+          SearchKey(materialNumber.getValue()),
+        ),
+      ],
+    );
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect product listing failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractProductSearchKey(
+            link: Uri(path: productListingLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: productListingLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+  });
+
+  group('Return redirection', () {
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Return Summary success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractReturnId(
+            selectedCustomerCode: fakeCustomerCode,
+            selectedShipTo: fakeShipToCode,
+            link: Uri(path: returnSummaryLink),
+          ),
+        ).thenReturn(Right(ReturnRequestsId(requestId: 'fake-request-id')));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: returnSummaryLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectReturnDetail(
+          ReturnRequestsId(requestId: 'fake-request-id'),
+        ),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Return Summary failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractReturnId(
+            selectedCustomerCode: fakeCustomerCode,
+            selectedShipTo: fakeShipToCode,
+            link: Uri(path: returnSummaryLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: returnSummaryLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+  });
+
+  group('Payment redirection', () {
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Payment home success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: paymentLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.redirectPaymentHome(),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Payment Summary invoice Details success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractPaymentIdentifierInfo(
+            link: Uri(path: paymentSummaryInvoiceDetailsLink),
+          ),
+        ).thenReturn(
+          Right(
+            PaymentSummaryDetails.empty().copyWith(
+              paymentID: StringValue('fake-payment-id'),
+            ),
+          ),
+        );
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: paymentSummaryInvoiceDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        DeepLinkingState.redirectPaymentDetail(
+          PaymentSummaryDetails.empty().copyWith(
+            paymentID: StringValue('fake-payment-id'),
+          ),
+        ),
+      ],
+    );
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Payment Summary invoice Details failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractPaymentIdentifierInfo(
+            link: Uri(path: paymentSummaryInvoiceDetailsLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: paymentSummaryInvoiceDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Account Summary invoice Details success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractInvoiceNumber(
+            link: Uri(path: accountSummaryInvoiceDetailsLink),
+          ),
+        ).thenReturn(const Right('fake-invoice-number'));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: accountSummaryInvoiceDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.redirectInvoiceDetail('fake-invoice-number'),
+      ],
+    );
+
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect Account Summary invoice Details failure',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      setUp: () {
+        when(
+          () => repository.extractInvoiceNumber(
+            link: Uri(path: accountSummaryInvoiceDetailsLink),
+          ),
+        ).thenReturn(const Left(fakeError));
+      },
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: accountSummaryInvoiceDetailsLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.error(fakeError),
+      ],
+    );
+  });
+group('Redirect FAQ', () {
+    blocTest<DeepLinkingBloc, DeepLinkingState>(
+      'Consume redirect FAQ success',
+      build: () => DeepLinkingBloc(
+        service: service,
+        repository: repository,
+        chatBotService: chatBotService,
+      ),
+      seed: () => DeepLinkingState.linkPending(
+        Uri(path: faqLink),
+      ),
+      act: (bloc) => bloc.add(
+        DeepLinkingEvent.consumePendingLink(
+          selectedCustomerCode: fakeCustomerCode,
+          selectedShipTo: fakeShipToCode,
+        ),
+      ),
+      expect: () => [
+        const DeepLinkingState.redirectFAQ(),
+      ],
+    );
+  });
+  
 }
--- test/common_mock_data/mock_other.dart
+++ test/common_mock_data/mock_other.dart
@@ -1,7 +1,14 @@
+
 import 'package:mocktail/mocktail.dart';
 import 'package:ezrxmobile/presentation/routes/router.gr.dart';
+import 'package:ezrxmobile/infrastructure/core/chatbot/chatbot_service.dart';
 import 'package:ezrxmobile/infrastructure/core/mixpanel/mixpanel_service.dart';
+import 'package:ezrxmobile/infrastructure/core/deep_linking/deep_linking_service.dart';
 
 class MixpanelServiceMock extends Mock implements MixpanelService {}
 
 class AutoRouteMock extends Mock implements AppRouter {}
+
+class UniversalLinkServiceMock extends Mock implements DeepLinkingService {}
+
+class ChatBotServiceMock extends Mock implements ChatBotService {}
