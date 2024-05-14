8b4c1e438d2e8060c7ae7a8e9186ebd950df1231
Ananth Kumar <ananthkumar@zuelligpharma.com>
2024-05-14 05:52:08 +0000

    Merged PR 105131: fix/EZRX-22041  If the license information is long then status is not getting dis...
    Make sure we are following : https://zuelligpharma.atlassian.net/wiki/spaces/DDPRG/pages/231670169/Pull+Request+PR+reviews
    
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-22041
    
    ### TDD PR url
    - Not required ,only UI layout.
    
    ### Root cause
    -  If the license information is long then status is not getting display
    ![image.png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/105131/attachments/image.png =x200)
    
    ### Last related PR
    - https://dev.azure.com/zuelligpharmadevops/eZRx%20Mobile/_git/eZRx%20Mobile/pullrequest/71030
    
    ### Solution
    -
    ![image (2).png](https://dev.azure.com/zuelligpharmadevops/3368559d-402f-4ca8-a029-737bca870679/_apis/git/repositories/d10de74c-8ac2-4e8e-af4b-a023b6c9dee2/pullRequests/105131/attachments/image%20%282%29.png =x200)
    
    ### Test account
    - environment:
    - username:
    - salesOrg:
    - customer:
    - shipto:
    - material/setup:
    
    ### Output screenshot
    -
    
    EZRX-22041  If the license information is long then status is not getting display fix

--- lib/presentation/account/profile/widgets/license_tile.dart
+++ lib/presentation/account/profile/widgets/license_tile.dart
@@ -13,89 +13,82 @@ class _LicenseTile extends StatelessWidget {
     return CustomCard(
       key: WidgetKeys.profileLicenseTile,
       padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              RichText(
-                key: WidgetKeys.profileLicenseName,
-                text: TextSpan(
-                  text: customerLicense.licenseType.displayNAIfEmpty,
+              Expanded(
+                child: Text(
+                  '${customerLicense.licenseType.displayNAIfEmpty} - ${customerLicense.licenseDescription.displayNAIfEmpty}',
                   style: Theme.of(context).textTheme.labelSmall,
-                  children: <TextSpan>[
-                    const TextSpan(text: ' - '),
-                    TextSpan(
-                      text: customerLicense.licenseDescription.displayNAIfEmpty,
-                    ),
-                  ],
+                  overflow: TextOverflow.visible,
                 ),
               ),
-              const SizedBox(height: 5),
-              RichText(
-                key: WidgetKeys.profileLicenseNo,
-                text: TextSpan(
-                  text: 'License no:'.tr(),
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall
-                      ?.copyWith(color: ZPColors.darkGray),
-                  children: <TextSpan>[
-                    const TextSpan(text: ' - '),
-                    TextSpan(
-                      text: customerLicense.licenseNumbers.displayNAIfEmpty,
-                    ),
-                  ],
-                ),
+              StatusLabel(
+                key: WidgetKeys.profileLicenseStatus,
+                status: customerLicense.licenseStatus,
               ),
-              const SizedBox(height: 20),
-              RichText(
-                key: WidgetKeys.profileLicenseDateRange,
-                text: TextSpan(
-                  text: 'From '.tr(),
-                  style: Theme.of(context).textTheme.labelSmall,
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: customerLicense.validFrom.dateTimeOrNaString,
-                    ),
-                    TextSpan(
-                      text: ' to '.tr(),
-                    ),
-                    TextSpan(
-                      text: customerLicense.validTo.dateTimeOrNaString,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 5),
-              customerLicense.validTo.aWeekDifference
-                  ? Row(
-                      children: [
-                        const Icon(
-                          Icons.info,
-                          color: ZPColors.skyBlueColor,
-                          size: 18,
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          'Expiring in 7 days'.tr(),
-                          style:
-                              Theme.of(context).textTheme.bodySmall?.copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    color: ZPColors.skyBlueColor,
-                                  ),
-                        ),
-                      ],
-                    )
-                  : const SizedBox.shrink(),
             ],
           ),
-          StatusLabel(
-            key: WidgetKeys.profileLicenseStatus,
-            status: customerLicense.licenseStatus,
+          const SizedBox(height: 5),
+          RichText(
+            key: WidgetKeys.profileLicenseNo,
+            text: TextSpan(
+              text: 'License no:'.tr(),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall
+                  ?.copyWith(color: ZPColors.darkGray),
+              children: <TextSpan>[
+                const TextSpan(text: ' - '),
+                TextSpan(
+                  text: customerLicense.licenseNumbers.displayNAIfEmpty,
+                ),
+              ],
+            ),
           ),
+          const SizedBox(height: 20),
+          RichText(
+            key: WidgetKeys.profileLicenseDateRange,
+            text: TextSpan(
+              text: 'From '.tr(),
+              style: Theme.of(context).textTheme.labelSmall,
+              children: <TextSpan>[
+                TextSpan(
+                  text: customerLicense.validFrom.dateTimeOrNaString,
+                ),
+                TextSpan(
+                  text: ' to '.tr(),
+                ),
+                TextSpan(
+                  text: customerLicense.validTo.dateTimeOrNaString,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 5),
+          customerLicense.validTo.aWeekDifference
+              ? Row(
+                  children: [
+                    const Icon(
+                      Icons.info,
+                      color: ZPColors.skyBlueColor,
+                      size: 18,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Expiring in 7 days'.tr(),
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: ZPColors.skyBlueColor,
+                          ),
+                    ),
+                  ],
+                )
+              : const SizedBox.shrink(),
         ],
       ),
     );
