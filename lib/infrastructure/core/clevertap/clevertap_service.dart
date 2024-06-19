43640619776a2cdbcade7280999bd2e019ef1e81
Vuong Quoc Viet (Victor) <VVuongQuoc@zuelligpharma.com>
2024-06-19 04:47:57 +0000

    Merged PR 109525: Feat/EZRX-23483: Add debug logs in Clevertap
    Make sure we are following : https://zuelligpharma.atlassian.net/wiki/spaces/DDPRG/pages/231670169/Pull+Request+PR+reviews
    
    ### Jira ticket url
    - https://zuelligpharma.atlassian.net/browse/EZRX-23483
    
    ### Root cause
    - Missing in Clevertap events. Like if the event happend 100 times, only 60 times it got sent to Clevertap.
    
    ### Last related PR
    - https://dev.azure.com/zuelligpharmadevops/eZRx%20Mobile/_git/eZRx%20Mobile/commit/f0a3adb4532f64a7f4f8a29ccd3e1b7c104f799e?refName=refs%2Fheads%2Fv3&path=%2Flib%2Finfrastructure%2Fcore%2Fclevertap%2Fclevertap_service.dart&_a=contents
    
    ### Solution
    - Enable debug mode to 3 for Production so Clevertap team can check the logging to find the root cause

--- lib/infrastructure/core/clevertap/clevertap_service.dart
+++ lib/infrastructure/core/clevertap/clevertap_service.dart
@@ -13,9 +13,7 @@ class ClevertapService {
   });
 
   Future<void> init() async {
-    if (config.appFlavor != Flavor.prod) {
-      await CleverTapPlugin.setDebugLevel(3);
-    }
+    await CleverTapPlugin.setDebugLevel(3);
     await createNotificationChannel();
     await setPushToken();
     if (Platform.isIOS) {
